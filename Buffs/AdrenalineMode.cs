48721f425d844bdb0e45348ca273682374a6ec58
Ozzatron <spazeglitch@gmail.com>
2019-07-09 00:05:25 -0400

    fixed inaccurate Rage/Adrenaline tooltips

--- Buffs/AdrenalineMode.cs
+++ Buffs/AdrenalineMode.cs
@@ -4,15 +4,19 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using CalamityMod;
 using CalamityMod.NPCs;
+using CalamityMod.World;
 
 namespace CalamityMod.Buffs
 {
 	public class AdrenalineMode : ModBuff
 	{
-		public override void SetDefaults()
+        public static string RevTip = "250% damage boost. Can burnout down to 149.5%.";
+        public static string DeathTip = "700% damage boost. Can burnout down to 298%.";
+
+        public override void SetDefaults()
 		{
 			DisplayName.SetDefault("Adrenaline Mode");
-			Description.SetDefault("250% (1000% in Death Mode) increased damage.");
+			Description.SetDefault(RevTip);
 			Main.debuff[Type] = true;
 			Main.pvpBuff[Type] = true;
 			Main.buffNoSave[Type] = false;
@@ -24,5 +28,11 @@ namespace CalamityMod.Buffs
 		{
 			player.GetModPlayer<CalamityPlayer>(mod).adrenalineMode = true;
 		}
+
+        public override void ModifyBuffTip(ref string tip, ref int rare)
+        {
+            if (CalamityWorld.death)
+                tip = DeathTip;
+        }
 	}
 }
--- Buffs/RageMode.cs
+++ Buffs/RageMode.cs
@@ -4,15 +4,19 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using CalamityMod;
 using CalamityMod.NPCs;
+using CalamityMod.World;
 
 namespace CalamityMod.Buffs
 {
 	public class RageMode : ModBuff
 	{
-		public override void SetDefaults()
+        public static string RevTip = "150% damage boost. Can be boosted by other items up to 210%.";
+        public static string DeathTip = "300% damage boost. Can be boosted by other items up to 510%.";
+
+        public override void SetDefaults()
 		{
 			DisplayName.SetDefault("Rage Mode");
-			Description.SetDefault("100% (400% in Death Mode) increased damage.  Can be boosted by other items up to 160% (640% in Death Mode).");
+			Description.SetDefault(RevTip);
 			Main.debuff[Type] = true;
 			Main.pvpBuff[Type] = true;
 			Main.buffNoSave[Type] = false;
@@ -24,5 +28,11 @@ namespace CalamityMod.Buffs
 		{
 			player.GetModPlayer<CalamityPlayer>(mod).rageMode = true;
 		}
-	}
+
+        public override void ModifyBuffTip(ref string tip, ref int rare)
+        {
+            if (CalamityWorld.death)
+                tip = DeathTip;
+        }
+    }
 }
