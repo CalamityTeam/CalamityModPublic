5d56ad597967a2e4087b90365fc26dde64731fdb
Ozzatron <spazeglitch@gmail.com>
2019-07-10 12:28:18 -0400

    fixed Aureus Cells

--- Items/Astrageldon/AstralJelly.cs
+++ Items/Astrageldon/AstralJelly.cs
@@ -13,8 +13,7 @@ namespace CalamityMod.Items.Astrageldon
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Aureus Cell");
-            Tooltip.SetDefault("Gives mana regeneration and magic power for 6 minutes\n" +
-                "Restores 200 mana");
+            Tooltip.SetDefault("Gives mana regeneration and magic power for 6 minutes");
         }
 		
 		public override void SetDefaults()
@@ -27,7 +26,8 @@ namespace CalamityMod.Items.Astrageldon
 			item.useTime = 17;
             item.rare = 7;
 			item.useStyle = 2;
-			item.UseSound = SoundID.Item3;
+            item.healMana = 200;
+            item.UseSound = SoundID.Item3;
 			item.consumable = true;
 			item.value = Item.buyPrice(0, 4, 50, 0);
 			item.buffType = BuffID.WellFed;
@@ -36,16 +36,6 @@ namespace CalamityMod.Items.Astrageldon
 
         public override void OnConsumeItem(Player player)
         {
-            player.statMana += 200;
-            if (player.statMana > player.statManaMax2)
-            {
-                player.statMana = player.statManaMax2;
-            }
-            player.AddBuff(BuffID.ManaSickness, Player.manaSickTime, true);
-            if (Main.myPlayer == player.whoAmI)
-            {
-                player.ManaEffect(200);
-            }
             player.AddBuff(BuffID.ManaRegeneration, 21600);
             player.AddBuff(BuffID.MagicPower, 21600);
             player.AddBuff(BuffID.WellFed, 108000);
