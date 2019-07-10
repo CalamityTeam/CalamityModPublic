1e0500ddbf220be56ff39e2fe2bf086453d86ec3
Ozzatron <spazeglitch@gmail.com>
2019-07-09 23:44:38 -0400

    purity axe dust now does damage again

--- Items/Weapons/PurityAxe.cs
+++ Items/Weapons/PurityAxe.cs
@@ -14,7 +14,7 @@ namespace CalamityMod.Items.Weapons
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Axe of Purity");
-            Tooltip.SetDefault("Right click to cleanse evil");
+            Tooltip.SetDefault("Left click to use as a tool\nRight click to cleanse evil");
 		}
 
 	    public override void SetDefaults()
@@ -40,13 +40,6 @@ namespace CalamityMod.Items.Weapons
             return true;
         }
 
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
-            damage = 0;
-            knockBack = 0.0f;
-            return true;
-        }
-
         public override bool CanUseItem(Player player)
         {
             if (player.altFunctionUse == 2)
@@ -62,27 +55,6 @@ namespace CalamityMod.Items.Weapons
                 item.shootSpeed = 0f;
             }
             return base.CanUseItem(player);
-        }
-
-        public override float MeleeSpeedMultiplier(Player player)
-        {
-            if (player.altFunctionUse == 2)
-                return 1.6f;
-            return 1f;
-        }
-
-        public override bool? CanHitNPC(Player player, NPC target)
-        {
-            if (player.altFunctionUse == 2)
-                return false;
-            return null;
-        }
-
-        public override bool CanHitPvp(Player player, Player target)
-        {
-            if (player.altFunctionUse == 2)
-                return false;
-            return true;
         }
 
         public override void MeleeEffects(Player player, Rectangle hitbox)
