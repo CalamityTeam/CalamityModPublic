0ec90df14d03c3313696c1e8530472950d1b6ff6
Ozzatron <spazeglitch@gmail.com>
2019-07-09 23:32:35 -0400

    Axe of Purity can now axe and clean. Clean is right click

--- Items/Weapons/PurityAxe.cs
+++ Items/Weapons/PurityAxe.cs
@@ -14,7 +14,7 @@ namespace CalamityMod.Items.Weapons
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Axe of Purity");
-			Tooltip.SetDefault("Cleanses the evil");
+            Tooltip.SetDefault("Right click to cleanse evil");
 		}
 
 	    public override void SetDefaults()
@@ -33,28 +33,76 @@ namespace CalamityMod.Items.Weapons
             item.rare = 5;
 	        item.UseSound = SoundID.Item1;
 	        item.autoReuse = true;
-			item.shoot = ProjectileID.PurificationPowder;
-			item.shootSpeed = 12f;
 		}
-	
-	    public override void AddRecipes()
-	    {
-	        ModRecipe recipe = new ModRecipe(mod);
-	        recipe.AddIngredient(null, "FellerofEvergreens");
-			recipe.AddIngredient(ItemID.PurificationPowder, 20);
-			recipe.AddIngredient(ItemID.PixieDust, 10);
-	        recipe.AddIngredient(ItemID.CrystalShard, 5);
-	        recipe.AddTile(TileID.Anvils);
-	        recipe.SetResult(this);
-	        recipe.AddRecipe();
-	    }
-	    
-	    public override void MeleeEffects(Player player, Rectangle hitbox)
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            damage = 0;
+            knockBack = 0.0f;
+            return true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                item.axe = 0;
+                item.shoot = ProjectileID.PurificationPowder;
+                item.shootSpeed = 12f;
+            }
+            else
+            {
+                item.axe = 25;
+                item.shoot = 0;
+                item.shootSpeed = 0f;
+            }
+            return base.CanUseItem(player);
+        }
+
+        public override float MeleeSpeedMultiplier(Player player)
+        {
+            if (player.altFunctionUse == 2)
+                return 1.6f;
+            return 1f;
+        }
+
+        public override bool? CanHitNPC(Player player, NPC target)
+        {
+            if (player.altFunctionUse == 2)
+                return false;
+            return null;
+        }
+
+        public override bool CanHitPvp(Player player, Player target)
+        {
+            if (player.altFunctionUse == 2)
+                return false;
+            return true;
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
 	    {
 	        if (Main.rand.Next(5) == 0)
 	        {
 	        	int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 58);
 	        }
 	    }
-	}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "FellerofEvergreens");
+            recipe.AddIngredient(ItemID.PurificationPowder, 20);
+            recipe.AddIngredient(ItemID.PixieDust, 10);
+            recipe.AddIngredient(ItemID.CrystalShard, 5);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
 }
