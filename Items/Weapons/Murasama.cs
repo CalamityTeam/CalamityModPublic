124e2f5d231129311cb783b8ea67405800120a63
SheerExpanse <spazeglitch@gmail.com>
2019-07-15 11:59:50 -0400

    Murasama explicitly cannot be used w/o Yharon dead or Sam name

--- Items/Weapons/Murasama.cs
+++ Items/Weapons/Murasama.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.ID;
 using Terraria.ModLoader;
-using CalamityMod.Items;
 using CalamityMod.World;
 
 namespace CalamityMod.Items.Weapons 
@@ -42,19 +36,15 @@ namespace CalamityMod.Items.Weapons
 			item.GetGlobalItem<CalamityGlobalItem>(mod).postMoonLordRarity = 15;
 		}
 
+        public override bool CanUseItem(Player player)
+        {
+            return CalamityWorld.downedYharon || player.name == "Sam" || player.name == "Samuel Rodrigues";
+        }
+
         public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            bool yharon = CalamityWorld.downedYharon;
-            if (player.name == "Sam" || player.name == "Samuel Rodrigues" || yharon)
-            {
-                Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI, 0.0f, 0.0f);
-                return false;
-            }
-            else
-            {
-                Projectile.NewProjectile(position.X, position.Y, 0f, 0f, 504, 6, 0f, player.whoAmI, 0.0f, 0.0f);
-                return false;
-            }
+            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI, 0.0f, 0.0f);
+            return false;
         }
     }
 }
