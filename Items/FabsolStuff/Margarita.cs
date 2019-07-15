462ef01c1812b84b0a0e376494d13f9fed4101cd
SheerExpanse <spazeglitch@gmail.com>
2019-07-15 11:40:57 -0400

    margarita works with quick heal & quick buff

--- Items/FabsolStuff/Margarita.cs
+++ Items/FabsolStuff/Margarita.cs
@@ -1,5 +1,4 @@
-using System;
-using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,10 +10,10 @@ namespace CalamityMod.Items.FabsolStuff
         public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Margarita");
-			Tooltip.SetDefault(@"Makes you immune to most debuffs
+			Tooltip.SetDefault(@"Restores 200 mana
+Makes you immune to most debuffs
 Reduces defense by 6 and life regen by 1
-One of the best drinks ever created, enjoy it while it lasts
-Restores 200 life and mana");
+One of the best drinks ever created, enjoy it while it lasts");
 		}
 
 		public override void SetDefaults()
@@ -29,17 +28,14 @@ Restores 200 life and mana");
             item.useStyle = 2;
             item.UseSound = SoundID.Item3;
             item.consumable = true;
+            item.potion = true;
+            item.healLife = 200;
             item.buffType = mod.BuffType("Margarita");
-            item.buffTime = 10800; //3 minutes
+            item.buffTime = 10800;
             item.value = Item.buyPrice(0, 23, 30, 0);
 		}
 
         public override bool CanUseItem(Player player)
-        {
-            return player.FindBuffIndex(BuffID.PotionSickness) == -1;
-        }
-
-        public override bool ConsumeItem(Player player)
         {
             return player.FindBuffIndex(BuffID.PotionSickness) == -1;
         }
@@ -63,7 +59,18 @@ Restores 200 life and mana");
                 player.ManaEffect(200);
             }
             player.AddBuff(mod.BuffType("Margarita"), 10800);
-            player.AddBuff(BuffID.PotionSickness, (player.pStone ? 2700 : 3600));
+        }
+
+        // Zeroes out the hardcoded healing function from having a healLife value. The item still heals in the UseItem hook.
+        public override void GetHealLife(Player player, bool quickHeal, ref int healValue)
+        {
+            healValue = 0;
+        }
+
+        // Forces the "Restores X life" tooltip to display the actual life restored instead of zero (due to the previous function).
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            tooltips.Find(line => line.Name == "HealLife").text = "Restores " + item.healLife + " life";
         }
     }
 }
