4c7608e80f8a02213b543acdbfb6152be1770d78
Ozzatron <spazeglitch@gmail.com>
2019-08-04 16:26:00 -0400

    Devastation base damage increased by 1.

--- Items/Weapons/Devastation.cs
+++ Items/Weapons/Devastation.cs
@@ -17,7 +17,7 @@ namespace CalamityMod.Items.Weapons
 		public override void SetDefaults()
 		{
 			item.width = 72;
-			item.damage = 114;
+			item.damage = 115;
 			item.melee = true;
 			item.useAnimation = 24;
 			item.useTime = 24;
