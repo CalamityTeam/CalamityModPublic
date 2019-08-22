11cb88c12defbf0ca84a51b6749c46dbfdb53925
Ozzatron <spazeglitch@gmail.com>
2019-08-22 16:07:58 -0400

    Added set bonus checks to mod support

--- ModSupport.cs
+++ ModSupport.cs
@@ -9,11 +9,15 @@ namespace CalamityMod
 {
 	public class ModSupport
 	{
-		// Returns Calamity's boss downed booleans based on a string provided.
-		public static readonly Func<string, bool> Downed = (name) =>
+        #region Boss / Event Downed
+        /// <summary>
+        /// Returns whether the Calamity boss or event corresponding to the given string has been defeated.
+        /// </summary>
+        /// <param name="boss">The boss or event name to check. Many aliases are accepted.</param>
+        /// <returns>Whether the boss or event has been defeated.</returns>
+        public static bool GetBossDowned(string boss)
 		{
-			name = name.ToLower();
-			switch (name)
+			switch (boss.ToLower())
 			{
 				default: return false;
 
@@ -146,15 +150,21 @@ namespace CalamityMod
 				case "supremecalamitas":
 				case "supreme calamitas": return CalamityWorld.downedSCal;
 			}
-		};
+		}
+        #endregion
 
-		// Returns Calamity's "in biome" booleans based on a string provided.
-		public static readonly Func<Player, string, bool> InZone = (p, name) =>
+        #region Player in Zone / Area
+        /// <summary>
+        /// Returns whether the specified player is in the Calamity biome or area corresponding to the given string.
+        /// </summary>
+        /// <param name="p">The player whose locale is being questioned.</param>
+        /// <param name="zone">The zone or area name to check. Many aliases are accepted.</param>
+        /// <returns>Whether the player is currently in the zone.</returns>
+        public static bool GetInZone(Player p, string zone)
 		{
 			Mod calamity = ModLoader.GetMod("CalamityMod");
 			CalamityPlayer mp = p.GetModPlayer<CalamityPlayer>(calamity);
-			name = name.ToLower();
-			switch (name)
+			switch (zone.ToLower())
 			{
 				default: return false;
 
@@ -241,13 +251,18 @@ namespace CalamityMod
 				case "abysslayer4":
 				case "abyss layer 4": return mp.ZoneAbyssLayer4;
 			}
-		};
+		}
+        #endregion
 
-		// Returns Calamity's various difficulty modes/modifiers based on a string provided.
-		public static readonly Func<string, bool> Difficulty = (name) =>
+        #region Difficulty Modes
+        /// <summary>
+        /// Returns whether the Calamity difficulty modifier corresponding to the given string is currently active.
+        /// </summary>
+        /// <param name="difficulty">The difficulty modifier to check for.</param>
+        /// <returns>Whether the difficulty is currently active.</returns>
+        public static bool GetDifficultyActive(string difficulty)
 		{
-			name = name.ToLower();
-			switch (name)
+			switch (difficulty.ToLower())
 			{
 				default: return false;
 
@@ -279,24 +294,251 @@ namespace CalamityMod
 				case "steel soul":
 				case "permadeath": return CalamityWorld.ironHeart;
 			}
-		};
+		}
+        #endregion
 
-		public static object Call(params object[] args)
+        #region Player Armor Set Bonuses
+        /// <summary>
+        /// Returns whether the specified player has the set bonus corresponding to the given string.
+        /// </summary>
+        /// <param name="p">The player whose set bonuses are being questioned.</param>
+        /// <param name="setBonus">The set bonus to check for.</param>
+        /// <returns>Whether the player currently has the set bonus.</returns>
+        public static bool GetSetBonus(Player p, string setBonus)
+        {
+            Mod calamity = ModLoader.GetMod("CalamityMod");
+            CalamityPlayer mp = p.GetModPlayer<CalamityPlayer>(calamity);
+
+            setBonus = setBonus.ToLower();
+
+            // Victide
+            if (setBonus == "victide" || setBonus.StartsWith("victide_") || setBonus.StartsWith("victide "))
+                return mp.victideSet;
+
+            // Aerospec
+            if (setBonus == "aerospec" || setBonus.StartsWith("aerospec_") || setBonus.StartsWith("aerospec "))
+                return mp.aeroSet;
+
+            // Statigel
+            if (setBonus == "statigel" || setBonus.StartsWith("statigel_") || setBonus.StartsWith("statigel "))
+                return mp.statigelSet;
+
+            // Mollusk
+            if (setBonus == "mollusk")
+                return mp.molluskSet;
+
+            // Daedalus
+            switch(setBonus)
+            {
+                default: break;
+                case "daedalus": return mp.daedalusReflect || mp.daedalusShard || mp.daedalusAbsorb || mp.daedalusCrystal || mp.daedalusSplit;
+                case "daedalus_melee":
+                case "daedalus melee": return mp.daedalusReflect;
+                case "daedalus_ranged":
+                case "daedalus ranged": return mp.daedalusShard;
+                case "daedalus_magic":
+                case "daedalus magic": return mp.daedalusAbsorb;
+                case "daedalus_summon":
+                case "daedalus summon": return mp.daedalusCrystal;
+                case "daedalus_rogue":
+                case "daedalus rogue": return mp.daedalusSplit;
+            }
+
+            // Reaver
+            switch(setBonus)
+            {
+                default: break;
+                case "reaver": return mp.reaverBlast || mp.reaverDoubleTap || mp.reaverBurst || mp.reaverOrb || mp.reaverSpore;
+                case "reaver_melee":
+                case "reaver melee": return mp.reaverBlast;
+                case "reaver_ranged":
+                case "reaver ranged": return mp.reaverDoubleTap;
+                case "reaver_magic":
+                case "reaver magic": return mp.reaverBurst;
+                case "reaver_summon":
+                case "reaver summon": return mp.reaverOrb;
+                case "reaver_rogue":
+                case "reaver rogue": return mp.reaverSpore;
+            }
+
+            // Ataxia
+            switch (setBonus)
+            {
+                default: break;
+                case "ataxia": return mp.ataxiaBlaze;
+                case "ataxia_melee":
+                case "ataxia melee": return mp.ataxiaGeyser;
+                case "ataxia_ranged":
+                case "ataxia ranged": return mp.ataxiaBolt;
+                case "ataxia_magic":
+                case "ataxia magic": return mp.ataxiaMage;
+                case "ataxia_summon":
+                case "ataxia summon": return mp.chaosSpirit;
+                case "ataxia_rogue":
+                case "ataxia rogue": return mp.ataxiaVolley;
+            }
+
+            // Astral
+            if (setBonus == "astral")
+                return mp.astralStarRain;
+
+            // Xeroc
+            if (setBonus == "xeroc")
+                return mp.xerocSet;
+
+            // Tarragon
+            switch(setBonus)
+            {
+                default: break;
+                case "tarragon": return mp.tarraSet;
+                case "tarragon_melee":
+                case "tarragon melee": return mp.tarraMelee;
+                case "tarragon_ranged":
+                case "tarragon ranged": return mp.tarraRanged;
+                case "tarragon_magic":
+                case "tarragon magic": return mp.tarraMage;
+                case "tarragon_summon":
+                case "tarragon summon": return mp.tarraSummon;
+                case "tarragon_rogue":
+                case "tarragon rogue": return mp.tarraThrowing;
+            }
+
+            // Bloodflare
+            switch (setBonus)
+            {
+                default: break;
+                case "bloodflare": return mp.bloodflareSet;
+                case "bloodflare_melee":
+                case "bloodflare melee": return mp.bloodflareMelee;
+                case "bloodflare_ranged":
+                case "bloodflare ranged": return mp.bloodflareRanged;
+                case "bloodflare_magic":
+                case "bloodflare magic": return mp.bloodflareMage;
+                case "bloodflare_summon":
+                case "bloodflare summon": return mp.bloodflareSummon;
+                case "bloodflare_rogue":
+                case "bloodflare rogue": return mp.bloodflareThrowing;
+            }
+
+            // Omega Blue
+            if (setBonus == "omegablue" || setBonus == "omega blue")
+                return mp.omegaBlueSet;
+
+            // God Slayer
+            switch (setBonus)
+            {
+                default: break;
+                case "godslayer":
+                case "god slayer": return mp.godSlayer;
+                case "godslayer_melee":
+                case "godslayer melee":
+                case "god slayer melee": return mp.godSlayerDamage; // melee helm's unique damage reducing property
+                case "godslayer_ranged":
+                case "godslayer ranged":
+                case "god slayer ranged": return mp.godSlayerRanged;
+                case "godslayer_magic":
+                case "godslayer magic":
+                case "god slayer magic": return mp.godSlayerMage;
+                case "godslayer_summon":
+                case "godslayer summon":
+                case "god slayer summon": return mp.godSlayerSummon;
+                case "godslayer_rogue":
+                case "godslayer rogue":
+                case "god slayer rogue": return mp.godSlayerThrowing;
+            }
+
+            // Silva
+            switch (setBonus)
+            {
+                default: break;
+                case "silva": return mp.silvaSet;
+                case "silva_melee":
+                case "silva melee": return mp.silvaMelee;
+                case "silva_ranged":
+                case "silva ranged": return mp.silvaRanged;
+                case "silva_magic":
+                case "silva magic": return mp.silvaMage;
+                case "silva_summon":
+                case "silva summon": return mp.silvaSummon;
+                case "silva_rogue":
+                case "silva rogue": return mp.silvaThrowing;
+            }
+
+            // Auric Tesla
+            if (setBonus == "auric" || setBonus == "aurictesla" || setBonus == "auric tesla")
+                return mp.auricSet;
+
+            // Demonshade
+            if (setBonus == "demonshade")
+                return mp.dsSetBonus;
+
+            return false;
+        }
+        #endregion
+
+        #region Call
+        public static object Call(params object[] args)
 		{
-			if (args.Length <= 0 || !(args[0] is string)) return new Exception("ERROR: No function name specified. First argument must be a function name.");
+            if (args is null || args.Length <= 0) return new ArgumentNullException("ERROR: No function name specified. First argument must be a function name.");
+            if (!(args[0] is string)) return new ArgumentException("ERROR: First argument must be a string function name.");
 
-			string methodName = (string)args[0];
+            string methodName = args[0].ToString();
+            Player p = null;
 			switch (methodName)
 			{
 				case "Downed":
-					return Downed;
-				case "InZone":
-					return InZone;
-				case "Difficulty":
-					return Difficulty;
-			}
+                case "GetDowned":
+                case "BossDowned":
+                case "GetBossDowned":
+                    if (args.Length < 2) return new ArgumentNullException("ERROR: Must specify a boss or event name as a string.");
+                    if (!(args[1] is string)) return new ArgumentException("ERROR: The argument to \"Downed\" must be a string.");
+                    return GetBossDowned(args[1].ToString());
 
-			return new Exception("ERROR: Invalid function name provided as first argument.");
+                case "Zone":
+                case "GetZone":
+                case "InZone":
+                case "GetInZone":
+                    if (args.Length < 2) return new ArgumentNullException("ERROR: Must specify both a Player object (or int index of a Player) and a zone name as a string.");
+                    if (args.Length < 3) return new ArgumentNullException("ERROR: Must specify a zone name as a string.");
+                    if (!(args[2] is string)) return new ArgumentException("ERROR: The second argument to \"InZone\" must be a string.");
+                    if (!(args[1] is int) || !(args[1] is Player)) return new ArgumentException("ERROR: The first argument to \"InZone\" must be a Player or an int.");
+
+                    // If the argument is an int, get the corresponding player
+                    if (args[1] is int)
+                        p = Main.player[(int)args[1]];
+                    else if (args[1] is Player)
+                        p = (Player)args[1];
+                    return GetInZone(p, args[2].ToString());
+
+				case "Difficulty":
+                case "GetDifficulty":
+                case "DifficultyActive":
+                case "GetDifficultyActive":
+                    if (args.Length < 2) return new ArgumentNullException("ERROR: Must specify a difficulty modifier name as a string.");
+                    if (!(args[1] is string)) return new ArgumentException("ERROR: The argument to \"Difficulty\" must be a string.");
+                    return GetDifficultyActive(args[1].ToString());
+
+                case "SetBonus":
+                case "SetBonusActive":
+                case "HasSetBonus":
+                case "GetSetBonus":
+                case "GetSetBonusActive":
+                    if (args.Length < 2) return new ArgumentNullException("ERROR: Must specify both a Player object (or int index of a Player) and a set bonus name as a string.");
+                    if (args.Length < 3) return new ArgumentNullException("ERROR: Must specify a set bonus name as a string.");
+                    if (!(args[2] is string)) return new ArgumentException("ERROR: The second argument to \"SetBonus\" must be a string.");
+                    if (!(args[1] is int) || !(args[1] is Player)) return new ArgumentException("ERROR: The first argument to \"SetBonus\" must be a Player or an int.");
+
+                    // If the argument is an int, get the corresponding player
+                    if (args[1] is int)
+                        p = Main.player[(int)args[1]];
+                    else if (args[1] is Player)
+                        p = (Player)args[1];
+                    return GetSetBonus(p, args[2].ToString());
+
+                default:
+                    return new ArgumentException("ERROR: Invalid method name.");
+			}
 		}
-	}
+        #endregion
+    }
 }
