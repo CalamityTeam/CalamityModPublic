d82c4b8d60e66d3b2140da9d6b854db4e439b24f
Ozzatron <spazeglitch@gmail.com>
2019-09-29 21:46:36 -0400

    Added Utensil Poker.

---
+++ Projectiles/Rogue/Fork.cs
@@ -0,0 +1,52 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class Fork : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Fork");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 14;
+			projectile.height = 14;
+			projectile.friendly = true;
+			projectile.penetrate = 1;
+			projectile.aiStyle = 113;
+			projectile.timeLeft = 600;
+			aiType = 598;
+			projectile.GetGlobalProjectile<CalamityGlobalProjectile>(mod).rogue = true;
+		}
+
+		public override void AI()
+		{
+			if (Main.rand.Next(5) == 0)
+			{
+				Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 5, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+			}
+			projectile.spriteDirection = projectile.direction = (projectile.velocity.X > 0).ToDirectionInt();
+			projectile.rotation = projectile.velocity.ToRotation();
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			Player player = Main.player[projectile.owner];
+			target.AddBuff(BuffID.Bleeding, 300);
+			target.AddBuff(mod.BuffType("BurningBlood"), 300);
+			player.AddBuff(BuffID.WellFed, 180);
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			CalamityGlobalProjectile.DrawCenteredAndAfterimage(projectile, lightColor, ProjectileID.Sets.TrailingMode[projectile.type], 1);
+			return false;
+		}
+	}
+}
