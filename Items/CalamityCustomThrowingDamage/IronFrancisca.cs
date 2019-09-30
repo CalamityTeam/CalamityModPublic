5b7526343b4209ac18a6539836f8285a33073374
Ozzatron <spazeglitch@gmail.com>
2019-09-29 21:50:38 -0400

    Added Iron Francisca and Lead Tomahawk.

---
+++ Items/CalamityCustomThrowingDamage/IronFrancisca.cs
@@ -0,0 +1,59 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.CalamityCustomThrowingDamage
+{
+    public class IronFrancisca : CalamityDamageItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Iron Francisca");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 40;
+            item.damage = 10;
+            item.noMelee = true;
+            item.consumable = true;
+            item.noUseGraphic = true;
+            item.useAnimation = 15;
+            item.useStyle = 1;
+            item.useTime = 15;
+            item.knockBack = 1f;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.height = 36;
+            item.maxStack = 999;
+            item.value = 1000;
+            item.rare = 0;
+            item.shoot = mod.ProjectileType("IronFrancisca");
+            item.shootSpeed = 12f;
+            Mod calamity = ModLoader.GetMod("CalamityMod");
+            item.GetGlobalItem<CalamityGlobalItem>(calamity).rogue = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (player.GetCalamityPlayer().StealthStrikeAvailable())
+            {
+                Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI, 0f, 1f);
+                return false;
+            }
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood);
+            recipe.anyWood = true;
+            recipe.AddIngredient(ItemID.IronBar);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this, 50);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/Rogue/LeadTomahawk.cs
@@ -0,0 +1,80 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class LeadTomahawk : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lead Tomahawk");
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 8;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = true;
+            projectile.penetrate = 2;
+            projectile.timeLeft = 600;
+            Mod calamity = ModLoader.GetMod("CalamityMod");
+            projectile.GetGlobalProjectile<CalamityGlobalProjectile>(calamity).rogue = true;
+        }
+
+        public override void AI()
+        {
+            if (projectile.ai[1] == 1f)
+            {
+                projectile.penetrate = -1;
+            }
+
+            projectile.rotation += 0.4f * projectile.direction;
+
+            projectile.velocity.Y = projectile.velocity.Y + 0.2f;
+            if (projectile.velocity.Y > 16f)
+            {
+                projectile.velocity.Y = 16f;
+            }
+
+            if (projectile.ai[0] == 0)
+            {
+                projectile.damage *= 2;
+            }
+            projectile.ai[0]++;
+            if (projectile.ai[0] == 20)
+            {
+                projectile.damage /= 2;
+            }
+
+            return;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D tex = Main.projectileTexture[projectile.type];
+            spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, tex.Size() / 2f, projectile.scale, SpriteEffects.None, 0f);
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            if (Main.rand.Next(2) == 0)
+            {
+                Item.NewItem((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height, mod.ItemType("LeadTomahawk"));
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+            Main.PlaySound(0, projectile.position);
+            projectile.Kill();
+            return false;
+        }
+    }
+}
