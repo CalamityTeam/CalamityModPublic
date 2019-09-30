b57635f4034b5ebf69e1a0e8863f3487e332b757
Ozzatron <spazeglitch@gmail.com>
2019-09-29 21:31:56 -0400

    Added Throwing Bricks.

---
+++ Projectiles/Rogue/BrickFragment.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class BrickFragment : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Brick Fragment");
+            Main.projFrames[projectile.type] = 2;
+        }
+        public override void SetDefaults()
+        {
+            projectile.friendly = true;
+            projectile.width = 13;
+            projectile.height = 13;
+            projectile.GetGlobalProjectile<CalamityGlobalProjectile>(mod).rogue = true;
+        }
+        public override void AI()
+        {
+            //Choose the sprite of the projectile
+            if (projectile.ai[0] != 1)
+            {
+                projectile.frame = Main.rand.Next(2);
+                projectile.ai[0] += 1;
+            }
+            //Rotation and gravity
+            projectile.rotation += 0.6f * projectile.direction;
+            projectile.velocity.Y = projectile.velocity.Y + 0.27f;
+            if (projectile.velocity.Y > 16f)
+            {
+                projectile.velocity.Y = 16f;
+            }
+
+        }
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(0, (int) projectile.position.X, (int) projectile.position.Y);
+            //Dust effect
+            int splash = 0;
+            while (splash < 4)
+            {
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, 9, -projectile.velocity.X * 0.15f, -projectile.velocity.Y * 0.10f, 150, default(Color), 0.9f);
+                splash += 1;
+            }
+        }
+    }
+}
