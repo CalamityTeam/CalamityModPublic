e679645b060b855b50fba3b4d0516b4099c6a1d1
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-09-30 21:09:59 -0400

    cosmilite bathtub table

--- Tiles/FurnitureCosmilite/CosmiliteBath.cs
+++ Tiles/FurnitureCosmilite/CosmiliteBath.cs
@@ -17,6 +17,7 @@ namespace CalamityMod.Tiles.FurnitureCosmilite
             TileObjectData.addTile(Type);
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("Cosmilite Bathtub");
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
             AddMapEntry(new Color(191, 142, 111), name);
             animationFrameHeight = 54;
         }
