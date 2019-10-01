47a59dcdcaef931c5ea0f2194e355dbaa22a8b5d
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-09-30 21:32:57 -0400

    Stratus Bathtub Table

--- Tiles/FurnitureStratus/StratusBathtub.cs
+++ Tiles/FurnitureStratus/StratusBathtub.cs
@@ -17,6 +17,7 @@ namespace CalamityMod.Tiles.FurnitureStratus
             TileObjectData.addTile(Type);
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("Stratus Bathtub");
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
             AddMapEntry(new Color(191, 142, 111), name);
             animationFrameHeight = 54;
         }
