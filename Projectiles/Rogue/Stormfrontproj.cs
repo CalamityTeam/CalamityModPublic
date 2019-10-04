22e84215bebcfc6753a4252b2c32a0c28cae40e4
Shucks <edgarjagh@gmail.com>
2019-10-03 19:03:13 -0500

    Added Stormfront Razor

---
+++ Projectiles/Rogue/Stormfrontproj.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class Stormfrontproj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Stormfront Knife");
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 4;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.friendly = true;
+            projectile.penetrate = 5;
+            projectile.timeLeft = 300;
+            projectile.extraUpdates = 1;
+            projectile.GetGlobalProjectile<CalamityGlobalProjectile>(mod).rogue = true;
+        }
+
+        public override void AI()
+        {
+            drawOriginOffsetX = 50;
+            drawOriginOffsetY = 20;
+            projectile.ai[0]++;
+            projectile.spriteDirection = projectile.direction = (projectile.velocity.X > 0).ToDirectionInt();
+            projectile.rotation = projectile.velocity.ToRotation() + (projectile.spriteDirection == 1 ? 0f : MathHelper.Pi);
+            projectile.rotation += projectile.spriteDirection * MathHelper.ToRadians(45f);
+            if (projectile.ai[1] == 0)
+            {
+                projectile.ai[1] = 1;
+            }
+            float sparkFreq = 80 / projectile.ai[1];
+            if (projectile.ai[0] >= sparkFreq)
+            {
+                Vector2 sparkS = new Vector2(Main.rand.NextFloat(-14f, 14f), Main.rand.NextFloat(-14f, 14f));
+                Projectile.NewProjectile(projectile.Center,sparkS,mod.ProjectileType("Stormfrontspark"),projectile.damage,3f,projectile.owner);
+                projectile.ai[0] = 0;
+            }
+            if (Main.rand.Next(10) == 0)
+            {
+                int d = Dust.NewDust(projectile.Center, projectile.width, projectile.height, 246, 0f, 0f, 100, new Color(255, Main.DiscoG, 53), 1f);
+                Main.dust[d].scale += (float)Main.rand.Next(50) * 0.01f;
+                Main.dust[d].noGravity = true;
+                Main.dust[d].position = projectile.Center;
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+            Main.PlaySound(0, projectile.position);
+            return true;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            CalamityGlobalProjectile.DrawCenteredAndAfterimage(projectile, lightColor, ProjectileID.Sets.TrailingMode[projectile.type], 1);
+            return false;
+        }
+    }
+}
---
+++ Projectiles/Rogue/Stormfrontspark.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class Stormfrontspark : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spark");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.friendly = true;
+            projectile.width = 6;
+            projectile.height = 12;
+            projectile.timeLeft = 240;
+            projectile.penetrate = -1;
+            projectile.GetGlobalProjectile<CalamityGlobalProjectile>(mod).rogue = true;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation += projectile.velocity.X * 0.1f;
+			int num199 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 246, 0f, 0f, 100, new Color(255, Main.DiscoG, 53), 1f);
+			Dust expr_8976_cp_0 = Main.dust[num199];
+			expr_8976_cp_0.position.X = expr_8976_cp_0.position.X - 2f;
+			Dust expr_8994_cp_0 = Main.dust[num199];
+			expr_8994_cp_0.position.Y = expr_8994_cp_0.position.Y + 2f;
+			Main.dust[num199].scale += (float)Main.rand.Next(50) * 0.01f;
+			Main.dust[num199].noGravity = true;
+			Dust expr_89E7_cp_0 = Main.dust[num199];
+			expr_89E7_cp_0.velocity.Y = expr_89E7_cp_0.velocity.Y - 2f;
+			if (Main.rand.NextBool(2))
+			{
+				int num200 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 246, 0f, 0f, 100, new Color(255, Main.DiscoG, 53), 1f);
+				Dust expr_8A4E_cp_0 = Main.dust[num200];
+				expr_8A4E_cp_0.position.X = expr_8A4E_cp_0.position.X - 2f;
+				Dust expr_8A6C_cp_0 = Main.dust[num200];
+				expr_8A6C_cp_0.position.Y = expr_8A6C_cp_0.position.Y + 2f;
+				Main.dust[num200].scale += 0.3f + (float)Main.rand.Next(50) * 0.01f;
+				Main.dust[num200].noGravity = true;
+				Main.dust[num200].velocity *= 0.1f;
+			}
+			if ((double)projectile.velocity.Y < 0.25 && (double)projectile.velocity.Y > 0.15)
+			{
+				projectile.velocity.X = projectile.velocity.X * 0.8f;
+			}
+			projectile.rotation = -projectile.velocity.X * 0.05f;
+            projectile.velocity.Y += 0.15f;
+			if (projectile.velocity.Y > 16f)
+			{
+				projectile.velocity.Y = 16f;
+			}
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (projectile.penetrate == 0)
+            {
+                projectile.Kill();
+            }
+            return false;
+        }
+    }
+}
