1fc332b07ad393b5378dbd6aa318bd7ea0a6d37b
DominicKarma <llama4581@yahoo.com>
2019-10-17 17:07:34 -0500

    Update EpidemicShredder.cs

--- Items/CalamityCustomThrowingDamage/EpidemicShredder.cs
+++ Items/CalamityCustomThrowingDamage/EpidemicShredder.cs
@@ -1,1 +1,45 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.CalamityCustomThrowingDamage
+{
+    public class EpidemicShredder : CalamityDamageItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Epidemic Shredder");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 34;
+            item.height = 34;
             item.damage = 75;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.autoReuse = true;
+            item.useAnimation = 12;
+            item.useTime = 12;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.knockBack = 4.5f;
+            item.UseSound = SoundID.Item1;
+            item.value = Item.buyPrice(0, 80, 0, 0);
+            item.rare = 8;
+            item.shoot = mod.ProjectileType("EpidemicShredderProjectile");
+            item.shootSpeed = 18f;
+            item.Calamity().rogue = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.ChlorophyteBar, 20);
+            recipe.AddIngredient(ItemID.Nanites, 150);
+            recipe.AddIngredient(null, "PlagueCellCluster", 15);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
