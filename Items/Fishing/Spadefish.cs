69a676ff6093827d6502aaa9f7d5ed03227f2ec9
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-11-06 21:26:40 -0500

    added sunken sea fish, gemfish, stuffed fish, and spadefish

---
+++ Items/Fishing/Spadefish.cs
@@ -0,0 +1,35 @@
+using CalamityMod.Items.Materials;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Fishing
+{
+    public class Spadefish : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spadefish");
+            Tooltip.SetDefault("How can a fish be used to dig through the ground?\n" +
+				"Some questions are best left unanswered.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 15;
+            item.melee = true;
+            item.width = 46;
+            item.height = 44;
+            item.useTime = 8;
+            item.useAnimation = 16;
+            item.useTurn = true;
+            item.pick = 30;
+            item.useStyle = 1;
+            item.knockBack = 2f;
+            item.value = Item.buyPrice(0, 2, 0, 0);
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+    }
+}
