81205b9051766d0056a951e32c807933cafc1019
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-11-14 22:15:50 -0500

    raider's crit stack info

--- Items/Accessories/Nanotech.cs
+++ Items/Accessories/Nanotech.cs
@@ -1,6 +1,7 @@
 ﻿using CalamityMod.CalPlayer;
 using CalamityMod.Items.Materials;
 using CalamityMod.Tiles.Furniture.CraftingStations;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -12,13 +13,7 @@ namespace CalamityMod.Items.Accessories
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Nanotech");
-            Tooltip.SetDefault("Rogue projectiles create nanoblades as they travel\n" +
-                "Stealth strikes summon lunar flares on enemy hits\n" +
-                "Rogue weapons have a chance to instantly kill normal enemies\n" +
-                "10% increased rogue damage, 5% increased rogue crit chance, and 15% increased rogue velocity\n" +
-                "Whenever you crit an enemy with a rogue weapon your rogue damage increases\n" +
-                "This effect can stack up to 250 times\n" +
-                "Max rogue damage boost is 25%");
+            Tooltip.SetDefault("Rogue projectiles create nanoblades as they travel");
         }
 
         public override void SetDefaults()
@@ -28,6 +23,25 @@ namespace CalamityMod.Items.Accessories
             item.value = Item.buyPrice(0, 90, 0, 0);
             item.accessory = true;
             item.Calamity().postMoonLordRarity = 20;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> list)
+        {
+            int critLevel = Main.player[Main.myPlayer].Calamity().raiderStack;
+            foreach (TooltipLine line2 in list)
+            {
+                if (line2.mod == "Terraria" && line2.Name == "Tooltip0")
+                {
+                    line2.text = "Rogue projectiles create nanoblades as they travel\n" +
+                "Stealth strikes summon lunar flares on enemy hits\n" +
+                "Rogue weapons have a chance to instantly kill normal enemies\n" +
+                "10% increased rogue damage, 5% increased rogue crit chance, and 15% increased rogue velocity\n" +
+                "Whenever you crit an enemy with a rogue weapon your rogue damage increases\n" +
+                "This effect can stack up to 250 times\n" +
+                "Max rogue damage boost is 25%\n" +
+                "Rogue Crit Level: " + critLevel;
+                }
+            }
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
