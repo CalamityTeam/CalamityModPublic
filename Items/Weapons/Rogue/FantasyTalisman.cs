405cc733fe5e1bbc20e9d7efd762d52722ac0e60
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-11-23 00:08:34 -0500

    added fantasy talisman (unobtainable)

---
+++ Items/Weapons/Rogue/FantasyTalisman.cs
@@ -0,0 +1,63 @@
+using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.Rogue;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Rogue
+{
+    public class FantasyTalisman : RogueWeapon
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fantasy Talisman");
+            Tooltip.SetDefault(@"Fires high velocity talismans that ignore gravity
+Talismans attach to enemies, causing them to release lost souls
+Stealth strikes release more souls and leave behind souls as they travel");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 34;
+            item.damage = 100;
+            item.noMelee = true;
+            item.consumable = true;
+            item.noUseGraphic = true;
+            item.useAnimation = 14;
+            item.useTime = 14;
+            item.useStyle = 1;
+            item.knockBack = 6f;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.height = 62;
+            item.maxStack = 999;
+            item.value = Item.buyPrice(0, 0, 60, 0);
+            item.rare = 7;
+            item.shoot = ModContent.ProjectileType<FantasyTalismanProj>();
+            item.shootSpeed = 18f;
+            item.Calamity().rogue = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (player.Calamity().StealthStrikeAvailable()) //setting the stealth strike
+            {
+                int stealth = Projectile.NewProjectile(position, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI, 0f, 0f);
+                Main.projectile[stealth].Calamity().stealthStrike = true;
+                return false;
+            }
+            return true;
+        }
+
+        /*public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<SolarVeil>());
+            recipe.AddIngredient(ItemID.Ectoplasm);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this, 5);
+            recipe.AddRecipe();
+        }*/
+    }
+}
