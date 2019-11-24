72131fc5856b63d26bf9e17a8b34e1793c4344d3
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-11-24 09:17:10 -0500

    added radiator light pet bab

---
+++ Buffs/Pets/RadiatorBuff.cs
@@ -0,0 +1,28 @@
+using CalamityMod.Projectiles.Pets;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Buffs.Pets
+{
+    public class RadiatorBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Radiator");
+            Description.SetDefault("Radioactive but adorable");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.lightPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.buffTime[buffIndex] = 18000;
+            player.Calamity().radiator = true;
+            bool petProjectileNotSpawned = player.ownedProjectileCounts[ModContent.ProjectileType<RadiatorPet>()] <= 0;
+            if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<RadiatorPet>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+        }
+    }
+}
