8c34115ac320e99868635a458b99a6448e1f2683
AlterraKira <kiramichellebruce@gmail.com>
2019-12-03 19:06:25 +0000

    Ashen Stalactite

---
+++ Items/Weapons/Rogue/AshenStalactite.cs
@@ -0,0 +1,54 @@
+using CalamityMod.Projectiles.Rogue;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Rogue
+{
+    public class AshenStalactite : RogueWeapon
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ashen Stalactite");
+            Tooltip.SetDefault("Throws a fast, small stalactite that crumbles to dust after travelling a short distance\n" +
+                "Stealth strikes cause a larger, more damaging stalagmite to be thrown which travels slower and further before crumbling to damaging dust");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 36;
+            item.height = 34;
+            item.damage = 50;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useAnimation = 18;
+            item.useStyle = 1;
+            item.useTime = 20;
+            item.knockBack = 1f;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.value = Item.buyPrice(0, 1, 0, 0);
+            item.rare = 3;
+            item.shoot = ModContent.ProjectileType<AshenStalactiteProj>();
+            item.shootSpeed = 15f;
+            item.Calamity().rogue = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (player.Calamity().StealthStrikeAvailable())
+            {
+                int stealthType = ModContent.ProjectileType<AshenStalagmiteProj>();
+                float stealthSpeedMult = 0.6f;
+                float stealthDamageMult = 1.5f;
+                float stealthKnockbackMult = 2.5f;
+                int p = Projectile.NewProjectile(position.X, position.Y, speedX * stealthSpeedMult, speedY * stealthSpeedMult, stealthType, (int)(damage * stealthDamageMult), (int)(knockBack * stealthKnockbackMult), player.whoAmI, 0f, 1f);
+                Main.projectile[p].Calamity().stealthStrike = true;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Projectiles/Rogue/AshenStalactiteProj.cs
@@ -0,0 +1,61 @@
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class AshenStalactiteProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ashen Stalactite");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 25;
+            projectile.Calamity().rogue = true;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 2.355f;
+
+            projectile.velocity.Y += 0.1f;
+            if (projectile.velocity.Y > 16f)
+            {
+                projectile.velocity.Y = 16f;
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D tex = Main.projectileTexture[projectile.type];
+            spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, tex.Size() / 2f, projectile.scale, SpriteEffects.None, 0f);
+            return false;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+            Main.PlaySound(0, projectile.position);
+            projectile.Kill();
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Dust.NewDust(projectile.Center, 1, 1, 1, projectile.velocity.X, projectile.velocity.Y);
+            }
+        }
+    }
+}
