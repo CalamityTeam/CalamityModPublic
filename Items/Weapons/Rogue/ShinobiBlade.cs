60430676c99067ccb939d244e606edd328ecc614
AlterraKira <kiramichellebruce@gmail.com>
2019-12-12 23:55:10 +0000

    Shinobi Blade

---
+++ Items/Weapons/Rogue/ShinobiBlade.cs
@@ -0,0 +1,59 @@
+using CalamityMod.Projectiles.Rogue;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Rogue
+{
+    public class ShinobiBlade : RogueWeapon
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shinobi Blade");
+            Tooltip.SetDefault("Throws a fast blade that spawns healing orbs when it kills an enemy\n" +
+				"Stealth strikes cause 3 blades to be thrown at once");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 30;
+            item.damage = 15;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useAnimation = 15;
+            item.useStyle = 1;
+            item.useTime = 15;
+            item.knockBack = 1f;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.height = 34;
+            item.maxStack = 1;
+            item.value = Item.buyPrice(0, 4, 0, 0);
+            item.rare = 2;
+            item.shoot = ModContent.ProjectileType<ShinobiBladeProjectile>();
+            item.shootSpeed = 20f;
+            item.Calamity().rogue = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (player.Calamity().StealthStrikeAvailable())
+            {
+                float spreadAngle = MathHelper.ToRadians(10f);
+                Vector2 direction = new Vector2(speedX, speedY);
+                Vector2 baseDirection = direction.RotatedBy(-spreadAngle);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Vector2 currentDirection = baseDirection.RotatedBy(spreadAngle * i);
+
+                    int p = Projectile.NewProjectile(position.X, position.Y, currentDirection.X, currentDirection.Y, type, damage, knockBack, player.whoAmI, 0f, 0f);
+                    Main.projectile[p].Calamity().stealthStrike = true;
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Projectiles/Healing/ShinobiHealOrb.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+namespace CalamityMod.Projectiles.Healing
+{
+    public class ShinobiHealOrb : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blue Healing Orb");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 20;
+            projectile.height = 20;
+            aiType = 348;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 300;
+        }
+
+        public override void AI()
+        {
+            int num487 = projectile.owner;
+
+            float num488 = 6f;
+            if (Main.player[num487].lifeMagnet)
+                num488 *= 1.5f;
+
+            Vector2 vector36 = new Vector2(projectile.position.X + (float)projectile.width * 0.5f, projectile.position.Y + (float)projectile.height * 0.5f);
+            float num489 = Main.player[num487].Center.X - vector36.X;
+            float num490 = Main.player[num487].Center.Y - vector36.Y;
+            float num491 = (float)Math.Sqrt((double)(num489 * num489 + num490 * num490));
+            if (num491 < 50f && projectile.position.X < Main.player[num487].position.X + (float)Main.player[num487].width && projectile.position.X + (float)projectile.width > Main.player[num487].position.X && projectile.position.Y < Main.player[num487].position.Y + (float)Main.player[num487].height && projectile.position.Y + (float)projectile.height > Main.player[num487].position.Y)
+            {
+                if (projectile.owner == Main.myPlayer && !Main.player[Main.myPlayer].moonLeech)
+                {
+                    int num492 = (int)projectile.ai[1];
+                    Main.player[num487].HealEffect(num492, false);
+                    Main.player[num487].statLife += num492;
+                    if (Main.player[num487].statLife > Main.player[num487].statLifeMax2)
+                    {
+                        Main.player[num487].statLife = Main.player[num487].statLifeMax2;
+                    }
+                    NetMessage.SendData(66, -1, -1, null, num487, (float)num492, 0f, 0f, 0, 0, 0);
+                }
+                projectile.Kill();
+            }
+            num491 = num488 / num491;
+            num489 *= num491;
+            num490 *= num491;
+            projectile.velocity.X = (projectile.velocity.X * 15f + num489) / 16f;
+            projectile.velocity.Y = (projectile.velocity.Y * 15f + num490) / 16f;
+
+            for (int num468 = 0; num468 < 3; num468++)
+            {
+                int num469 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 15, 0f, 0f, 100, default, 1.3f);
+                Main.dust[num469].noGravity = true;
+                Main.dust[num469].velocity *= 0f;
+            }
+        }
+    }
+}
