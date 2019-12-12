467339b3ee6dc334adebe3f00ab0e44068a5da3d
AlterraKira <kiramichellebruce@gmail.com>
2019-12-12 17:09:22 +0000

    Nychthemeron

---
+++ Items/Weapons/Rogue/Nychthemeron.cs
@@ -0,0 +1,152 @@
+using CalamityMod.Projectiles.Rogue;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Rogue
+{
+    public class Nychthemeron : RogueWeapon
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nychthemeron");
+            Tooltip.SetDefault("Throws a spiky ball that ignores gravity and summons a pair of dark and light orbs that orbit the player\n" +
+                "Once the spiky ball disappears the orbs will home in on the nearest target\n" +
+                "Stacks up to 10\n" +
+                "Stealth strikes cause all spiky balls and orbs to be thrown at once");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.width = 18;
+            item.damage = 30;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useAnimation = 20;
+            item.useStyle = 1;
+            item.useTime = 20;
+            item.knockBack = 1f;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.height = 18;
+            item.maxStack = 10;
+            item.value = Item.buyPrice(0, 20, 0, 0);
+            item.rare = 5;
+            item.shoot = ModContent.ProjectileType<NychthemeronProjectile>();
+            item.shootSpeed = 5f;
+            item.Calamity().rogue = true;
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Projectile p = Main.projectile[i];
+                if (p.type == ModContent.ProjectileType<NychthemeronProjectile>() && p.owner == player.whoAmI)
+                {
+                    p.ai[0] = 1f;
+                }
+            }
+            return true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int orbDamage = (int)(damage * 0.75f);
+
+            if (player.Calamity().StealthStrikeAvailable())
+            {
+                for (int j = 0; j < item.stack - player.ownedProjectileCounts[item.shoot]; j++)
+                {
+                    float spread = 2;
+                    int pIndex = Projectile.NewProjectile(position.X, position.Y, speedX + Main.rand.NextFloat(-spread, spread), speedY + Main.rand.NextFloat(-spread, spread), type, damage, knockBack, player.whoAmI, 0f, 1f);
+                    Projectile p = Main.projectile[pIndex];
+                    p.Calamity().stealthStrike = true;
+                    int pID = p.identity;
+
+                    CreateOrbs(position, orbDamage, knockBack, pID, player);
+                }
+            }
+            else
+            {
+                int pIndex = Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI, 0f, 1f);
+                int pID = Main.projectile[pIndex].identity;
+                
+                CreateOrbs(position, orbDamage, knockBack, pID, player);
+            }
+            return false;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[item.shoot] < item.stack && player.altFunctionUse != 2;
+        }
+
+        //public override void AddRecipes()
+        //{
+        //    ModRecipe recipe = new ModRecipe(mod);
+        //    recipe.AddIngredient(ItemID.SpikyBall, 100);
+        //    recipe.AddIngredient(ItemID.LightShard);
+        //    recipe.AddIngredient(ItemID.DarkShard);
+        //    recipe.AddIngredient(ItemID.HallowedBar, 12);
+        //    recipe.AddTile(TileID.Anvils);
+        //    recipe.SetResult(this);
+        //    recipe.AddRecipe();
+        //}
+
+        private static void CreateOrbs(Vector2 position, int damage, float knockBack, int projectileID, Player player)
+        {
+            float rotationOffset = 0f;
+
+            // Ideally new projectiles will fill in the most recently vacated spots in the pattern
+            int[] activeSlots = new int[10] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+            for (int i = 0; i < 200; i++)
+            {
+                Projectile proj = Main.projectile[i];
+                if (proj.type == ModContent.ProjectileType<NychthemeronOrb>() && proj.owner == player.whoAmI && proj.active && proj.localAI[0] == 0f && activeSlots[(int)proj.localAI[1]] == -1)
+                {
+                    activeSlots[(int)proj.localAI[1]] = i;
+                }
+            }
+
+            int pos = 0;
+            bool assignedOffset = false;
+            for (int i = 0; i < 10; i++)
+            {
+                if (activeSlots[i] != -1)
+                {
+                    rotationOffset = Main.projectile[activeSlots[i]].rotation;
+                    assignedOffset = true;
+                }
+                if (activeSlots[i] == -1 && assignedOffset)
+                {
+                    pos = i;
+                    break;
+                }
+            }
+
+            float orb1Col = 0f;
+            float orb2Col = 1f;
+
+            if (pos > 0 && pos < 5)
+            {
+                rotationOffset += MathHelper.ToRadians(45f);
+
+                orb1Col = pos % 2;
+                orb2Col = pos % 2;
+            }
+            else if (pos >= 5)
+            {
+                rotationOffset += MathHelper.ToRadians(72f);
+            }
+
+            int orb1 = Projectile.NewProjectile(position.X, position.Y, 0f, 0f, ModContent.ProjectileType<NychthemeronOrb>(), damage, knockBack, player.whoAmI, orb1Col, projectileID);
+            int orb2 = Projectile.NewProjectile(position.X, position.Y, 0f, 0f, ModContent.ProjectileType<NychthemeronOrb>(), damage, knockBack, player.whoAmI, orb2Col, projectileID);
+            Main.projectile[orb1].localAI[1] = pos;
+            Main.projectile[orb2].localAI[1] = pos;
+            Main.projectile[orb1].rotation = rotationOffset;
+            Main.projectile[orb2].rotation = rotationOffset + MathHelper.ToRadians(180f);
+        }
+    }
+}
---
+++ Projectiles/Rogue/NychthemeronOrb.cs
@@ -0,0 +1,121 @@
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class NychthemeronOrb : ModProjectile
+    {
+        public static float[] playerDists = new float[10] { 48f, 80f, 80f, 80f, 80f, 112f, 112f, 112f, 112f, 112f };
+        public static float[] rotationSpeeds = new float[10] { 0.1f, -0.075f, -0.075f, -0.075f, -0.075f, 0.05f, 0.05f, 0.05f, 0.05f, 0.05f };
+
+        private Vector2 velocity = Vector2.Zero;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nychthemeron Orb");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 600;
+            projectile.Calamity().rogue = true;
+        }
+
+        public override void AI()
+        {
+            bool isActive = false;
+            for (int i = 0; i < 200; i++)
+            {
+                Projectile p = Main.projectile[i];
+                if (p.identity == projectile.ai[1] && p.active)
+                {
+                    isActive = true;
+                }
+            }
+            if (!isActive)
+            {
+                projectile.localAI[0] = 1f;
+                projectile.penetrate = 1;
+            }
+
+            Player player = Main.player[projectile.owner];
+
+            if (projectile.localAI[0] == 0f)
+            {
+                // Orbit
+                projectile.rotation += rotationSpeeds[(int)projectile.localAI[1]];
+
+                Vector2 relativePos = new Vector2(0, -1);
+                relativePos *= playerDists[(int)projectile.localAI[1]];
+                relativePos = relativePos.RotatedBy(projectile.rotation);
+                projectile.Center = player.Center + relativePos;
+                velocity = projectile.position - projectile.oldPosition;
+            }
+            else if (projectile.localAI[0] == 1f)
+            {
+                // Follow Enemy
+                float minDist = 999f;
+                int index = 0;
+                for (int i = 0; i < Main.npc.Length; i++)
+                {
+                    NPC npc = Main.npc[i];
+                    if (!npc.friendly && !npc.townNPC && npc.active && !npc.dontTakeDamage && npc.chaseable)
+                    {
+                        float dist = (projectile.Center - npc.Center).Length();
+                        if (dist < minDist)
+                        {
+                            minDist = dist;
+                            index = i;
+                        }
+                    }
+                }
+
+                Vector2 velocityNew;
+                if (minDist < 999f)
+                {
+                    velocityNew = Main.npc[index].Center - projectile.Center;
+                    velocityNew.Normalize();
+                    projectile.velocity += velocityNew;
+                    if (projectile.velocity.Length() > 10f)
+                    {
+                        projectile.velocity.Normalize();
+                        projectile.velocity *= 10f;
+                    }
+                    velocity = projectile.velocity;
+                }
+                else
+                {
+                    projectile.velocity = velocity;
+                }
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texLight = Main.projectileTexture[projectile.type];
+            Texture2D texDark = ModContent.GetTexture("CalamityMod/Projectiles/Rogue/NychthemeronOrb2");
+            if (projectile.ai[0] == 0f)
+            {
+                spriteBatch.Draw(texLight, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, texLight.Size() / 2f, projectile.scale, SpriteEffects.None, 0f);
+            }
+            else if (projectile.ai[0] == 1f)
+            {
+                spriteBatch.Draw(texDark, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, texDark.Size() / 2f, projectile.scale, SpriteEffects.None, 0f);
+            }
+            return false;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+    }
+}
