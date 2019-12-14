7bad9a77b55fca8e374b2fcd05a0d559f3dbf686
Ozzatron <spazeglitch@gmail.com>
2019-12-13 23:14:13 -0500

    terror talons reworked and fixed

--- Projectiles/Rogue/TalonSmallProj.cs
+++ Projectiles/Rogue/TalonSmallProj.cs
@@ -1,22 +1,19 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Projectiles.Rogue
 {
     public class TalonSmallProj : ModProjectile
     {
-        private int x;
-        private double speed = 10;
-        private float startSpeedY = 0f;
-		private bool sign = true;
+        private static float SineFrequency = 0.05f;
+        private static float SineAmplitude = 0.008f;
+        private static float RotationIncrement = 0.26f;
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Talon");
+            DisplayName.SetDefault("Terror Talon");
         }
 
         public override void SetDefaults()
@@ -28,48 +25,51 @@ namespace CalamityMod.Projectiles.Rogue
             projectile.tileCollide = false;
             projectile.timeLeft = 600;
             projectile.Calamity().rogue = true;
+
+            SineFrequency = 0.15f;
+            SineAmplitude = 0.06f;
+            RotationIncrement = 0.26f;
         }
 
         public override void AI()
         {
-			if (projectile.ai[0] == 0f) //only happens once
-			{
-				if (Main.rand.NextBool(2))
-					sign = false;
-			}
+            // On-spawn effects
+            if (projectile.ai[0] == 0)
+            {
+                // Store the X and Y of the spawn velocity so it can be used for trig calculations
+                projectile.localAI[0] = projectile.velocity.X;
+                projectile.localAI[1] = projectile.velocity.Y;
+            }
+
+            // Doesn't collide with tiles for the first 2 frames
+            projectile.tileCollide = projectile.ai[0] > 2f;
+
+            // Apply fancy sine movement, then slight gravity, then cap velocity.
+            // Original velocity is reconstructed so that it can be used in the calculation
+            Vector2 originalVelocity = new Vector2(projectile.localAI[0], projectile.localAI[1]);
+            ApplySineVelocity(originalVelocity);
+            // projectile.velocity.Y += 0.08f;
+            float currentSpeed = projectile.velocity.Length();
+            float maxSpeed = 1.4f * originalVelocity.Length();
+            if (currentSpeed > maxSpeed)
+                projectile.velocity *= maxSpeed / currentSpeed;
+
+            // spawn dust
             if (Main.rand.Next(5) == 0)
             {
                 Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 85, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
             }
-            projectile.ai[0]++;
-            if (projectile.ai[0] > 2f)
-            {
-                projectile.tileCollide = true;
-            }
-            if (projectile.localAI[0] == 0f)
-            {
-                projectile.velocity.X = projectile.velocity.X + (Main.player[projectile.owner].velocity.X * 0.5f);
-                startSpeedY = projectile.velocity.Y + (Main.player[projectile.owner].velocity.Y * 0.5f);
-                projectile.velocity.Y = startSpeedY;
-            }
-            projectile.localAI[0] += 1f;
-            if (projectile.localAI[0] >= 5f)
-            {
-				if (sign == true)
-				{
-					x++;
-					speed += 0.01;
-					projectile.velocity.Y = startSpeedY + (float)(speed * Math.Sin(x / 4));
-				}
-				else
-				{
-					x++;
-					speed += 0.01;
-					projectile.velocity.Y = startSpeedY + (float)(speed * -Math.Sin(x / 4));
-				}
-            }
+
+            // freely spins instead of facing forwards
+            projectile.rotation += RotationIncrement;
             projectile.spriteDirection = projectile.direction = (projectile.velocity.X > 0).ToDirectionInt();
-            projectile.rotation = projectile.velocity.ToRotation();
+            projectile.ai[0]++;
+        }
+
+        private void ApplySineVelocity(Vector2 baseVelocity)
+        {
+            float radians = projectile.ai[1] * (-MathHelper.PiOver2 + SineFrequency * projectile.ai[0]);
+            projectile.velocity += SineAmplitude * baseVelocity.RotatedBy(radians);
         }
 
         public override void Kill(int timeLeft)
