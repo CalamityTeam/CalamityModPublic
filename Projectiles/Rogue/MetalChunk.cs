2e276c274886bcc8de5d42bfbd210a39a1cde970
Shucks <edgarjagh@gmail.com>
2019-12-16 22:20:11 -0600

    Added metal monstrosity, added a function for sticky projectiles and fixed the rarity of burning strife

---
+++ Projectiles/Rogue/MetalChunk.cs
@@ -0,0 +1,65 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class MetalChunk : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Metal Chunk"); //Metal Chungus - Shucks
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 32;
+            projectile.height = 32;
+            projectile.friendly = true;
+            projectile.Calamity().rogue = true;
+            projectile.ignoreWater = true; //Its hella heavy so ofc
+            projectile.extraUpdates = 1;
+        }
+
+        public override void AI()
+        { 
+            //Gravity 
+            projectile.velocity.Y += 0.11f;
+            if (projectile.velocity.Y > 16f)
+                projectile.velocity.Y = 16f;
+            //Rotation
+            projectile.rotation += 0.14f * projectile.direction;
+            if (projectile.Calamity().stealthStrike)
+            {
+                projectile.ai[0]++;
+                if(projectile.ai[0] >= 10f)
+                {
+                    Vector2 speed = new Vector2(Main.rand.NextFloat(-6f, 6f), Main.rand.NextFloat(-6f, 6f));
+                    Projectile.NewProjectile(projectile.Center, speed, ModContent.ProjectileType<MetalShard>(), (int)(projectile.damage * 0.3f), 0f, projectile.owner, 0f, 0f);
+                    projectile.ai[0] = 0f;
+                }
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(SoundID.NPCHit42, projectile.Center);
+            for (int i = 0; i < 3; i++)
+            {
+                Vector2 S1 = new Vector2(-projectile.velocity.X, -projectile.velocity.Y).RotatedBy(MathHelper.ToRadians(Main.rand.Next(-45, 46)));
+                int proj = Projectile.NewProjectile(projectile.Center, S1, ProjectileID.SpikyBall, (int)(projectile.damage * 0.3), 0f, projectile.owner, 0f, 0f);
+                Main.projectile[proj].timeLeft = 600;
+                Main.projectile[proj].usesLocalNPCImmunity = true;
+                Main.projectile[proj].localNPCHitCooldown = 20;
+                S1 = new Vector2(-projectile.velocity.X * 0.7f, -projectile.velocity.Y*0.7f).RotatedBy(MathHelper.ToRadians(Main.rand.Next(-45, 46)));
+                Projectile.NewProjectile(projectile.Center, S1, ModContent.ProjectileType<MetalShard>(), (int)(projectile.damage * 0.3), 0f, projectile.owner, 0f, 0f);
+            }
+            //Dust
+            for (int i = 0; i < 20; i++)
+            {
+                Dust.NewDust(projectile.Center, 1, 1, DustID.Lead, 0f, 0f, 0, default, 1.1f);
+            }
+        }
+    }
+}
--- Utilities/CalamityUtils.cs
+++ Utilities/CalamityUtils.cs
@@ -324,6 +324,130 @@ namespace CalamityMod
                 proj = x;
             }
         }
+        /// <summary>
+        /// Call this function in the ai of your projectile so it can stick to enemies, also requires ModifyHitNPCSticky to be called in ModifyHitNPC
+        /// </summary>
+        /// <param name="projectile">The projectile you're adding sticky behaviour to</param>
+        public static void StickyProjAI (Projectile projectile)
+        {
+            if (projectile.ai[0] == 1f)
+            {
+                projectile.tileCollide = false;
+                int num988 = 15;
+                bool flag54 = false;
+                bool flag55 = false;
+                projectile.localAI[0]++;
+                if (projectile.localAI[0] % 30f == 0f)
+                {
+                    flag55 = true;
+                }
+                int num989 = (int)projectile.ai[1];
+                if (projectile.localAI[0] >= (float)(60 * num988))
+                {
+                    flag54 = true;
+                }
+                else if (num989 < 0 || num989 >= 200)
+                {
+                    flag54 = true;
+                }
+                else if (Main.npc[num989].active && !Main.npc[num989].dontTakeDamage)
+                {
+                    projectile.Center = Main.npc[num989].Center - projectile.velocity * 2f;
+                    projectile.gfxOffY = Main.npc[num989].gfxOffY;
+                    if (flag55)
+                    {
+                        Main.npc[num989].HitEffect(0, 1.0);
+                    }
+                }
+                else
+                {
+                    flag54 = true;
+                }
+                if (flag54)
+                {
+                    projectile.Kill();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Call this function in ModifyHitNPC to make your projectiles stick to enemies, needs StickyProjAI to be called in the AI of the projectile
+        /// </summary>
+        /// <param name="projectile">The projectile you're giving sticky behaviour to</param>
+        /// <param name="maxStick">How many projectiles of this type can stick to one enemy</param>
+        /// <param name="constantDamage">Decides if you want the projectile to deal damage while its sticked to enemies or not</param>
+        public static void ModifyHitNPCSticky(Projectile projectile, int maxStick, bool constantDamage)
+        {
+            Rectangle myRect = new Rectangle((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height);
+            if (projectile.owner == Main.myPlayer)
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    if (Main.npc[i].active && !Main.npc[i].dontTakeDamage &&
+                        ((projectile.friendly && (!Main.npc[i].friendly || projectile.type == 318 || (Main.npc[i].type == 22 && projectile.owner < 255 && Main.player[projectile.owner].killGuide) || (Main.npc[i].type == 54 && projectile.owner < 255 && Main.player[projectile.owner].killClothier))) ||
+                        (projectile.hostile && Main.npc[i].friendly && !Main.npc[i].dontTakeDamageFromHostiles)) && (projectile.owner < 0 || Main.npc[i].immune[projectile.owner] == 0 || projectile.maxPenetrate == 1))
+                    {
+                        if (Main.npc[i].noTileCollide || !projectile.ownerHitCheck || projectile.CanHit(Main.npc[i]))
+                        {
+                            bool flag3;
+                            if (Main.npc[i].type == 414)
+                            {
+                                Rectangle rect = Main.npc[i].getRect();
+                                int num5 = 8;
+                                rect.X -= num5;
+                                rect.Y -= num5;
+                                rect.Width += num5 * 2;
+                                rect.Height += num5 * 2;
+                                flag3 = projectile.Colliding(myRect, rect);
+                            }
+                            else
+                            {
+                                flag3 = projectile.Colliding(myRect, Main.npc[i].getRect());
+                            }
+                            if (flag3)
+                            {
+                                if (Main.npc[i].reflectingProjectiles && projectile.CanReflect())
+                                {
+                                    Main.npc[i].ReflectProjectile(projectile.whoAmI);
+                                    return;
+                                }
+                                projectile.ai[0] = 1f;
+                                projectile.ai[1] = (float)i;
+                                projectile.velocity = (Main.npc[i].Center - projectile.Center) * 0.75f;
+                                projectile.netUpdate = true;
+                                if (!constantDamage)
+                                    projectile.damage = 0;
+                                Point[] array2 = new Point[maxStick];
+                                int num29 = 0;
+                                for (int l = 0; l < 1000; l++)
+                                {
+                                    if (l != projectile.whoAmI && Main.projectile[l].active && Main.projectile[l].owner == Main.myPlayer && Main.projectile[l].type == projectile.type && Main.projectile[l].ai[0] == 1f && Main.projectile[l].ai[1] == (float)i)
+                                    {
+                                        array2[num29++] = new Point(l, Main.projectile[l].timeLeft);
+                                        if (num29 >= array2.Length)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (num29 >= array2.Length)
+                                {
+                                    int num30 = 0;
+                                    for (int m = 1; m < array2.Length; m++)
+                                    {
+                                        if (array2[m].Y < array2[num30].Y)
+                                        {
+                                            num30 = m;
+                                        }
+                                    }
+                                    Main.projectile[array2[num30].X].Kill();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         #endregion
 
         #region Tile Utilities
