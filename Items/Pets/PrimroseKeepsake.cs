080140d470508c71df227a0cfb413b8d7e0b3e21
Ozzatron <spazeglitch@gmail.com>
2019-12-23 18:50:30 -0500

    Added combination Kendra/Bear item

---
+++ Items/Pets/PrimroseKeepsake.cs
@@ -0,0 +1,62 @@
+﻿using CalamityMod.Buffs.Pets;
+using CalamityMod.Projectiles.Pets;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Pets
+{
+    public class PrimroseKeepsake : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Primrose Keepsake");
+            Tooltip.SetDefault("Did they just...");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 0;
+            item.useStyle = 1;
+            item.useAnimation = 20;
+            item.useTime = 20;
+            item.noMelee = true;
+            item.width = 30;
+            item.height = 30;
+            item.value = Item.sellPrice(1, 0, 0, 0);
+            item.shoot = 0; // neither kendra nor bear is the direct "shoot"
+            item.buffType = ModContent.BuffType<FurtasticDuoBuff>();
+            item.rare = 6;
+            item.UseSound = SoundID.Item44;
+        }
+
+        public override void UseStyle(Player player)
+        {
+            if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+            {
+                player.AddBuff(item.buffType, 15, true);
+            }
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            List<int> pets = new List<int> { ModContent.ProjectileType<Bear>(), ModContent.ProjectileType<KendraPet>() };
+            foreach(int petProjID in pets)
+                Projectile.NewProjectile(position, new Vector2(speedX, speedY), petProjID, damage, knockBack, player.whoAmI);
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<BearEye>());
+            recipe.AddIngredient(ModContent.ItemType<RomajedaOrchid>());
+            recipe.AddIngredient(ItemID.LovePotion);
+            recipe.AddTile(TileID.TinkerersWorkbench);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
