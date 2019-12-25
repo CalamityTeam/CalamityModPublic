d6554ca3c0273d67abc638e70f107872dcb53fbb
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2019-12-25 18:30:45 -0500

    wulfrum droid and galileo dust effects

---
+++ Buffs/Summon/WulfrumDroidBuff.cs
@@ -0,0 +1,36 @@
+using CalamityMod.CalPlayer;
+using CalamityMod.Projectiles.Summon;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Buffs.Summon
+{
+    public class WulfrumDroidBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Wulfrum Droid");
+            Description.SetDefault("The wulfrum droid will protect you");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            CalamityPlayer modPlayer = player.Calamity();
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<WulfrumDroid>()] > 0)
+            {
+                modPlayer.wDroid = true;
+            }
+            if (!modPlayer.wDroid)
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+            else
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+        }
+    }
+}
