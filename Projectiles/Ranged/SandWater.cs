f289ac8f287dd997dde0a32d842f93b591be7d88
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-01-03 14:14:46 -0500

    a

--- Projectiles/Ranged/SandWater.cs
+++ Projectiles/Ranged/SandWater.cs
@@ -47,14 +47,14 @@ namespace CalamityMod.Projectiles.Ranged
                 num463 *= num466;
                 num464 *= num466;
                 int num467 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 33, 0f, 0f, 100, default, 0.6f);
-                Dust dust = Main.dust[num467];
-                dust.noGravity = true;
-                dust.position.X = projectile.Center.X;
-                dust.position.Y = projectile.Center.Y;
-                dust.position.X += (float)Main.rand.Next(-10, 11);
-                dust.position.Y += (float)Main.rand.Next(-10, 11);
-                dust.velocity.X = num463;
-                dust.velocity.Y = num464;
+                Dust dust2 = Main.dust[num467];
+                dust2.noGravity = true;
+                dust2.position.X = projectile.Center.X;
+                dust2.position.Y = projectile.Center.Y;
+                dust2.position.X += (float)Main.rand.Next(-10, 11);
+                dust2.position.Y += (float)Main.rand.Next(-10, 11);
+                dust2.velocity.X = num463;
+                dust2.velocity.Y = num464;
             }
         }
     }
