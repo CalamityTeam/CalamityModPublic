f603fb5167f3a045a72df25bfb30964f396c7974
Amber Sienna <sienna@amberino.co.uk>
2020-01-04 18:34:09 +0000

    Sentinel masks

---
+++ Items/Armor/Vanity/CeaselessVoidMask.cs
@@ -0,0 +1,26 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Armor.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    class CeaselessVoidMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ceaseless Void Mask");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 20;
+            item.rare = 1;
+            item.vanity = true;
+        }
+
+        public override bool DrawHead()
+        {
+            return false;
+        }
+    }
+}
---
+++ Items/Armor/Vanity/SignusMask.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Armor.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    class SignusMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Signus Mask");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 20;
+            item.rare = 1;
+            item.vanity = true;
+        }
+
+        public override bool DrawHead()
+        {
+            return false;
+        }
+    }
+}
+
