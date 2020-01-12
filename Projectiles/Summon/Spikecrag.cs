29dafe317b8413acb6a7962f68c041b53c5e6b0d
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-01-11 20:48:23 -0500

    fixed minion targetting on certain sentries

--- Projectiles/Summon/Spikecrag.cs
+++ Projectiles/Summon/Spikecrag.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 namespace CalamityMod.Projectiles.Summon
 {
@@ -11,6 +12,7 @@ namespace CalamityMod.Projectiles.Summon
         {
             DisplayName.SetDefault("Spikecrag");
             Main.projFrames[projectile.type] = 4;
+            ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
         }
 
         public override void SetDefaults()
