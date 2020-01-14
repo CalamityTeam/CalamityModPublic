7f877765ebfc5f59f3cb4983ea3a3bb991d59635
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-01-13 23:27:18 -0500

    added seeking scorcher

---
+++ Items/Weapons/Melee/DivineHatchet.cs
@@ -0,0 +1,50 @@
+using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.Melee;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Melee
+{
+    public class DivineHatchet : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Seeking Scorcher");
+            Tooltip.SetDefault("May your enemies burn in hell for the sins they have committed\n" +
+			"Throws a holy boomerang that seeks out up to three enemies before returning to the player");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 60;
+            item.damage = 500;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useStyle = 1;
+            item.useTime = 15;
+            item.knockBack = 8.5f;
+            item.UseSound = SoundID.Item1;
+            item.melee = true;
+            item.height = 62;
+            item.value = Item.buyPrice(1, 40, 0, 0);
+            item.rare = 8;
+            item.shoot = ModContent.ProjectileType<DivineHatchetBoomerang>();
+            item.shootSpeed = 14f;
+            item.Calamity().postMoonLordRarity = 13;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.PossessedHatchet);
+            recipe.AddIngredient(ModContent.ItemType<DivineGeode>(), 5);
+            recipe.AddIngredient(ModContent.ItemType<UeliaceBar>(), 9);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
