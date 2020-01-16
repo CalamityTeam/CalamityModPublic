350ce701ddaa702f737e90fb8a18ed779a4d9067
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-01-15 20:54:49 -0500

    ice barrage/hypothermia -> ice list, terra disk -> nature list

--- CalamityMod.cs
+++ CalamityMod.cs
@@ -925,6 +925,8 @@ namespace CalamityMod
                 ModContent.ItemType<FrostcrushValari>(),
                 ModContent.ItemType<Endogenesis>(),
                 ModContent.ItemType<FlurrystormCannon>(),
+                ModContent.ItemType<Hypothermia>(),
+                ModContent.ItemType<IceBarrage>(),
 				//Cryonic Bar set stuff, could potentially be removed
                 ModContent.ItemType<Trinity>(),
                 ModContent.ItemType<Shimmerspark>(),
@@ -1046,7 +1048,9 @@ namespace CalamityMod
                 ModContent.ItemType<PoisonPack>(),
                 ModContent.ItemType<PlantationStaff>(),
                 ModContent.ItemType<SeasSearing>(),
-                ModContent.ItemType<YateveoBloom>()
+                ModContent.ItemType<YateveoBloom>(),
+                ModContent.ItemType<TerraDisk>(),
+                ModContent.ItemType<TerraDiskMelee>()
             };
 
             alcoholList = new List<int>()
