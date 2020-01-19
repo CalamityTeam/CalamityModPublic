2ec27f28fa2c6739a0739446d123d685ecc15057
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-01-18 21:31:57 -0500

    attempted destroyer fix

--- Items/DifficultyItems/Armageddon.cs
+++ Items/DifficultyItems/Armageddon.cs
@@ -45,7 +45,8 @@ namespace CalamityMod.Items.DifficultyItems
             for (int doom = 0; doom < 200; doom++)
             {
                 if ((Main.npc[doom].active && (Main.npc[doom].boss || Main.npc[doom].type == NPCID.EaterofWorldsHead || Main.npc[doom].type == NPCID.EaterofWorldsTail || Main.npc[doom].type == ModContent.NPCType<SlimeGodRun>() ||
-                    Main.npc[doom].type == ModContent.NPCType<SlimeGodRunSplit>() || Main.npc[doom].type == ModContent.NPCType<SlimeGod>() || Main.npc[doom].type == ModContent.NPCType<SlimeGodSplit>())) || CalamityWorld.DoGSecondStageCountdown > 0)
+                    Main.npc[doom].type == ModContent.NPCType<SlimeGodRunSplit>() || Main.npc[doom].type == ModContent.NPCType<SlimeGod>() || Main.npc[doom].type == ModContent.NPCType<SlimeGodSplit>())) || CalamityWorld.DoGSecondStageCountdown > 0 || 
+					Main.npc[doom].type == NPCID.TheDestroyerBody || Main.npc[doom].type == NPCID.TheDestroyerTail)
                 {
                     player.KillMe(PlayerDeathReason.ByCustomReason(player.name + " tried to change the rules."), 1000.0, 0, false);
                     Main.npc[doom].active = false;
