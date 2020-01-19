b2e61987b8a2e170905351bd7d071c42f2e63657
Amber Sienna <sienna@amberino.co.uk>
2020-01-19 10:45:49 +0000

    Ultra liquidator and Everglade spray recipe adjustments / liquidator ichor duration adjustment

--- Items/Weapons/Magic/EvergladeSpray.cs
+++ Items/Weapons/Magic/EvergladeSpray.cs
@@ -37,7 +37,7 @@ namespace CalamityMod.Items.Weapons.Magic
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.Ichor, 20);
+            recipe.AddIngredient(ItemID.GoldenShower);
             recipe.AddIngredient(ModContent.ItemType<DraedonBar>(), 3);
             recipe.AddTile(TileID.Bookcases);
             recipe.SetResult(this);
