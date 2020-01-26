906d5126816100fc12cc19b3344f1a411a7d15dc
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-01-26 16:57:03 -0500

    another vivid tweak

--- Projectiles/Magic/VividLaser2.cs
+++ Projectiles/Magic/VividLaser2.cs
@@ -28,10 +28,11 @@ namespace CalamityMod.Projectiles.Magic
         public override void AI()
         {
             Vector2 value7 = new Vector2(5f, 10f);
+            projectile.ai[1] += 1f;
             for (int dust = 0; dust < 2; dust++)
             {
 				Vector2 value8 = Vector2.UnitX * -12f;
-				value8 = -Vector2.UnitY.RotatedBy((double)(projectile.ai[0] * 0.1308997f + (float)dust * 3.14159274f), default) * value7 - projectile.rotation.ToRotationVector2() * 10f;
+				value8 = -Vector2.UnitY.RotatedBy((double)(projectile.ai[1] * 0.1308997f + (float)dust * 3.14159274f), default) * value7 - projectile.rotation.ToRotationVector2() * 10f;
 				int num42 = Dust.NewDust(projectile.Center, 0, 0, 66, 0f, 0f, 160, new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB), 1f);
 				Main.dust[num42].scale = 0.75f;
 				Main.dust[num42].noGravity = true;
