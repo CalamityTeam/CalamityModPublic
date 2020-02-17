5ac8b5b32fd5b31431070dab73650cd19f8ea9d1
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-02-16 19:01:00 -0500

    stat meter no longer vores smol resolution

--- Items/Accessories/StatMeter.cs
+++ Items/Accessories/StatMeter.cs
@@ -26,7 +26,9 @@ namespace CalamityMod.Items.Accessories
 
 		public override void ModifyTooltips(List<TooltipLine> list)
 		{
-			CalamityPlayer modPlayer = Main.player[Main.myPlayer].Calamity();
+			Player player = Main.player[Main.myPlayer];
+			CalamityPlayer modPlayer = player.Calamity();
+			Item heldItem = player.inventory[player.selectedItem];
 			int defense = modPlayer.defenseStat;
 			int DR = modPlayer.DRStat;
 			int meleeSpeed = modPlayer.meleeSpeedStat;
@@ -42,26 +44,36 @@ namespace CalamityMod.Items.Accessories
 			int moveSpeed = modPlayer.moveSpeedStat;
 			int lightLevel = modPlayer.abyssLightLevelStat;
 			int breathLossRate = modPlayer.abyssBreathLossRateStat;
+			bool melee = heldItem.melee;
+			bool ranged = heldItem.ranged;
+			bool magic = heldItem.magic;
+			bool summon = heldItem.summon;
+			bool rogue = heldItem.Calamity().rogue;
+
+			string meleeString = "Melee Damage: " + modPlayer.damageStats[0] + "% | Melee Crit Chance: " + modPlayer.critStats[0] + "%\n" +
+							"Melee Speed Boost: " + meleeSpeed + "%\n\n";
+			string rangedString = "Ranged Damage: " + modPlayer.damageStats[1] + "% | Ranged Crit Chance: " + modPlayer.critStats[1] + "%\n" +
+							"Ammo Consumption Chance: " + modPlayer.ammoReductionRanged + "%\n\n";
+			string magicString = "Magic Damage: " + modPlayer.damageStats[2] + "% | Magic Crit Chance: " + modPlayer.critStats[2] + "%\n" +
+							"Mana Usage: " + manaCost + "% | Mana Regen: " + manaRegen + "\n\n";
+			string summonString = "Minion Damage: " + modPlayer.damageStats[3] + "% | Minion Slots: " + minionSlots + "\n\n";
+			string rogueString = "Rogue Damage: " + modPlayer.damageStats[4] + "% | Rogue Crit Chance: " + modPlayer.critStats[3] + "%\n" +
+							"Rogue Velocity Boost: " + rogueVelocity + "% | Rogue Weapon Consumption Chance: " + modPlayer.ammoReductionRogue + "%\n\n";
+			string defaultString = "";
 
 			if (CalamityWorld.revenge && CalamityMod.CalamityConfig.AdrenalineAndRage)
 			{
 				int adrenalineChargeTime = modPlayer.adrenalineChargeStat;
 				int rageDamage = modPlayer.rageDamageStat;
+				string ripperString = "Adrenaline Charge Time: " + adrenalineChargeTime + " seconds | Rage Damage Boost: " + rageDamage + "%\n\n";
 
 				foreach (TooltipLine line2 in list)
 				{
 					if (line2.mod == "Terraria" && line2.Name == "Tooltip0")
 					{
-						line2.text = "Adrenaline Charge Time: " + adrenalineChargeTime + " seconds | Rage Damage Boost: " + rageDamage + "%\n\n" +
-							"Melee Damage: " + modPlayer.damageStats[0] + "% | Melee Crit Chance: " + modPlayer.critStats[0] + "%\n" +
-							"Melee Speed Boost: " + meleeSpeed + "%\n\n" +
-							"Ranged Damage: " + modPlayer.damageStats[1] + "% | Ranged Crit Chance: " + modPlayer.critStats[1] + "%\n" +
-							"Ammo Consumption Chance: " + modPlayer.ammoReductionRanged + "%\n\n" +
-							"Magic Damage: " + modPlayer.damageStats[2] + "% | Magic Crit Chance: " + modPlayer.critStats[2] + "%\n" +
-							"Mana Usage: " + manaCost + "% | Mana Regen: " + manaRegen + "\n\n" +
-							"Minion Damage: " + modPlayer.damageStats[3] + "% | Minion Slots: " + minionSlots + "\n\n" +
-							"Rogue Damage: " + modPlayer.damageStats[4] + "% | Rogue Crit Chance: " + modPlayer.critStats[3] + "%\n" +
-							"Rogue Velocity Boost: " + rogueVelocity + "% | Rogue Weapon Consumption Chance: " + modPlayer.ammoReductionRogue + "%\n\n" +
+						line2.text = "Displays info about most of your stats\n\n" +
+							ripperString +
+							(melee ? meleeString : (ranged ? rangedString : (magic ? magicString : (summon ? summonString : (rogue ? rogueString : defaultString))))) +
 							"Defense: " + defense + " | DR: " + DR + "%\n" +
 							"Life Regen: " + lifeRegen + " | Armor Penetration: " + armorPenetration + "\n" +
 							"Wing Flight Time: " + wingFlightTime + " | Movement Speed Boost: " + moveSpeed + "%\n\n" +
@@ -83,15 +95,8 @@ namespace CalamityMod.Items.Accessories
 				{
 					if (line2.mod == "Terraria" && line2.Name == "Tooltip0")
 					{
-						line2.text = "Melee Damage: " + modPlayer.damageStats[0] + "% | Melee Crit Chance: " + modPlayer.critStats[0] + "%\n" +
-							"Melee Speed Boost: " + meleeSpeed + "%\n\n" +
-							"Ranged Damage: " + modPlayer.damageStats[1] + "% | Ranged Crit Chance: " + modPlayer.critStats[1] + "%\n\n" +
-							"Ammo Consumption Chance: " + modPlayer.ammoReductionRanged + "%\n" +
-							"Magic Damage: " + modPlayer.damageStats[2] + "% | Magic Crit Chance: " + modPlayer.critStats[2] + "%\n" +
-							"Mana Usage: " + manaCost + "% | Mana Regen: " + manaRegen + "\n\n" +
-							"Minion Damage: " + modPlayer.damageStats[3] + "% | Minion Slots: " + minionSlots + "\n\n" +
-							"Rogue Damage: " + modPlayer.damageStats[4] + "% | Rogue Crit Chance: " + modPlayer.critStats[3] + "%\n" +
-							"Rogue Velocity Boost: " + rogueVelocity + "% | Rogue Weapon Consumption Chance: " + modPlayer.ammoReductionRogue + "%\n\n" +
+						line2.text = "Displays info about most of your stats\n\n" +
+							(melee ? meleeString : (ranged ? rangedString : (magic ? magicString : (summon ? summonString : (rogue ? rogueString : defaultString))))) +
 							"Defense: " + defense + " | DR: " + DR + "%\n" +
 							"Life Regen: " + lifeRegen + " | Armor Penetration: " + armorPenetration + "\n" +
 							"Wing Flight Time: " + wingFlightTime + " | Movement Speed Boost: " + moveSpeed + "%\n\n" +
