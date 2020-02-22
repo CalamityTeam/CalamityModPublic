b741197a7d296b5c5373829f092c8ddeb8a8e759
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-02-21 20:50:02 -0500

    removed a bit of unnecessary code

--- Projectiles/Summon/SandElementalMinion.cs
+++ Projectiles/Summon/SandElementalMinion.cs
@@ -276,8 +276,7 @@ namespace CalamityMod.Projectiles.Summon
                         Vector2 value19 = vector46 - projectile.Center;
                         value19.Normalize();
                         value19 *= scaleFactor3;
-                        int num659 = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, value19.X, value19.Y, num658, projectile.damage, 0f, Main.myPlayer, 0f, 0f);
-                        Main.projectile[num659].timeLeft = 300;
+                        Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, value19.X, value19.Y, num658, projectile.damage, 0f, Main.myPlayer, 0f, 0f);
                         projectile.netUpdate = true;
                     }
                 }
