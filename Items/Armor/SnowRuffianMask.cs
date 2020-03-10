e2f8915a5e8c19e72b3fae3837ef9ddc7f6064bf
Amber Sienna <sienna@amberino.co.uk>
2020-03-10 15:31:48 +0000

    Updated all rogue armour tooltips to reflect the ability to gain stealth whilst moving.

--- Items/Armor/SnowRuffianMask.cs
+++ Items/Armor/SnowRuffianMask.cs
@@ -38,7 +38,7 @@ namespace CalamityMod.Items.Armor
             modPlayer.rogueStealthMax += 0.5f;
             player.setBonus = "5% increased rogue damage\n" +
                 "You can glide to negate fall damage\n" +
-                "Rogue stealth builds while not attacking and not moving, up to a max of 50\n" +
+                "Rogue stealth builds while not attacking and slower while moving, up to a max of 50\n" +
                 "Once you have built max stealth, you will be able to perform a Stealth Strike\n" +
                 "Rogue stealth only reduces when you attack, it does not reduce while moving\n" +
                 "The higher your rogue stealth the higher your rogue damage, crit, and movement speed";
