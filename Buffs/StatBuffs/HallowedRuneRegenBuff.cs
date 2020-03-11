51551ad6db8bafd6f33e9d5f8045f9e8efb54e44
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-03-11 14:31:31 -0400

    hallowed rune fix

--- Buffs/StatBuffs/HallowedRuneRegenBuff.cs
+++ Buffs/StatBuffs/HallowedRuneRegenBuff.cs
@@ -18,7 +18,7 @@ namespace CalamityMod.Buffs.StatBuffs
 
         public override void Update(Player player, ref int buffIndex)
         {
-            player.Calamity().sRegen = true;
+            player.Calamity().hallowedRegen = true;
         }
     }
 }
