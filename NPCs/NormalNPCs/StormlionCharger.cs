8a3ab347d00eb55f22206b15463ee5c6d8989a97
Amber Sienna <sienna@amberino.co.uk>
2020-03-16 17:43:10 +0000

    minor drop tweak

--- NPCs/NormalNPCs/StormlionCharger.cs
+++ NPCs/NormalNPCs/StormlionCharger.cs
@@ -64,9 +64,8 @@ namespace CalamityMod.NPCs.NormalNPCs
         public override void NPCLoot()
         {
             DropHelper.DropItemChance(npc, ModContent.ItemType<StormjawStaff>(), 0.2f, 1, 1);
-			//100% chance if DS isn't dead yet, otherwise 50%
-            DropHelper.DropItemCondition(npc, ModContent.ItemType<StormlionMandible>(), !CalamityWorld.downedDesertScourge, 1f, 1, 1);
-            DropHelper.DropItemCondition(npc, ModContent.ItemType<StormlionMandible>(), CalamityWorld.downedDesertScourge, 0.5f, 1, 1);
+            //100% chance if DS isn't dead yet, otherwise 50%
+            DropHelper.DropItemChance(npc, ModContent.ItemType<StormlionMandible>(), !CalamityWorld.downedDesertScourge ? 1f : 0.5f, 1, 1);
         }
     }
 }
