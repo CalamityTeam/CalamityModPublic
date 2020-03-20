c146cd5fed7c55b25dc178c5e3bcafeed1dbd36e
Mishiro Usui <MishiroUsuiDev@users.noreply.github.com>
2020-03-20 11:15:13 +0000

    Fixed drop condition for RE ammo on old duke

--- NPCs/OldDuke/OldDuke.cs
+++ NPCs/OldDuke/OldDuke.cs
@@ -387,7 +387,7 @@ namespace CalamityMod.NPCs.OldDuke
 
             DropHelper.DropItemChance(npc, ModContent.ItemType<OldDukeTrophy>(), 10);
             DropHelper.DropItemCondition(npc, ModContent.ItemType<KnowledgeOldDuke>(), true, !CalamityWorld.downedBoomerDuke);
-            DropHelper.DropResidentEvilAmmo(npc, CalamityWorld.downedBoomerDuke, 6, 3, 2);
+            DropHelper.DropResidentEvilAmmo(npc, !CalamityWorld.downedBoomerDuke, 6, 3, 2);
 
             // All other drops are contained in the bag, so they only drop directly on Normal
             if (!Main.expertMode)
