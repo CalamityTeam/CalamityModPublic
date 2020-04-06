eb386b1702460eb0f19b6f2f9c01cfa7143cc0f9
DominicKarma42 <51094361+DominicKarma@users.noreply.github.com>
2020-04-05 20:52:40 -0500

    Heart Rapier = Goldplume Spear iirc

--- Projectiles/Melee/Spears/HeartRapierProjectile.cs
+++ Projectiles/Melee/Spears/HeartRapierProjectile.cs
@@ -30,21 +30,6 @@ namespace CalamityMod.Projectiles.Melee.Spears
         public override float InitialSpeed => 3f;
         public override float ReelbackSpeed => 1.1f;
         public override float ForwardSpeed => 0.95f;
-        public override void ExtraBehavior()
-        {
-            if (Main.rand.NextBool(5))
-                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 12, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
-            projectile.localAI[0] += 1f;
-            if (projectile.localAI[0] >= 6f)
-            {
-                projectile.localAI[0] = 0f;
-                if (Main.myPlayer == projectile.owner)
-                {
-                    Projectile.NewProjectile(projectile.Center.X + projectile.velocity.X, projectile.Center.Y + projectile.velocity.Y,
-                        projectile.velocity.X, projectile.velocity.Y, ModContent.ProjectileType<Feather>(), (int)(projectile.damage * 0.2), 0f, projectile.owner, 0f, 0f);
-                }
-            }
-        }
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
