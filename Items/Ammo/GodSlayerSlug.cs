d71c41b04b824fdb6abeb7d3a7c47a1a3738c35a
Ozzatron <spazeglitch@gmail.com>
2020-04-11 13:30:18 -0400

    god slayer slugs (they don't work)

---
+++ Items/Ammo/GodSlayerSlug.cs
@@ -0,0 +1,44 @@
+﻿using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.Ranged;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Ammo
+{
+    public class GodSlayerSlug : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("God Slayer Slug");
+            Tooltip.SetDefault("No real effect yet");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 42;
+            item.ranged = true;
+            item.width = 22;
+            item.height = 22;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.knockBack = 3f;
+            item.value = Item.buyPrice(silver: 4);
+            item.rare = 10;
+            item.Calamity().customRarity = CalamityRarity.DarkBlue;
+            item.shoot = ModContent.ProjectileType<GodSlayerSlugMain>();
+            item.shootSpeed = 6f;
+            item.ammo = ItemID.MusketBall;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<CosmiliteBar>());
+            recipe.AddRecipeGroup("NForEE");
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this, 333);
+            recipe.AddRecipe();
+        }
+    }
+}
