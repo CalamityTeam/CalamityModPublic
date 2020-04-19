be5c2a930db6568fd4d3cd34aebba696a80d329c
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-04-18 21:27:40 -0400

    gel dart stealth strike

--- Items/Weapons/Rogue/GelDart.cs
+++ Items/Weapons/Rogue/GelDart.cs
@@ -1,5 +1,6 @@
 using CalamityMod.Items.Materials;
 using CalamityMod.Projectiles.Rogue;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,7 +12,9 @@ namespace CalamityMod.Items.Weapons.Rogue
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Gel Dart");
-            Tooltip.SetDefault("Throws bouncing darts");
+            Tooltip.SetDefault("Throws bouncing darts\n" +
+			"Stealth strikes ignore gravity and bounce more vigorously\n" +
+			"They additionally leak slime and cover enemies in dark sludge");
         }
 
         public override void SafeSetDefaults()
@@ -34,6 +37,20 @@ namespace CalamityMod.Items.Weapons.Rogue
             item.shoot = ModContent.ProjectileType<GelDartProjectile>();
             item.shootSpeed = 14f;
             item.Calamity().rogue = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (player.Calamity().StealthStrikeAvailable()) //setting the stealth strike
+            {
+                int stealth = Projectile.NewProjectile(position, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI, 0f, 0f);
+                Main.projectile[stealth].Calamity().stealthStrike = true;
+                Main.projectile[stealth].usesLocalNPCImmunity = true;
+                Main.projectile[stealth].penetrate = 6;
+                Main.projectile[stealth].aiStyle = -1;
+                return false;
+            }
+            return true;
         }
     }
 }
