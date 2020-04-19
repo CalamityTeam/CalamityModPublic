35fe011b36d0f618ad8b50c8b24f0f19dec06613
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-04-19 17:27:16 -0400

    fixes to some of the above weapons
    undid gel dart change because it broke the SS lmao

--- Projectiles/Magic/NettleRight.cs
+++ Projectiles/Magic/NettleRight.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -14,7 +16,6 @@ namespace CalamityMod.Projectiles.Magic
         {
 			projectile.width = 28;
 			projectile.height = 28;
-			projectile.aiStyle = 4;
 			projectile.friendly = true;
 			projectile.penetrate = 1;
 			projectile.tileCollide = false;
@@ -25,15 +26,37 @@ namespace CalamityMod.Projectiles.Magic
 
 		public override void AI()
 		{
+			projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + MathHelper.PiOver2;
 			if (projectile.ai[0] == 0f)
 			{
+				projectile.alpha -= 75;
+				if (projectile.alpha > 0)
+					return;
+				projectile.alpha = 0;
+				projectile.ai[0] = 1f;
 				if (Main.myPlayer == projectile.owner)
 				{
 					int Type = ModContent.ProjectileType<NettleLeft>();
 					int number = Projectile.NewProjectile(projectile.position.X + projectile.velocity.X + (float)(projectile.width / 2), projectile.position.Y + projectile.velocity.Y + (float)(projectile.height / 2), projectile.velocity.X, projectile.velocity.Y, Type, projectile.damage, projectile.knockBack, projectile.owner, 0f, 0f);
 					Main.projectile[number].ai[1] = projectile.ai[1] + 1f;
-					projectile.ai[0]++;
 				}
+			}
+			else
+			{
+				if (projectile.alpha < 170 && projectile.alpha + 5 >= 170)
+				{
+					for (int index1 = 0; index1 < 8; ++index1)
+					{
+						int index2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 7, projectile.velocity.X * 0.025f, projectile.velocity.Y * 0.025f, 200, new Color(), 1.3f);
+						Dust dust = Main.dust[index2];
+						dust.noGravity = true;
+						dust.velocity *= 0.5f;
+					}
+				}
+				projectile.alpha += 3;
+				if (projectile.alpha < 255)
+					return;
+				projectile.Kill();
 			}
 		}
     }
--- Projectiles/Melee/ThornBase.cs
+++ Projectiles/Melee/ThornBase.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -14,7 +16,6 @@ namespace CalamityMod.Projectiles.Melee
         {
 			projectile.width = 28;
 			projectile.height = 28;
-			projectile.aiStyle = 4;
 			projectile.friendly = true;
 			projectile.penetrate = 1;
 			projectile.tileCollide = false;
@@ -25,17 +26,37 @@ namespace CalamityMod.Projectiles.Melee
 
 		public override void AI()
 		{
+			projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + MathHelper.PiOver2;
 			if (projectile.ai[0] == 0f)
 			{
+				projectile.alpha -= 50;
+				if (projectile.alpha > 0)
+					return;
+				projectile.alpha = 0;
+				projectile.ai[0] = 1f;
 				if (Main.myPlayer == projectile.owner)
 				{
 					int type = projectile.type;
 					if (projectile.ai[1] >= 6f)
 						type = ModContent.ProjectileType<ThornTip>();
-					int number = Projectile.NewProjectile(projectile.position.X + projectile.velocity.X + (float)(projectile.width / 2), projectile.position.Y + projectile.velocity.Y + (float)(projectile.height / 2), projectile.velocity.X, projectile.velocity.Y, type, projectile.damage, projectile.knockBack, projectile.owner, 0.0f, 0.0f);
+					int number = Projectile.NewProjectile(projectile.position.X + projectile.velocity.X + (float)(projectile.width / 2), projectile.position.Y + projectile.velocity.Y + (float)(projectile.height / 2), projectile.velocity.X, projectile.velocity.Y, type, projectile.damage, projectile.knockBack, projectile.owner, 0f, 0f);
 					Main.projectile[number].ai[1] = projectile.ai[1] + 1f;
-					projectile.ai[0]++;
 				}
+			}
+			else
+			{
+				if (projectile.alpha < 170 && projectile.alpha + 5 >= 170)
+				{
+					for (int index = 0; index < 3; ++index)
+					{
+						Dust.NewDust(projectile.position, projectile.width, projectile.height, 18, projectile.velocity.X * 0.025f, projectile.velocity.Y * 0.025f, 170, new Color(), 1.2f);
+					}
+					Dust.NewDust(projectile.position, projectile.width, projectile.height, 14, 0f, 0f, 170, new Color(), 1.1f);
+				}
+				projectile.alpha += 3;
+				if (projectile.alpha < 255)
+					return;
+				projectile.Kill();
 			}
 		}
     }
