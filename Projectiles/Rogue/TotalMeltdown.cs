904df31e0c04e8b13ecb2531f46e2ee22db44dfe
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-04-20 13:25:50 -0400

    if someone complains about totality, I'm ripping their head off

---
+++ Projectiles/Rogue/TotalMeltdown.cs
@@ -0,0 +1,46 @@
+using CalamityMod.Dusts;
+using Microsoft.Xna.Framework;
+using System.IO;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace CalamityMod.Projectiles.Rogue
+{
+    public class TotalMeltdown : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Explosion");
+            ProjectileID.Sets.MinionShot[projectile.type] = true;
+            Main.projFrames[projectile.type] = 13;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 44;
+            projectile.height = 48;
+            projectile.friendly = true;
+            projectile.Calamity().rogue = true;
+            projectile.penetrate = -1;
+            projectile.timeLeft = Main.projFrames[projectile.type] * 5;
+            projectile.usesLocalNPCImmunity = true;
+            projectile.localNPCHitCooldown = 30;
+        }
+
+        public override void AI()
+        {
+            if (projectile.timeLeft % 5f == 4f)
+                projectile.frame++;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.OnFire, 300);
+        }
+
+        public override void OnHitPvp(Player target, int damage, bool crit)
+        {
+            target.AddBuff(BuffID.OnFire, 300);
+        }
+    }
+}
