0f893a443b10432ab571c734c2977fa2b8ed6275
DominicKarma42 <51094361+DominicKarma@users.noreply.github.com>
2020-04-23 14:22:51 -0500

    Wyvern's call recipe fix

--- Items/Weapons/Magic/WyvernsCall.cs
+++ Items/Weapons/Magic/WyvernsCall.cs
@@ -1,5 +1,4 @@
 using CalamityMod.Projectiles.Magic;
-using CalamityMod.Items.Materials;
 using Microsoft.Xna.Framework;
 using System;
 using Terraria;
@@ -120,7 +119,7 @@ Fires wyverns and colored feathers from the sky that stick to enemies and tiles 
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.SoulofFlight, 15);
-            recipe.AddIngredient(ModContent.ItemType<EnchantedMetal>(), 5);
+            recipe.AddIngredient(ItemID.HallowedBar, 5);
             recipe.AddIngredient(ModContent.ItemType<SkyGlaze>());
             recipe.AddTile(TileID.MythrilAnvil);
             recipe.SetResult(this);
