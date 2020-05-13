a74c0bd003b12a7f0e94db7d278d0340fd8ee3a7
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-05-13 12:20:16 -0400

    marksman offset adjustment

--- Items/Weapons/Ranged/MarksmanBow.cs
+++ Items/Weapons/Ranged/MarksmanBow.cs
@@ -36,7 +36,7 @@ namespace CalamityMod.Items.Weapons.Ranged
             item.Calamity().customRarity = CalamityRarity.Dedicated;
         }
 
-        public override Vector2? HoldoutOffset() => new Vector2(-8, 0);
+        public override Vector2? HoldoutOffset() => new Vector2(-4, 0);
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
