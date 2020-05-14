867729d4e0298d4e1b60534c83fcee66b488b690
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-05-14 09:56:44 -0400

    final dawn tooltip cleanup

--- Items/Weapons/Rogue/FinalDawn.cs
+++ Items/Weapons/Rogue/FinalDawn.cs
@@ -2,6 +2,7 @@ using CalamityMod.Projectiles.Rogue;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace CalamityMod.Items.Weapons.Rogue
 {
@@ -10,11 +11,11 @@ namespace CalamityMod.Items.Weapons.Rogue
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("The Final Dawn");
-			Tooltip.SetDefault("'We shall ride into the sunrise once more'\n" +
+			Tooltip.SetDefault("We shall ride into the sunrise once more\n" +
 							   "Attack enemies with a giant scythe swing to replenish stealth\n" +
 							   "Press up and attack to throw the scythe \n" +
-							   "Stealth Strikes allow you to perform a vertical swing that leaves a lingering fire aura\n" +
-							   "Stealth Strike performed while pressing up allow you to fling yourself at the enemy and slice through them, causing homing fireballs to emerge");
+							   "Stealth strikes perform a vertical swing that leaves a lingering fire aura\n" +
+							   "Stealth strikes performed while pressing up fling yourself at the enemy and slice through them, causing homing fireballs to emerge");
 		}
 		public override void SafeSetDefaults()
 		{
@@ -27,19 +28,19 @@ namespace CalamityMod.Items.Weapons.Rogue
 			item.useStyle = ItemUseStyleID.SwingThrow;
 			item.knockBack = 4;
 			item.rare = 10;
-			item.Calamity().customRarity = CalamityRarity.Dedicated ;
+			item.Calamity().customRarity = CalamityRarity.Dedicated;
 			item.value = CalamityGlobalItem.RarityDarkBlueBuyPrice;
 			item.autoReuse = false;
-			item.shoot = ModContent.ProjectileType<FinalDawnProjectile>();
+			item.shoot = ProjectileType<FinalDawnProjectile>();
 			item.shootSpeed = 1f;
 			item.useTurn = false;
             item.channel = true;
 			item.noUseGraphic = true;
 		}
 		public override bool CanUseItem(Player player) => player.ownedProjectileCounts[item.shoot] +
-			player.ownedProjectileCounts[ModContent.ProjectileType<FinalDawnFireSlash>()] +
-			player.ownedProjectileCounts[ModContent.ProjectileType<FinalDawnHorizontalSlash>()] +
-			player.ownedProjectileCounts[ModContent.ProjectileType<FinalDawnThrow>()] +
-			player.ownedProjectileCounts[ModContent.ProjectileType<FinalDawnThrow2>()] <= 0;
+			player.ownedProjectileCounts[ProjectileType<FinalDawnFireSlash>()] +
+			player.ownedProjectileCounts[ProjectileType<FinalDawnHorizontalSlash>()] +
+			player.ownedProjectileCounts[ProjectileType<FinalDawnThrow>()] +
+			player.ownedProjectileCounts[ProjectileType<FinalDawnThrow2>()] <= 0;
     }
 }
