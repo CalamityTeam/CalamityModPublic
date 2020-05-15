5289308be6e2501b90e44b2ccdabc49ae8fe4c22
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-05-15 18:42:42 -0400

    added vortexpopper to tyranny's end recipe

--- Items/Weapons/Ranged/TyrannysEnd.cs
+++ Items/Weapons/Ranged/TyrannysEnd.cs
@@ -8,56 +8,57 @@ using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Weapons.Ranged
 {
-    public class TyrannysEnd : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Tyranny's End");
-            Tooltip.SetDefault("Pierce the heart of even the most heavily-armored foe\n" +
+	public class TyrannysEnd : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Tyranny's End");
+			Tooltip.SetDefault("Pierce the heart of even the most heavily-armored foe\n" +
 				"Fires a .70 caliber sniper round that bypasses enemy defense and DR\n" +
-                "Rounds mark enemies for death and summon a swarm of additional bullets on crits");
-        }
+				"Rounds mark enemies for death and summon a swarm of additional bullets on crits");
+		}
 
-        public override void SetDefaults()
-        {
-            item.damage = 4000;
-            item.crit += 35;
-            item.knockBack = 9.5f;
-            item.ranged = true;
-            item.useTime = 55;
-            item.useAnimation = 55;
-            item.shoot = ProjectileID.BulletHighVelocity;
-            item.shootSpeed = 12f;
-            item.useAmmo = AmmoID.Bullet;
-            item.autoReuse = true;
+		public override void SetDefaults()
+		{
+			item.damage = 4000;
+			item.crit += 35;
+			item.knockBack = 9.5f;
+			item.ranged = true;
+			item.useTime = 55;
+			item.useAnimation = 55;
+			item.shoot = ProjectileID.BulletHighVelocity;
+			item.shootSpeed = 12f;
+			item.useAmmo = AmmoID.Bullet;
+			item.autoReuse = true;
 
-            item.width = 94;
-            item.height = 32;
-            item.useStyle = 5;
-            item.noMelee = true;
-            item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/LargeWeaponFire");
-            item.value = CalamityGlobalItem.Rarity15BuyPrice;
-            item.rare = 10;
-            item.Calamity().customRarity = CalamityRarity.Dedicated;
-        }
+			item.width = 94;
+			item.height = 32;
+			item.useStyle = 5;
+			item.noMelee = true;
+			item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/LargeWeaponFire");
+			item.value = CalamityGlobalItem.Rarity15BuyPrice;
+			item.rare = 10;
+			item.Calamity().customRarity = CalamityRarity.Dedicated;
+		}
 
-        public override Vector2? HoldoutOffset() => new Vector2(-10, 0);
+		public override Vector2? HoldoutOffset() => new Vector2(-10, 0);
 
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ModContent.ItemType<GoldenEagle>());
-            recipe.AddIngredient(ModContent.ItemType<AMR>());
-            recipe.AddIngredient(ModContent.ItemType<AuricBar>(), 4);
-            recipe.AddTile(ModContent.TileType<DraedonsForge>());
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Vortexpopper>());
+			recipe.AddIngredient(ModContent.ItemType<GoldenEagle>());
+			recipe.AddIngredient(ModContent.ItemType<AMR>());
+			recipe.AddIngredient(ModContent.ItemType<AuricBar>(), 4);
+			recipe.AddTile(ModContent.TileType<DraedonsForge>());
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
 
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
-            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ModContent.ProjectileType<PiercingBullet>(), damage, knockBack, player.whoAmI, 0f, 0f);
-            return false;
-        }
-    }
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ModContent.ProjectileType<PiercingBullet>(), damage, knockBack, player.whoAmI, 0f, 0f);
+			return false;
+		}
+	}
 }
