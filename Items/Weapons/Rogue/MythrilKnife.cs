98006a6ca3cf7756586d277848947a574e7e3755
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-05-17 10:46:31 -0400

    spear of destiny Stealth strike

--- Items/Weapons/Rogue/MythrilKnife.cs
+++ Items/Weapons/Rogue/MythrilKnife.cs
@@ -5,38 +5,38 @@ using Terraria.ID;
 using Terraria.ModLoader;
 namespace CalamityMod.Items.Weapons.Rogue
 {
-    public class MythrilKnife : RogueWeapon
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Mythril Knife");
-            Tooltip.SetDefault("Stealth strikes inflict a wide assortment of debuffs");
-        }
+	public class MythrilKnife : RogueWeapon
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Mythril Knife");
+			Tooltip.SetDefault("Stealth strikes inflict a wide assortment of debuffs");
+		}
 
-        public override void SafeSetDefaults()
-        {
-            item.width = 12;
-            item.damage = 40;
-            item.noMelee = true;
-            item.consumable = true;
-            item.noUseGraphic = true;
-            item.useAnimation = 10;
-            item.useStyle = 1;
-            item.useTime = 10;
-            item.knockBack = 1.75f;
-            item.UseSound = SoundID.Item1;
-            item.autoReuse = true;
-            item.height = 30;
-            item.maxStack = 999;
-            item.value = 1100;
-            item.rare = 4;
-            item.shoot = ModContent.ProjectileType<MythrilKnifeProjectile>();
-            item.shootSpeed = 12f;
-            item.Calamity().rogue = true;
-        }
+		public override void SafeSetDefaults()
+		{
+			item.width = 12;
+			item.damage = 40;
+			item.noMelee = true;
+			item.consumable = true;
+			item.noUseGraphic = true;
+			item.useAnimation = 10;
+			item.useStyle = 1;
+			item.useTime = 10;
+			item.knockBack = 1.75f;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.height = 30;
+			item.maxStack = 999;
+			item.value = 1100;
+			item.rare = 4;
+			item.shoot = ModContent.ProjectileType<MythrilKnifeProjectile>();
+			item.shootSpeed = 12f;
+			item.Calamity().rogue = true;
+		}
 
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
 			if (player.Calamity().StealthStrikeAvailable())
 			{
 				int stealth = Projectile.NewProjectile(position, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI, 0f, 0f);
@@ -44,15 +44,15 @@ namespace CalamityMod.Items.Weapons.Rogue
 				return false;
 			}
 			return true;
-        }
+		}
 
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.MythrilBar);
-            recipe.AddTile(TileID.MythrilAnvil);
-            recipe.SetResult(this, 40);
-            recipe.AddRecipe();
-        }
-    }
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.MythrilBar);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this, 40);
+			recipe.AddRecipe();
+		}
+	}
 }
--- Projectiles/Rogue/CraniumSmasherStealth.cs
+++ Projectiles/Rogue/CraniumSmasherStealth.cs
@@ -4,39 +4,39 @@ using Terraria;
 using Terraria.ModLoader;
 namespace CalamityMod.Projectiles.Rogue
 {
-    public class CraniumSmasherStealth : ModProjectile
-    {
-        public override string Texture => "CalamityMod/Projectiles/Rogue/CraniumSmasherExplosive";
+	public class CraniumSmasherStealth : ModProjectile
+	{
+		public override string Texture => "CalamityMod/Projectiles/Rogue/CraniumSmasherExplosive";
 
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Stealthy Cranium Smasher");
-        }
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Stealthy Cranium Smasher");
+		}
 
-        public override void SetDefaults()
-        {
-            projectile.width = 50;
-            projectile.height = 50;
-            projectile.friendly = true;
-            projectile.penetrate = 5;
-            projectile.timeLeft = 300;
+		public override void SetDefaults()
+		{
+			projectile.width = 50;
+			projectile.height = 50;
+			projectile.friendly = true;
+			projectile.penetrate = 5;
+			projectile.timeLeft = 300;
 			projectile.usesLocalNPCImmunity = true;
-            projectile.localNPCHitCooldown = 15;
-            projectile.tileCollide = false;
-            projectile.Calamity().rogue = true;
-        }
+			projectile.localNPCHitCooldown = 15;
+			projectile.tileCollide = false;
+			projectile.Calamity().rogue = true;
+		}
 
-        public override void AI()
-        {
-            projectile.ai[0] += 1f;
-            if (projectile.ai[0] >= 5f)
-            {
-                projectile.tileCollide = true;
-            }
-            projectile.rotation += projectile.velocity.X * 0.02f;
-            projectile.velocity.Y = projectile.velocity.Y + 0.085f;
-            projectile.velocity.X = projectile.velocity.X * 0.99f;
-        }
+		public override void AI()
+		{
+			projectile.ai[0] += 1f;
+			if (projectile.ai[0] >= 5f)
+			{
+				projectile.tileCollide = true;
+			}
+			projectile.rotation += projectile.velocity.X * 0.02f;
+			projectile.velocity.Y = projectile.velocity.Y + 0.085f;
+			projectile.velocity.X = projectile.velocity.X * 0.99f;
+		}
 
 		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
@@ -47,40 +47,40 @@ namespace CalamityMod.Projectiles.Rogue
 			}
 		}
 
-        public override void OnHitPvp(Player target, int damage, bool crit)
-        {
+		public override void OnHitPvp(Player target, int damage, bool crit)
+		{
 			if (projectile.owner == Main.myPlayer)
 			{
 				int smash = Projectile.NewProjectile(projectile.Center, Vector2.Zero, ModContent.ProjectileType<CraniumSMASH>(), (int)(projectile.damage * 0.5), projectile.knockBack, projectile.owner, 0f, 0f);
 				Main.projectile[smash].Center = projectile.Center;
 			}
-        }
+		}
 
-        public override void Kill(int timeLeft)
-        {
-            projectile.position = projectile.Center;
-            projectile.width = projectile.height = 192;
-            projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
-            projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
-            projectile.maxPenetrate = -1;
-            projectile.penetrate = -1;
-            projectile.usesLocalNPCImmunity = true;
-            projectile.localNPCHitCooldown = 10;
-            projectile.Damage();
-            Main.PlaySound(2, (int)projectile.Center.X, (int)projectile.Center.Y, 14);
+		public override void Kill(int timeLeft)
+		{
+			projectile.position = projectile.Center;
+			projectile.width = projectile.height = 192;
+			projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+			projectile.maxPenetrate = -1;
+			projectile.penetrate = -1;
+			projectile.usesLocalNPCImmunity = true;
+			projectile.localNPCHitCooldown = 10;
+			projectile.Damage();
+			Main.PlaySound(2, (int)projectile.Center.X, (int)projectile.Center.Y, 14);
 			CalamityUtils.ExplosionGores(projectile, 3);
-            for (int num194 = 0; num194 < 25; num194++)
-            {
-                int num195 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 135, 0f, 0f, 100, default, 2f);
-                Main.dust[num195].noGravity = true;
-                Main.dust[num195].velocity *= 0f;
-            }
-        }
+			for (int num194 = 0; num194 < 25; num194++)
+			{
+				int num195 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 135, 0f, 0f, 100, default, 2f);
+				Main.dust[num195].noGravity = true;
+				Main.dust[num195].velocity *= 0f;
+			}
+		}
 
 		public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
 			Texture2D tex = ModContent.GetTexture("CalamityMod/Projectiles/Rogue/CraniumSmasherGlow");
 			spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, Color.White, projectile.rotation, tex.Size() / 2, projectile.scale, SpriteEffects.None, 0f);
 		}
-    }
+	}
 }
