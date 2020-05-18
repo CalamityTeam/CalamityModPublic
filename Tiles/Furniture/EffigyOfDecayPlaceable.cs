ac3b6ec70f3a86b9d0234f8c4b31db32a1ad1999
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-05-18 18:35:46 -0400

    fixed some pieces of furniture not having a preview when being placed

--- Tiles/Furniture/EffigyOfDecayPlaceable.cs
+++ Tiles/Furniture/EffigyOfDecayPlaceable.cs
@@ -23,6 +23,7 @@ namespace CalamityMod.Tiles.Furniture
             TileObjectData.newTile.CoordinateWidth = 16;
             TileObjectData.newTile.CoordinatePadding = 2;
             TileObjectData.newTile.Origin = new Point16(0, 3);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
             TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, 2, 0);
             TileObjectData.addTile(Type);
             ModTranslation name = CreateMapEntryName();
