b4ae9b5622c8b824a5e2a59478ac16ab7fa48539
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-06-09 12:12:38 -0400

    stupid using directive

--- NPCs/NormalNPCs/Sunskater.cs
+++ NPCs/NormalNPCs/Sunskater.cs
@@ -1,6 +1,7 @@
 using CalamityMod.Buffs.DamageOverTime;
 using CalamityMod.Items.Materials;
 using CalamityMod.Items.Placeables.Banners;
+using Microsoft.Xna.Framework;
 using System;
 using System.IO;
 using Terraria;
