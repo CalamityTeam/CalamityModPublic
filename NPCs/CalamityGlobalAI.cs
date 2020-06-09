fea96d90cb5a03d6fc2dc2bbf58212a9a180c0cd
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-06-09 11:17:27 -0400

    oops

--- NPCs/CalamityGlobalAI.cs
+++ NPCs/CalamityGlobalAI.cs
@@ -21620,7 +21620,7 @@ namespace CalamityMod.NPCs
                 npc.type == NPCID.IceBat || 
                 npc.type == NPCID.Lavabat || 
                 npc.type == NPCID.GiantFlyingFox || 
-                npc.type == npc.type == ModContent.NPCType<PlaguedFlyingFox>())
+                npc.type == ModContent.NPCType<PlaguedFlyingFox>())
             {
                 maxSpeedX = 6f;
                 maxSpeedY = 2.5f;
