7fa028676e3cb7dbdd3280a482409edcd5d213d1
Cameron Ewell <spazeglitch@gmail.com>
2020-06-15 21:28:13 -0400

    rarity parallelism

--- Items/Accessories/CoreOfTheBloodGod.cs
+++ Items/Accessories/CoreOfTheBloodGod.cs
@@ -25,7 +25,7 @@ namespace CalamityMod.Items.Accessories
             item.height = 26;
             item.value = CalamityGlobalItem.Rarity14BuyPrice;
             item.expert = true;
-            item.rare = 9;
+            item.rare = 10;
             item.accessory = true;
         }
 
