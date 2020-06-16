2fcdcccd7ab27ee1210c3c8af7d005bba7e41305
Cameron Ewell <spazeglitch@gmail.com>
2020-06-15 21:27:17 -0400

    rarity parallelism

--- Items/Accessories/Wings/SoulofCryogen.cs
+++ Items/Accessories/Wings/SoulofCryogen.cs
@@ -31,7 +31,7 @@ namespace CalamityMod.Items.Accessories.Wings
             item.height = 26;
             item.value = CalamityGlobalItem.Rarity5BuyPrice;
             item.expert = true;
-            item.rare = 9;
+            item.rare = 5;
             item.accessory = true;
         }
 
