73333c57746c328590c30e57fbf11f3c7770d7b3
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-07-10 22:10:52 -0400

    nerfed plantation staff base dmg

--- Items/Weapons/Summon/PlantationStaff.cs
+++ Items/Weapons/Summon/PlantationStaff.cs
@@ -21,7 +21,7 @@ namespace CalamityMod.Items.Weapons.Summon
 
 		public override void SetDefaults()
 		{
-			item.damage = 75;
+			item.damage = 69;
 			item.mana = 10;
 			item.width = 66;
 			item.height = 70;
