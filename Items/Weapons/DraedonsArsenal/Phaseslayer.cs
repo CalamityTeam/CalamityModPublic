3e16a5aac61d394524b27dd5d8a40ed09a6f7ca9
Ozzatron <spazeglitch@gmail.com>
2020-07-22 23:14:03 -0400

    Significant behavior tweaks to Phaseslayer.

--- Items/Weapons/DraedonsArsenal/Phaseslayer.cs
+++ Items/Weapons/DraedonsArsenal/Phaseslayer.cs
@@ -8,65 +8,74 @@ using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Weapons.DraedonsArsenal
 {
-    public class Phaseslayer : ModItem
-    {
-        public const int MinDamage = 4500;
-        public const int MaxDamage = 12500;
-        public const float SmallPhaseDamageMultiplier = 0.66f;
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Phaseslayer");
-            Tooltip.SetDefault("Creates a blade that aims towards the cursor. The faster you swing the blade, the more damage it does.\n" +
-                               "Swinging quickly causes the blade to release energy forward");
-        }
-        public override void SetDefaults()
-        {
-            item.damage = MinDamage;
-            item.melee = true;
-            item.width = 26;
-            item.height = 26;
-            item.useTime = 24;
-            item.useAnimation = 24;
-            item.useStyle = ItemUseStyleID.HoldingOut;
-            item.useTurn = false;
-            item.knockBack = 7f;
+	public class Phaseslayer : ModItem
+	{
+		public const int Damage = 12500;
+		// When below this percentage of charge, the sword is small instead of big.
+		public const float SizeChargeThreshold = 0.33f;
+		public const float SmallDamageMultiplier = 0.66f;
 
-            item.noUseGraphic = true;
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Phaseslayer");
+			Tooltip.SetDefault("Wield a colossal laser blade which is controlled by the cursor\n" +
+							   "Faster swings deal more damage and release sword beams\n" +
+							   "When at low charge, the blade is smaller and weaker");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = Damage;
+			item.melee = true;
+			item.width = 26;
+			item.height = 26;
+			item.useTime = 24;
+			item.useAnimation = 24;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.useTurn = false;
+			item.knockBack = 7f;
 
-            item.value = CalamityGlobalItem.RarityVioletBuyPrice;
-            item.rare = 10;
-            item.Calamity().customRarity = CalamityRarity.RareVariant;
+			item.noUseGraphic = true;
 
-            item.UseSound = SoundID.Item1;
-            item.autoReuse = true;
+			item.value = CalamityGlobalItem.RarityVioletBuyPrice;
+			item.rare = ItemRarityID.Red;
+			item.Calamity().customRarity = CalamityRarity.RareVariant;
 
-            item.Calamity().Chargeable = true;
-            item.Calamity().ChargeMax = 250;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
 
-            item.shoot = ModContent.ProjectileType<PhaseslayerProjectile>();
+			item.Calamity().Chargeable = true;
+			item.Calamity().ChargeMax = 250;
 
-            item.channel = true;
-        }
+			item.shoot = ModContent.ProjectileType<PhaseslayerProjectile>();
+			item.channel = true;
+		}
 
-        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[item.shoot] <= 0;
+		public override bool CanUseItem(Player player) => player.ownedProjectileCounts[item.shoot] <= 0;
 
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
-            Projectile blade = Projectile.NewProjectileDirect(position, Vector2.Zero, type, damage, knockBack, player.whoAmI);
-            blade.rotation = blade.AngleTo(Main.MouseWorld);
-            return false;
-        }
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Projectile blade = Projectile.NewProjectileDirect(position, Vector2.Zero, type, damage, knockBack, player.whoAmI);
+			blade.rotation = blade.AngleTo(Main.MouseWorld);
+			return false;
+		}
 
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ModContent.ItemType<MysteriousCircuitry>(), 15);
-            recipe.AddIngredient(ModContent.ItemType<DubiousPlating>(), 15);
-            recipe.AddIngredient(ModContent.ItemType<AuricBar>(), 5);
-            recipe.AddIngredient(ItemID.RedPhasesaber);
-            recipe.AddTile(ModContent.TileType<DraedonsForge>());
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
-    }
+		public override void AddRecipes()
+		{
+			void AddIndividualRecipe(int saberID)
+			{
+				ModRecipe recipe = new ModRecipe(mod);
+				recipe.AddIngredient(ModContent.ItemType<MysteriousCircuitry>(), 15);
+				recipe.AddIngredient(ModContent.ItemType<DubiousPlating>(), 15);
+				recipe.AddIngredient(ModContent.ItemType<AuricBar>(), 5);
+				recipe.AddIngredient(saberID);
+				recipe.AddTile(ModContent.TileType<DraedonsForge>());
+				recipe.SetResult(this);
+				recipe.AddRecipe();
+			}
+			
+			int[] phasesabers = { ItemID.WhitePhasesaber, ItemID.RedPhasesaber, ItemID.GreenPhasesaber, ItemID.BluePhasesaber, ItemID.PurplePhasesaber, ItemID.YellowPhasesaber };
+			foreach (int id in phasesabers)
+				AddIndividualRecipe(id);
+		}
+	}
 }
