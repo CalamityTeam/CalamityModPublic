e6fa6167bee66f675ef11668f0aea13482a2753f
Cameron Ewell <spazeglitch@gmail.com>
2020-07-23 22:48:25 -0400

    minor recipe nitpick 2

--- Items/Weapons/DraedonsArsenal/PulseTurretRemote.cs
+++ Items/Weapons/DraedonsArsenal/PulseTurretRemote.cs
@@ -68,7 +68,7 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ModContent.ItemType<MysteriousCircuitry>(), 14);
+			recipe.AddIngredient(ModContent.ItemType<MysteriousCircuitry>(), 12);
 			recipe.AddIngredient(ModContent.ItemType<DubiousPlating>(), 18);
 			recipe.AddIngredient(ModContent.ItemType<InfectedArmorPlating>(), 5);
 			recipe.AddIngredient(ModContent.ItemType<BarofLife>(), 5);
