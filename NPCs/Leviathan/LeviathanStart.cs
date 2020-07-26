4a7fe12bfa969812f21f9a08dc49d1900c3ffa71
Andrew Marheine <marhaj04@gmail.com>
2020-07-25 21:11:23 -0500

    1 - Fixed ??? hitbox.

--- NPCs/Leviathan/LeviathanStart.cs
+++ NPCs/Leviathan/LeviathanStart.cs
@@ -93,7 +93,7 @@ namespace CalamityMod.NPCs.Leviathan
 			Vector2 vector11 = new Vector2(Main.npcTexture[npc.type].Width / 2, Main.npcTexture[npc.type].Height / 2);
 
 			Vector2 vector43 = npc.Center - Main.screenPosition;
-			vector43 -= new Vector2(texture2D15.Width, texture2D15.Height) * npc.scale / 2f;
+			vector43 -= new Vector2(texture2D15.Width, texture2D15.Height / Main.npcFrameCount[npc.type]) * npc.scale / 2f;
 			vector43 += vector11 * npc.scale + new Vector2(0f, 4f + npc.gfxOffY);
 			spriteBatch.Draw(texture2D15, vector43, npc.frame, npc.GetAlpha(lightColor), npc.rotation, vector11, npc.scale, spriteEffects, 0f);
 
