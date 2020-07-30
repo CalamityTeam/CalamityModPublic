73247abc609db7201c4d165ec4fc46d990024c22
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-07-29 20:03:51 -0400

    nerfed snake eyes and plasma rifle

--- Items/Weapons/DraedonsArsenal/SnakeEyes.cs
+++ Items/Weapons/DraedonsArsenal/SnakeEyes.cs
@@ -19,7 +19,7 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 		public override void SetDefaults()
 		{
 			item.shootSpeed = 10f;
-			item.damage = 50;
+			item.damage = 40;
 			item.mana = 12;
 			item.width = 38;
 			item.height = 24;
