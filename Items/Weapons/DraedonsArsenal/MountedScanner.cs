dbedd1d07edeb6488f6670d82c3b44733195022f
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-07-29 20:14:03 -0400

    buffed mounted scanner

--- Items/Weapons/DraedonsArsenal/MountedScanner.cs
+++ Items/Weapons/DraedonsArsenal/MountedScanner.cs
@@ -20,7 +20,7 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 			item.width = 26;
 			item.height = 26;
 			item.summon = true;
-			item.damage = 35;
+			item.damage = 41;
 			item.knockBack = 2f;
 			item.mana = 10;
 			item.useTime = item.useAnimation = 25;
