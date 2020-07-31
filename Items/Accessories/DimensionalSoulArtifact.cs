c51ceb2f9359eb1b43b0871daa4227d129a724e7
Andrew Marheine <marhaj04@gmail.com>
2020-07-31 15:31:51 -0500

    1 - Fixed github's fuckery once again, this time with the artifacts -_-

--- Items/Accessories/DimensionalSoulArtifact.cs
+++ Items/Accessories/DimensionalSoulArtifact.cs
@@ -20,13 +20,13 @@ namespace CalamityMod.Items.Accessories
 
         public override void SetDefaults()
         {
-            item.width = 28;
-            item.height = 28;
-            item.value = CalamityGlobalItem.Rarity14BuyPrice;
-            item.accessory = true;
-            item.rare = 10;
-            item.Calamity().postMoonLordRarity = 14;
-        }
+			item.width = 28;
+			item.height = 28;
+			item.accessory = true;
+			item.rare = ItemRarityID.Red;
+			item.Calamity().customRarity = CalamityRarity.DarkBlue;
+			item.value = CalamityGlobalItem.Rarity14BuyPrice;
+		}
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
@@ -37,11 +37,10 @@ namespace CalamityMod.Items.Accessories
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ModContent.ItemType<Cinderplate>(), 5);
-            recipe.AddIngredient(ModContent.ItemType<EssenceofEleum>(), 10);
-            recipe.AddIngredient(ModContent.ItemType<CosmiliteBar>(), 10);
-            recipe.AddIngredient(ModContent.ItemType<ExodiumClusterOre>(), 15);
-            recipe.AddTile(TileID.DemonAltar);
+			recipe.AddIngredient(ModContent.ItemType<ExodiumClusterOre>(), 25);
+			recipe.AddIngredient(ModContent.ItemType<Elumplate>(), 25);
+			recipe.AddIngredient(ModContent.ItemType<CosmiliteBar>(), 5);
+			recipe.AddTile(TileID.DemonAltar);
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
