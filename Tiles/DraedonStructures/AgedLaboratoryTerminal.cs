54cbdd6219ed84c9c14a658efd47fc99de8167ed
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-08-20 15:46:49 -0400

    extra lavaproof security

--- Tiles/DraedonStructures/AgedLaboratoryTerminal.cs
+++ Tiles/DraedonStructures/AgedLaboratoryTerminal.cs
@@ -23,6 +23,7 @@ namespace CalamityMod.Tiles.DraedonStructures
             TileObjectData.newTile.Origin = new Point16(0, 2);
             TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
             TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+            TileObjectData.newTile.LavaDeath = false;
 
             TileObjectData.newTile.StyleWrapLimit = 2;
             TileObjectData.newTile.StyleMultiplier = 2;
