aff579ec9d398e1d57cd2d4dc8b50d159d1e2e31
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-08-22 19:36:21 -0400

    let's not be dumb and remove a check

--- Items/Tools/CementShoes.cs
+++ Items/Tools/CementShoes.cs
@@ -4,34 +4,38 @@ using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Tools
 {
-    public class CementShoes : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Cement Shoes");
-            Tooltip.SetDefault("So heavy...\n" +
-                "Favorite this item to disable any dashes granted by equipment.");
-        }
+	public class CementShoes : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cement Shoes");
+			Tooltip.SetDefault("So heavy...\n" +
+				"Favorite this item to disable any dashes granted by equipment.");
+		}
 
-        public override void SetDefaults()
-        {
-            item.width = 32;
-            item.height = 30;
-            item.rare = 1;
-        }
+		public override void SetDefaults()
+		{
+			item.width = 32;
+			item.height = 30;
+			item.rare = 1;
+		}
 
-        public override bool CanUseItem(Player player) => false;
+		public override bool CanUseItem(Player player) => false;
 
-        public override void UpdateInventory(Player player) => player.Calamity().cementShoes = true;
+		public override void UpdateInventory(Player player)
+		{
+			if (item.favorited)
+				player.Calamity().cementShoes = true;
+		}
 
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddRecipeGroup("SiltGroup", 20);
-            recipe.AddIngredient(ItemID.StoneBlock, 20);
-            recipe.AddTile(TileID.Furnaces);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
-    }
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddRecipeGroup("SiltGroup", 20);
+			recipe.AddIngredient(ItemID.StoneBlock, 20);
+			recipe.AddTile(TileID.Furnaces);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
 }
