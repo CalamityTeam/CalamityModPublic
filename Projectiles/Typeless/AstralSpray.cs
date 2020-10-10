bda29ac960d0de99caaf8dfa6d525b6856375e51
Dominic <farfetchedking541@gmail.com>
2020-10-09 20:56:29 -0500

    Fixed Astral Spray in MP(?) (TEST THIS)

--- Projectiles/Typeless/AstralSpray.cs
+++ Projectiles/Typeless/AstralSpray.cs
@@ -24,11 +24,12 @@ namespace CalamityMod.Projectiles.Typeless
 
         public override bool PreAI()
         {
-            if (projectile.owner == Main.myPlayer/* && Main.netMode != NetmodeID.MultiplayerClient*/)
+            if (Main.netMode != NetmodeID.MultiplayerClient)
             {
                 int x = (int)(projectile.Center.X / 16f);
                 int y = (int)(projectile.Center.Y / 16f);
                 WorldGenerationMethods.ConvertToAstral(x - 1, x + 1, y - 1, y + 1);
+                NetMessage.SendTileRange(-1, x - 1, y - 1, 2, 2);
             }
             if (projectile.timeLeft > 133)
             {
