cb190952cdfa4237c17e08eac00fae2f3dd68524
Cameron Ewell <spazeglitch@gmail.com>
2020-11-15 20:14:12 -0500

    Plasma Grenade base damage 11,780 --> 17,800
    these got 7:34 on scal and need the help

--- Items/Weapons/DraedonsArsenal/PlasmaGrenade.cs
+++ Items/Weapons/DraedonsArsenal/PlasmaGrenade.cs
@@ -26,7 +26,7 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 
             item.width = 22;
             item.height = 28;
-            item.damage = 11780;
+            item.damage = 17800;
             item.noMelee = true;
             item.noUseGraphic = true;
             item.consumable = true;
