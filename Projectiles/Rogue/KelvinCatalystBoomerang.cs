c87ea1c8e41f59bb3bba058e53f3c216d63a7f22
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-11-24 13:53:52 -0500

    kelvin catalyst stealth strike

--- Projectiles/Rogue/KelvinCatalystBoomerang.cs
+++ Projectiles/Rogue/KelvinCatalystBoomerang.cs
@@ -12,6 +12,7 @@ namespace CalamityMod.Projectiles.Rogue
 	public class KelvinCatalystBoomerang : ModProjectile
 	{
 		public override string Texture => "CalamityMod/Items/Weapons/Rogue/KelvinCatalyst";
+		public int AIState = 0;
 
 		public override void SetStaticDefaults()
 		{
@@ -34,14 +35,93 @@ namespace CalamityMod.Projectiles.Rogue
 		public override void SendExtraAI(BinaryWriter writer)
 		{
 			writer.Write(projectile.localAI[0]);
+			writer.Write(AIState);
 		}
 
 		public override void ReceiveExtraAI(BinaryReader reader)
 		{
 			projectile.localAI[0] = reader.ReadSingle();
+			AIState = reader.ReadInt32();
 		}
 
 		public override void AI()
+		{
+			VisualAudioEffects();
+			BoomerangAI();
+		}
+
+		private void BoomerangAI()
+		{
+			switch (AIState)
+			{
+				case 0:
+					projectile.localAI[0] += 1f;
+					if (projectile.localAI[0] >= 75f)
+					{
+						ResetStats(projectile.Calamity().stealthStrike);
+					}
+					break;
+				case 1:
+					ReturnToPlayer();
+					break;
+				case 2:
+					// Will target the targetted NPC that minions use btw
+					projectile.ChargingMinionAI(1200f, 1500f, 2200f, 150f, 0, 40f, 9f, 4f, new Vector2(0f, -60f), 40f, 9f, true, true);
+					projectile.localAI[0] += 1f;
+					if (projectile.localAI[0] >= 120)
+					{
+						ResetStats(false);
+					}
+					break;
+			}
+		}
+
+		private void ReturnToPlayer()
+		{
+			Player player = Main.player[projectile.owner];
+			float returnSpeed = 20f;
+			float acceleration = 2f;
+			Vector2 playerVec = player.Center - projectile.Center;
+			float dist = playerVec.Length();
+
+			// Delete the projectile if it's excessively far away.
+			if (dist > 3000f)
+				projectile.Kill();
+
+			playerVec.Normalize();
+			playerVec *= returnSpeed;
+
+			// Home back in on the player.
+			if (projectile.velocity.X < playerVec.X)
+			{
+				projectile.velocity.X += acceleration;
+				if (projectile.velocity.X < 0f && playerVec.X > 0f)
+					projectile.velocity.X += acceleration;
+			}
+			else if (projectile.velocity.X > playerVec.X)
+			{
+				projectile.velocity.X -= acceleration;
+				if (projectile.velocity.X > 0f && playerVec.X < 0f)
+					projectile.velocity.X -= acceleration;
+			}
+			if (projectile.velocity.Y < playerVec.Y)
+			{
+				projectile.velocity.Y += acceleration;
+				if (projectile.velocity.Y < 0f && playerVec.Y > 0f)
+					projectile.velocity.Y += acceleration;
+			}
+			else if (projectile.velocity.Y > playerVec.Y)
+			{
+				projectile.velocity.Y -= acceleration;
+				if (projectile.velocity.Y > 0f && playerVec.Y < 0f)
+					projectile.velocity.Y -= acceleration;
+			}
+			if (Main.myPlayer == projectile.owner)
+				if (projectile.Hitbox.Intersects(player.Hitbox))
+					projectile.Kill();
+		}
+
+		private void VisualAudioEffects()
 		{
 			Lighting.AddLight(projectile.Center, Main.DiscoR * 0.3f / 255f, Main.DiscoR * 0.4f / 255f, Main.DiscoR * 0.5f / 255f);
 
@@ -51,68 +131,7 @@ namespace CalamityMod.Projectiles.Rogue
 				Main.PlaySound(SoundID.Item7, projectile.position);
 			}
 
-			if (projectile.ai[0] == 0f)
-			{
-				projectile.localAI[0] += 1f;
-				if (projectile.localAI[0] >= 75f)
-				{
-					projectile.ai[0] = 1f;
-					projectile.localAI[0] = 0f;
-					projectile.width = projectile.height = 60;
-					projectile.tileCollide = false;
-					projectile.netUpdate = true;
-				}
-			}
-			else
-			{
-				float num42 = 20f;
-				float num43 = 2f;
-				Vector2 vector2 = new Vector2(projectile.position.X + projectile.width * 0.5f, projectile.position.Y + projectile.height * 0.5f);
-				float num44 = Main.player[projectile.owner].position.X + Main.player[projectile.owner].width / 2 - vector2.X;
-				float num45 = Main.player[projectile.owner].position.Y + Main.player[projectile.owner].height / 2 - vector2.Y;
-				float num46 = (float)Math.Sqrt(num44 * num44 + num45 * num45);
-
-				if (num46 > 3000f)
-					projectile.Kill();
-
-				num46 = num42 / num46;
-				num44 *= num46;
-				num45 *= num46;
-
-				if (projectile.velocity.X < num44)
-				{
-					projectile.velocity.X = projectile.velocity.X + num43;
-					if (projectile.velocity.X < 0f && num44 > 0f)
-						projectile.velocity.X = projectile.velocity.X + num43;
-				}
-				else if (projectile.velocity.X > num44)
-				{
-					projectile.velocity.X = projectile.velocity.X - num43;
-					if (projectile.velocity.X > 0f && num44 < 0f)
-						projectile.velocity.X = projectile.velocity.X - num43;
-				}
-				if (projectile.velocity.Y < num45)
-				{
-					projectile.velocity.Y = projectile.velocity.Y + num43;
-					if (projectile.velocity.Y < 0f && num45 > 0f)
-						projectile.velocity.Y = projectile.velocity.Y + num43;
-				}
-				else if (projectile.velocity.Y > num45)
-				{
-					projectile.velocity.Y = projectile.velocity.Y - num43;
-					if (projectile.velocity.Y > 0f && num45 < 0f)
-						projectile.velocity.Y = projectile.velocity.Y - num43;
-				}
-				if (Main.myPlayer == projectile.owner)
-				{
-					Rectangle rectangle = new Rectangle((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height);
-					Rectangle value2 = new Rectangle((int)Main.player[projectile.owner].position.X, (int)Main.player[projectile.owner].position.Y, Main.player[projectile.owner].width, Main.player[projectile.owner].height);
-					if (rectangle.Intersects(value2))
-						projectile.Kill();
-				}
-			}
-
-			int dust = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 67, 0f, 0f, 100, default, 1f);
+			int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 67, 0f, 0f, 100, default, 1f);
 			Main.dust[dust].noGravity = true;
 			Main.dust[dust].velocity *= 0f;
 
@@ -121,11 +140,7 @@ namespace CalamityMod.Projectiles.Rogue
 
 		public override bool OnTileCollide(Vector2 oldVelocity)
 		{
-			projectile.ai[0] = 1f;
-			projectile.localAI[0] = 0f;
-			projectile.width = projectile.height = 60;
-			projectile.tileCollide = false;
-			projectile.netUpdate = true;
+			ResetStats(projectile.Calamity().stealthStrike);
 			if (projectile.velocity.X != oldVelocity.X)
 				projectile.velocity.X = -oldVelocity.X;
 			if (projectile.velocity.Y != oldVelocity.Y)
@@ -133,49 +148,43 @@ namespace CalamityMod.Projectiles.Rogue
 			return false;
 		}
 
+		private void ResetStats(bool chaseEnemies)
+		{
+			AIState = chaseEnemies ? 2 : 1;
+			projectile.localAI[0] = 0f;
+			projectile.width = projectile.height = 60;
+			projectile.tileCollide = false;
+			projectile.netUpdate = true;
+		}
+
 		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
 			target.AddBuff(BuffID.Frostburn, 240);
-			if (projectile.owner == Main.myPlayer && Main.player[projectile.owner].ownedProjectileCounts[ModContent.ProjectileType<KelvinCatalystStar>()] < 25)
-			{
-				float spread = 45f * 0.0174f;
-				double startAngle = Math.Atan2(projectile.velocity.X, projectile.velocity.Y) - spread / 2;
-				double deltaAngle = spread / 8f;
-				double offsetAngle;
-				int i;
-				for (i = 0; i < 4; i++)
-				{
-					offsetAngle = startAngle + deltaAngle * (i + i * i) / 2f + 32f * i;
-
-					Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, (float)(Math.Sin(offsetAngle) * 4f), (float)(Math.Cos(offsetAngle) * 4f),
-						ModContent.ProjectileType<KelvinCatalystStar>(), projectile.damage / 6, projectile.knockBack * 0.5f, projectile.owner, 0f, 0f);
-
-					Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, (float)(-Math.Sin(offsetAngle) * 4f), (float)(-Math.Cos(offsetAngle) * 4f),
-						ModContent.ProjectileType<KelvinCatalystStar>(), projectile.damage / 6, projectile.knockBack * 0.5f, projectile.owner, 0f, 0f);
-				}
-			}
-			Main.PlaySound(SoundID.Item30, projectile.position);
+			OnHitEffects();
 		}
 
 		public override void OnHitPvp(Player target, int damage, bool crit)
 		{
 			target.AddBuff(BuffID.Frostburn, 240);
-			if (projectile.owner == Main.myPlayer && Main.player[projectile.owner].ownedProjectileCounts[ModContent.ProjectileType<KelvinCatalystStar>()] < 25)
+			OnHitEffects();
+		}
+
+		private void OnHitEffects()
+		{
+			int projType = ModContent.ProjectileType<KelvinCatalystStar>();
+			if (projectile.owner == Main.myPlayer && Main.player[projectile.owner].ownedProjectileCounts[projType] < 25)
 			{
 				float spread = 45f * 0.0174f;
 				double startAngle = Math.Atan2(projectile.velocity.X, projectile.velocity.Y) - spread / 2;
 				double deltaAngle = spread / 8f;
 				double offsetAngle;
-				int i;
-				for (i = 0; i < 4; i++)
+				for (int i = 0; i < 4; i++)
 				{
 					offsetAngle = startAngle + deltaAngle * (i + i * i) / 2f + 32f * i;
 
-					Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, (float)(Math.Sin(offsetAngle) * 4f), (float)(Math.Cos(offsetAngle) * 4f),
-						ModContent.ProjectileType<KelvinCatalystStar>(), projectile.damage / 6, projectile.knockBack * 0.5f, projectile.owner, 0f, 0f);
+					Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, (float)(Math.Sin(offsetAngle) * 4f), (float)(Math.Cos(offsetAngle) * 4f), projType, projectile.damage / 6, projectile.knockBack * 0.5f, projectile.owner);
 
-					Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, (float)(-Math.Sin(offsetAngle) * 4f), (float)(-Math.Cos(offsetAngle) * 4f),
-						ModContent.ProjectileType<KelvinCatalystStar>(), projectile.damage / 6, projectile.knockBack * 0.5f, projectile.owner, 0f, 0f);
+					Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, (float)(-Math.Sin(offsetAngle) * 4f), (float)(-Math.Cos(offsetAngle) * 4f), projType, projectile.damage / 6, projectile.knockBack * 0.5f, projectile.owner);
 				}
 			}
 			Main.PlaySound(SoundID.Item30, projectile.position);
