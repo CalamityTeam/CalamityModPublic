cea6bfe05407ea5aade251972f12789910bd212e
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-11-27 11:59:01 -0500

    using directive hage

--- Projectiles/Rogue/StellarKnifeProj.cs
+++ Projectiles/Rogue/StellarKnifeProj.cs
@@ -3,6 +3,7 @@ using CalamityMod.Dusts;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.IO;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
