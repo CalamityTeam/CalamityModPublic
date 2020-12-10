b1c513c917c1367cd26538152cd03fffc164d92f
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-12-10 10:35:33 -0500

    yharim's gift code cleanup

--- Items/Accessories/YharimsGift.cs
+++ Items/Accessories/YharimsGift.cs
@@ -33,7 +33,7 @@ namespace CalamityMod.Items.Accessories
         {
             player.allDamage += 0.15f;
             player.statDefense += 30;
-            if ((double)player.velocity.X > 0 || (double)player.velocity.Y > 0 || (double)player.velocity.X < -0.1 || (double)player.velocity.Y < -0.1)
+            if (!player.StandingStill())
             {
                 dragonTimer--;
                 if (dragonTimer <= 0)
@@ -56,24 +56,7 @@ namespace CalamityMod.Items.Accessories
                 {
                     if (player.whoAmI == Main.myPlayer)
                     {
-                        for (int l = 0; l < 1; l++)
-                        {
-                            float x = player.position.X + (float)Main.rand.Next(-400, 400);
-                            float y = player.position.Y - (float)Main.rand.Next(500, 800);
-                            Vector2 vector = new Vector2(x, y);
-                            float num15 = player.position.X + (float)(player.width / 2) - vector.X;
-                            float num16 = player.position.Y + (float)(player.height / 2) - vector.Y;
-                            num15 += (float)Main.rand.Next(-100, 101);
-                            int num17 = 22;
-                            float num18 = (float)Math.Sqrt((double)(num15 * num15 + num16 * num16));
-                            num18 = (float)num17 / num18;
-                            num15 *= num18;
-                            num16 *= num18;
-                            int num19 = Projectile.NewProjectile(x, y, num15, num16, ModContent.ProjectileType<SkyFlareFriendly>(), (int)(750 * player.AverageDamage()), 9f, player.whoAmI, 0f, 0f);
-                            Main.projectile[num19].ai[1] = player.position.Y;
-                            Main.projectile[num19].hostile = false;
-                            Main.projectile[num19].friendly = true;
-                        }
+						CalamityUtils.ProjectileRain(player.Center, 400f, 100f, 500f, 800f, 22f, ModContent.ProjectileType<SkyFlareFriendly>(), (int)(750 * player.AverageDamage()), 9f, player.whoAmI);
                     }
                 }
             }
