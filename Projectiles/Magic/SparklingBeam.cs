abd1ec02d960f59debcdd61f1a690976e926f5bc
ben-tk <45545085+termigator1359@users.noreply.github.com>
2021-01-11 18:07:52 -0500

    sparkling empress no longer checks Main.time

--- Projectiles/Magic/SparklingBeam.cs
+++ Projectiles/Magic/SparklingBeam.cs
@@ -234,7 +234,7 @@ namespace CalamityMod.Projectiles.Magic
 			}
 			else {
 				// Do we still have enough mana? If not, we kill the projectile because we cannot use it anymore
-				if (Main.time % 10 < 1 && !player.CheckMana(player.ActiveItem().mana, true))
+				if (Main.player[projectile.owner].miscCounter % 10 == 0 && !player.CheckMana(player.ActiveItem().mana, true))
 				{
 					projectile.Kill();
 				}
