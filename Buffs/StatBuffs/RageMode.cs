1a1d0f0349709dbbc5203f9b628d6c8fcaec5f75
Ozzatron <spazeglitch@gmail.com>
2021-01-21 21:10:41 -0500

    Removed Rage DR because that was broken

--- Buffs/StatBuffs/RageMode.cs
+++ Buffs/StatBuffs/RageMode.cs
@@ -1,5 +1,4 @@
 using CalamityMod.CalPlayer;
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -10,7 +9,7 @@ namespace CalamityMod.Buffs.StatBuffs
         public override void SetDefaults()
         {
             DisplayName.SetDefault("Rage Mode");
-            Description.SetDefault("50% damage boost. 10% to 20% DR, increased based on how full the Rage Meter is.");
+            Description.SetDefault("50% damage boost.");
             Main.debuff[Type] = true;
             Main.pvpBuff[Type] = true;
             Main.buffNoSave[Type] = false;
@@ -28,8 +27,6 @@ namespace CalamityMod.Buffs.StatBuffs
             {
                 player.buffTime[buffIndex] = 2; // Every frame, give another frame for the buff to live.
                 mp.rageModeActive = true;
-                float rageRatio = MathHelper.Clamp(mp.rage / mp.rageMax, 0f, 1f);
-                player.endurance += MathHelper.Lerp(CalamityPlayer.MinRageDR, CalamityPlayer.MaxRageDR, rageRatio);
             }
 
             // Otherwise, Rage Mode ends instantly.
