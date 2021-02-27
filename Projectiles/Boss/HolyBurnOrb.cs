d3349fc62902ce1c3d4aa3f8d881f06d8401e4b0
Ozzatron <spazeglitch@gmail.com>
2021-02-27 17:22:56 -0500

    Made Providence's healing shots even more visible.

--- Projectiles/Boss/HolyBurnOrb.cs
+++ Projectiles/Boss/HolyBurnOrb.cs
@@ -6,9 +6,8 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
 using Terraria.DataStructures;
-using Terraria.ModLoader;
 using Terraria.ID;
-using CalamityMod.Events;
+using Terraria.ModLoader;
 
 namespace CalamityMod.Projectiles.Boss
 {
@@ -27,36 +26,36 @@ namespace CalamityMod.Projectiles.Boss
             projectile.hostile = true;
             projectile.ignoreWater = true;
             projectile.tileCollide = false;
-			projectile.alpha = 255;
+            projectile.alpha = 255;
             projectile.penetrate = 1;
             projectile.timeLeft = 200;
         }
 
         public override void AI()
         {
-			if (projectile.ai[0] < 240f)
-			{
-				projectile.ai[0] += 1f;
+            if (projectile.ai[0] < 240f)
+            {
+                projectile.ai[0] += 1f;
 
-				if (projectile.timeLeft < 160)
-					projectile.timeLeft = 160;
-			}
+                if (projectile.timeLeft < 160)
+                    projectile.timeLeft = 160;
+            }
 
-			if (projectile.velocity.Length() < 16f)
-				projectile.velocity *= 1.01f;
+            if (projectile.velocity.Length() < 16f)
+                projectile.velocity *= 1.01f;
 
-			int index = Player.FindClosest(projectile.Center, projectile.width, projectile.height);
-			Player player = Main.player[index];
-			if (player is null || player.Calamity().lol)
-				return;
+            int index = Player.FindClosest(projectile.Center, projectile.width, projectile.height);
+            Player player = Main.player[index];
+            if (player is null || player.Calamity().lol)
+                return;
 
-			float playerDist = Vector2.Distance(player.Center, projectile.Center);
+            float playerDist = Vector2.Distance(player.Center, projectile.Center);
             if (playerDist < 50f && !player.dead && projectile.position.X < player.position.X + player.width && projectile.position.X + projectile.width > player.position.X && projectile.position.Y < player.Bottom.Y && projectile.Bottom.Y > player.position.Y)
             {
                 int dmgAmt = (int)projectile.ai[1];
                 player.HealEffect(dmgAmt, false);
                 player.statLife += dmgAmt;
-				player.Calamity().adrenaline = player.Calamity().stressPills ? player.Calamity().adrenaline / 2 : 0;
+                player.Calamity().adrenaline = player.Calamity().stressPills ? player.Calamity().adrenaline / 2 : 0;
                 if (player.statLife > player.statLifeMax2)
                 {
                     player.statLife = player.statLifeMax2;
@@ -70,45 +69,48 @@ namespace CalamityMod.Projectiles.Boss
             }
         }
 
-		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
-		{
-			float lerpMult = Utils.InverseLerp(15f, 30f, projectile.timeLeft, clamped: true) * Utils.InverseLerp(240f, 200f, projectile.timeLeft, clamped: true) * (1f + 0.2f * (float)Math.Cos(Main.GlobalTime % 30f / 0.5f * (MathHelper.Pi * 2f) * 3f)) * 0.8f;
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            float lerpMult = Utils.InverseLerp(15f, 30f, projectile.timeLeft, clamped: true) * Utils.InverseLerp(240f, 200f, projectile.timeLeft, clamped: true) * (1f + 0.2f * (float)Math.Cos(Main.GlobalTime % 30f / 0.5f * (MathHelper.Pi * 2f) * 3f)) * 0.8f;
 
-			Texture2D texture = Main.projectileTexture[projectile.type];
-			Vector2 drawPos = projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY);
-			Color baseColor = Main.dayTime ? new Color(255, 200, 100, 255) : new Color(100, 200, 255, 255);
-			baseColor *= 0.5f;
-			baseColor.A = 0;
-			Color colorA = baseColor;
-			Color colorB = baseColor * 0.5f;
-			colorA *= lerpMult;
-			colorB *= lerpMult;
-			Vector2 origin = texture.Size() / 2f;
-			Vector2 scale = new Vector2(0.5f, 1.5f) * lerpMult;
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            Vector2 drawPos = projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY);
+            Color baseColor = Main.dayTime ? new Color(255, 200, 100, 255) : new Color(100, 200, 255, 255);
+            baseColor *= 0.5f;
+            baseColor.A = 0;
+            Color colorA = baseColor;
+            Color colorB = baseColor * 0.5f;
+            colorA *= lerpMult;
+            colorB *= lerpMult;
+            Vector2 origin = texture.Size() / 2f;
+            Vector2 scale = new Vector2(0.5f, 1.5f) * lerpMult;
 
-			SpriteEffects spriteEffects = SpriteEffects.None;
-			if (projectile.spriteDirection == -1)
-				spriteEffects = SpriteEffects.FlipHorizontally;
+            SpriteEffects spriteEffects = SpriteEffects.None;
+            if (projectile.spriteDirection == -1)
+                spriteEffects = SpriteEffects.FlipHorizontally;
 
-			spriteBatch.Draw(texture, drawPos, null, colorA, MathHelper.PiOver2, origin, scale, spriteEffects, 0);
-			spriteBatch.Draw(texture, drawPos, null, colorA, 0f, origin, scale, spriteEffects, 0);
-			spriteBatch.Draw(texture, drawPos, null, colorB, MathHelper.PiOver2, origin, scale * 0.6f, spriteEffects, 0);
-			spriteBatch.Draw(texture, drawPos, null, colorB, 0f, origin, scale * 0.6f, spriteEffects, 0);
+            float upRight = MathHelper.PiOver4;
+            float up = MathHelper.PiOver2;
+            float upLeft = 3f * MathHelper.PiOver4;
+            float left = MathHelper.Pi;
+            spriteBatch.Draw(texture, drawPos, null, colorA, upLeft, origin, scale, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorA, upRight, origin, scale, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorB, upLeft, origin, scale * 0.6f, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorB, upRight, origin, scale * 0.6f, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorA, up, origin, scale * 0.6f, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorA, left, origin, scale * 0.6f, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorB, up, origin, scale * 0.36f, spriteEffects, 0);
+            spriteBatch.Draw(texture, drawPos, null, colorB, left, origin, scale * 0.36f, spriteEffects, 0);
 
-			spriteBatch.Draw(texture, drawPos, null, colorA, MathHelper.PiOver4, origin, scale * 0.6f, spriteEffects, 0);
-			spriteBatch.Draw(texture, drawPos, null, colorA, MathHelper.PiOver4 * 3f, origin, scale * 0.6f, spriteEffects, 0);
-			spriteBatch.Draw(texture, drawPos, null, colorB, MathHelper.PiOver4, origin, scale * 0.36f, spriteEffects, 0);
-			spriteBatch.Draw(texture, drawPos, null, colorB, MathHelper.PiOver4 * 3f, origin, scale * 0.36f, spriteEffects, 0);
+            return false;
+        }
 
-			return false;
-		}
-
-		public override void Kill(int timeLeft)
+        public override void Kill(int timeLeft)
         {
             Main.PlaySound(SoundID.Item14, projectile.Center);
-			CalamityGlobalProjectile.ExpandHitboxBy(projectile, 50);
-			int dustType = Main.dayTime ? (int)CalamityDusts.ProfanedFire : (int)CalamityDusts.Nightwither;
-			for (int d = 0; d < 5; d++)
+            CalamityGlobalProjectile.ExpandHitboxBy(projectile, 50);
+            int dustType = Main.dayTime ? (int)CalamityDusts.ProfanedFire : (int)CalamityDusts.Nightwither;
+            for (int d = 0; d < 5; d++)
             {
                 int holy = Dust.NewDust(projectile.position, projectile.width, projectile.height, dustType, 0f, 0f, 100, default, 2f);
                 Main.dust[holy].velocity *= 3f;
@@ -130,8 +132,8 @@ namespace CalamityMod.Projectiles.Boss
 
         public override void OnHitPlayer(Player target, int damage, bool crit)
         {
-			int buffType = Main.dayTime ? ModContent.BuffType<HolyFlames>() : ModContent.BuffType<Nightwither>();
-			target.AddBuff(buffType, 60);
-		}
+            int buffType = Main.dayTime ? ModContent.BuffType<HolyFlames>() : ModContent.BuffType<Nightwither>();
+            target.AddBuff(buffType, 60);
+        }
     }
 }
