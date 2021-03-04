e46fdd435776f45d2ef34794e19fc8cb46cbc4f1
Ozzatron <spazeglitch@gmail.com>
2021-03-03 21:24:40 -0500

    Finished Telluric Glare rework.

--- Items/Weapons/Ranged/TelluricGlare.cs
+++ Items/Weapons/Ranged/TelluricGlare.cs
@@ -8,50 +8,50 @@ namespace CalamityMod.Items.Weapons.Ranged
 {
     public class TelluricGlare : ModItem
     {
-        public const int RayCount = 4;
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Telluric Glare");
-            Tooltip.SetDefault($"Fires a burst of {RayCount} solar rays outward");
+            Tooltip.SetDefault("Fires volleys of four colossal radiant arrows which can pass through walls");
         }
 
         public override void SetDefaults()
         {
-            item.damage = 150;
+            item.damage = 315;
             item.ranged = true;
             item.width = 54;
             item.height = 92;
-            item.useTime = item.useAnimation = 16;
+            item.useTime = 5;
+            item.useAnimation = 20;
+            item.reuseDelay = 23;
+            item.knockBack = 7.5f;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.noMelee = true;
-            item.knockBack = 4f;
-			item.value = CalamityGlobalItem.Rarity12BuyPrice;
-			item.rare = ItemRarityID.Purple;
-			item.Calamity().customRarity = CalamityRarity.Turquoise;
-			item.UseSound = SoundID.Item5;
+
+            item.value = CalamityGlobalItem.Rarity12BuyPrice;
+            item.rare = ItemRarityID.Purple;
+            item.Calamity().customRarity = CalamityRarity.Turquoise;
+
+            item.UseSound = SoundID.Item102;
             item.autoReuse = true;
-            item.shoot = ModContent.ProjectileType<TelluricGlareProj>();
+            item.shoot = ModContent.ProjectileType<TelluricGlareArrow>();
             item.shootSpeed = 18f;
             item.useAmmo = AmmoID.Arrow;
         }
 
+        public override Vector2? HoldoutOffset() => new Vector2(-14f, 0f);
+
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            Vector2 velocity = new Vector2(speedX, speedY).SafeNormalize(Vector2.UnitX * player.direction) * 30f;
-            bool willHitTiles = Collision.CanHit(position, 0, 0, position + velocity, 0, 0);
-            for (int i = 0; i < RayCount; i++)
-            {
-                Vector2 offset = velocity.RotatedBy(MathHelper.Pi * (i - (RayCount - 1f) / 2f) * 0.18f, default);
-                
-                // Go back if the spawn position and offset spawn position have an obstacle to prevent arrows from appearing
-                // behind walls the player cannot access.
-                if (!willHitTiles)
-                    offset -= velocity;
+            // Always fires Radiant Arrows regardless of ammo used
+            type = item.shoot;
 
-                Projectile.NewProjectile(position + offset, new Vector2(speedX, speedY), item.shoot, damage, knockBack, player.whoAmI);
-            }
-
-            return false;
+            // The arrow appears from a random location "on the bow".
+            // They are also moved backwards so that they have some time to build up past positions. This helps make them not appear out of thin air.
+            Vector2 velocity = new Vector2(speedX, speedY);
+            Vector2 offset = Vector2.Normalize(velocity.RotatedBy(MathHelper.PiOver2));
+            position += offset * Main.rand.NextFloat(-19f, 19f);
+            position -= 3f * velocity;
+            return true;
         }
     }
 }
