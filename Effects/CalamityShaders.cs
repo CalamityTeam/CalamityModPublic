3e30e7376ad9d23d6f7d1f4a12712603feb50416
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2021-03-03 18:20:04 -0600

    Start of Telluric Glare rework

--- Effects/CalamityShaders.cs
+++ Effects/CalamityShaders.cs
@@ -12,7 +12,8 @@ namespace CalamityMod.Effects
         public static Effect LightShader;
         public static Effect TentacleShader;
         public static Effect LightDistortionShader;
-        
+        public static Effect FadedUVMapStreakShader;
+
         public static void LoadShaders()
         {
             if (Main.dedServ)
@@ -21,6 +22,7 @@ namespace CalamityMod.Effects
             LightShader = CalamityMod.Instance.GetEffect("Effects/LightBurstShader");
             TentacleShader = CalamityMod.Instance.GetEffect("Effects/TentacleShader");
             LightDistortionShader = CalamityMod.Instance.GetEffect("Effects/DistortionShader");
+            FadedUVMapStreakShader = CalamityMod.Instance.GetEffect("Effects/FadedUVMapStreak");
 
             Filters.Scene["CalamityMod:Astral"] = new Filter(new AstralScreenShaderData(new Ref<Effect>(AstralFogShader), "AstralPass").UseColor(0.18f, 0.08f, 0.24f), EffectPriority.VeryHigh);
 
@@ -29,6 +31,7 @@ namespace CalamityMod.Effects
 
             GameShaders.Misc["CalamityMod:SubsumingTentacle"] = new MiscShaderData(new Ref<Effect>(TentacleShader), "BurstPass");
             GameShaders.Misc["CalamityMod:LightDistortion"] = new MiscShaderData(new Ref<Effect>(LightDistortionShader), "DistortionPass");
+            GameShaders.Misc["CalamityMod:TrailStreak"] = new MiscShaderData(new Ref<Effect>(FadedUVMapStreakShader), "TrailPass");
         }
     }
 }
