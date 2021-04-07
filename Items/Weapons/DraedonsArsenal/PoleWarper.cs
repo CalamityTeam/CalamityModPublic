f901d5ff5a29842a75b7b03a4a13da3169f142c8
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2021-04-07 16:17:43 -0500

    Nerfed Pole Warper damage to 356

--- Items/Weapons/DraedonsArsenal/PoleWarper.cs
+++ Items/Weapons/DraedonsArsenal/PoleWarper.cs
@@ -22,7 +22,7 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 			CalamityGlobalItem modItem = item.Calamity();
 
 			item.shootSpeed = 10f;
-			item.damage = 367;
+			item.damage = 356;
 			item.mana = 12;
 			item.width = 38;
 			item.height = 24;
