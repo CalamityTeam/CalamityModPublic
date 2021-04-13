361f31f7f59fb22fe7bff79d4a93cd8b590830fa
Ozzatron <spazeglitch@gmail.com>
2021-04-13 00:08:39 -0400

    1 - Fixed hitbox alignment for all bullets. 2 - Improved Terra Bullet dust.

--- Projectiles/Ranged/SuperballBulletProj.cs
+++ Projectiles/Ranged/SuperballBulletProj.cs
@@ -10,13 +10,14 @@ namespace CalamityMod.Projectiles.Ranged
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Bullet");
+            DisplayName.SetDefault("Superball Bullet");
             ProjectileID.Sets.TrailCacheLength[projectile.type] = 4;
             ProjectileID.Sets.TrailingMode[projectile.type] = 0;
         }
 
         public override void SetDefaults()
         {
+            // Intentionally large bullet hitbox because this thing speeds up significantly
             projectile.width = 8;
             projectile.height = 8;
             projectile.aiStyle = 1;
@@ -65,7 +66,7 @@ namespace CalamityMod.Projectiles.Ranged
 
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
-            CalamityGlobalProjectile.DrawCenteredAndAfterimage(projectile, lightColor, ProjectileID.Sets.TrailingMode[projectile.type], 1);
+            CalamityUtils.DrawAfterimagesFromEdge(projectile, 0, lightColor);
             return false;
         }
 
