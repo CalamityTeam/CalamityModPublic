82eab17f9140ffff92138d106a3ccde014ba7842
Dominic <farfetchedking541@gmail.com>
2021-05-29 22:08:16 -0500

    Added a SW lightning flash bg effect

---
+++ Skies/StormWeaverFlashSky.cs
@@ -0,0 +1,59 @@
+using CalamityMod.NPCs.StormWeaver;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Skies
+{
+    public class StormWeaverFlashSky : CustomSky
+    {
+        public int StormWeaverHeadIndex = -1;
+
+        public override void Update(GameTime gameTime) => UpdateCryogenIndex();
+
+        public bool UpdateCryogenIndex()
+        {
+            int weaverType = ModContent.NPCType<StormWeaverHeadNaked>();
+            if (StormWeaverHeadIndex >= 0 && Main.npc[StormWeaverHeadIndex].active && Main.npc[StormWeaverHeadIndex].type == weaverType)
+                return true;
+
+            StormWeaverHeadIndex = -1;
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                if (Main.npc[i].active && Main.npc[i].type == weaverType)
+                {
+                    StormWeaverHeadIndex = i;
+                    break;
+                }
+            }
+            return StormWeaverHeadIndex != -1;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, float minDepth, float maxDepth)
+        {
+            if (maxDepth < float.MaxValue || !Main.npc.IndexInRange(StormWeaverHeadIndex))
+                return;
+
+            // Draw lightning in the background based on Main.magicPixel.
+            // It is a long, white vertical strip that exists for some reason.
+            // This lightning effect is achieved by expanding this to fit the entire background and then drawing it as a distinct element.
+            float lightningFlashPower = (Main.npc[StormWeaverHeadIndex].modNPC as StormWeaverHeadNaked).lightning;
+            Vector2 scale = new Vector2(Main.screenWidth * 1.1f / Main.magicPixel.Width, Main.screenHeight * 1.1f / Main.magicPixel.Height);
+            Vector2 screenCenter = new Vector2(Main.screenWidth, Main.screenHeight) * 0.5f;
+            Color drawColor = Color.White * MathHelper.Lerp(0f, 0.88f, lightningFlashPower);
+
+            for (int i = 0; i < 2; i++)
+                spriteBatch.Draw(Main.magicPixel, screenCenter, null, drawColor, 0f, Main.magicPixel.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+        }
+
+        public override void Reset() { }
+
+        public override void Activate(Vector2 position, params object[] args) { }
+
+        public override void Deactivate(params object[] args) { }
+
+        public override bool IsActive() => StormWeaverHeadIndex != -1 && !Main.gameMenu;
+    }
+}
