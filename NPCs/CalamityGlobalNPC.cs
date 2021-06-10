ade86040d76b29908dbb07c78407725b423b22de
Dominic <farfetchedking541@gmail.com>
2021-06-09 22:59:30 -0500

    Gave SCal a 15% resist to Corvid Harbinger and reduced damage to 97

--- NPCs/CalamityGlobalNPC.cs
+++ NPCs/CalamityGlobalNPC.cs
@@ -3875,7 +3875,8 @@ namespace CalamityMod.NPCs
 			}
 			else if (npc.type == NPCType<SupremeCalamitas.SupremeCalamitas>())
 			{
-				if (projectile.type == ProjectileType<EndoBeam>() || projectile.type == ProjectileType<RadiantResolutionOrb>() || projectile.type == ProjectileType<RadiantResolutionFire>())
+				if (projectile.type == ProjectileType<EndoBeam>() || projectile.type == ProjectileType<RadiantResolutionOrb>() || projectile.type == ProjectileType<RadiantResolutionFire>() || 
+					projectile.type == ProjectileType<PowerfulRaven>())
 				{
 					damage = (int)(damage * 0.85);
 				}
