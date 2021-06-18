cfad0e0cc3d6c5b46dcfc29c48cd4abba9b384e6
Dominic <farfetchedking541@gmail.com>
2021-06-18 03:34:24 -0500

    Supernova SS 10% damage penalty

--- Items/Weapons/Rogue/Supernova.cs
+++ Items/Weapons/Rogue/Supernova.cs
@@ -43,6 +43,7 @@ Stealth strikes release energy as they fly");
         {
             if (player.Calamity().StealthStrikeAvailable()) //setting the stealth strike
             {
+                damage = (int)(damage * 0.9f);
                 int stealth = Projectile.NewProjectile(position, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI);
 				if (stealth.WithinBounds(Main.maxProjectiles))
 					Main.projectile[stealth].Calamity().stealthStrike = true;
