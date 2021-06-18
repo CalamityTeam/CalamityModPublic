df5735cecf4e23b08405009c780999ca9d1b646b
Dominic <farfetchedking541@gmail.com>
2021-06-17 20:50:15 -0500

    Hopefully fixed a stupid GetGlobalNPC IOOB problem

--- UI/BossHealthBarManager.cs
+++ UI/BossHealthBarManager.cs
@@ -592,7 +592,7 @@ namespace CalamityMod.UI
                     return;
 
                 int currentLife = _npc.life;
-                bool enraged = _npc.Calamity().CurrentlyEnraged;
+                bool enraged = _npc.Calamity()?.CurrentlyEnraged ?? false;
 
                 // Calculate current life based all types that are available and considered part of one boss
                 if (_oneToMany)
