ac4859e6a03f9b989cadfb7c1dd35a841b1e7778
Dominic <farfetchedking541@gmail.com>
2021-06-28 21:26:31 -0500

    UI hover text

--- UI/ModeIndicatorUI.cs
+++ UI/ModeIndicatorUI.cs
@@ -24,21 +24,49 @@ namespace CalamityMod.UI
 			Texture2D armageddonIconTexture = ModContent.GetTexture("CalamityMod/ExtraTextures/UI/ModeIndicatorArma");
 			Texture2D maliceIconTexture = ModContent.GetTexture("CalamityMod/ExtraTextures/UI/ModeIndicatorMalice");
 
+			Rectangle mouseRectangle = Utils.CenteredRectangle(Main.MouseScreen, Vector2.One * 2f);
+
 			Vector2 drawCenter = new Vector2(Main.screenWidth - 400f, 72f) + outerAreaTexture.Size() * 0.5f;
 
 			spriteBatch.Draw(outerAreaTexture, drawCenter, null, Color.White, 0f, outerAreaTexture.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
 
+			Texture2D difficultyIconToUse = CalamityWorld.death ? deathIconTexture : revengeanceIconTexture;
+			bool modeIsActive = CalamityWorld.revenge;
+			bool renderingText = false;
 			if (CalamityWorld.revenge)
-			{
-				Texture2D difficultyIconToUse = CalamityWorld.death ? deathIconTexture : revengeanceIconTexture;
 				spriteBatch.Draw(difficultyIconToUse, drawCenter + DifficultyIconOffset, null, Color.White, 0f, difficultyIconToUse.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+
+			// Rev/Death active text.
+			if (mouseRectangle.Intersects(Utils.CenteredRectangle(drawCenter + DifficultyIconOffset, difficultyIconToUse.Size())))
+			{
+				Main.instance.MouseText($"{(CalamityWorld.death ? "Death" : "Revengeance")} mode is {(modeIsActive ? "active" : "not active")}.");
+				renderingText = true;
 			}
 
 			if (CalamityWorld.armageddon)
+			{
 				spriteBatch.Draw(armageddonIconTexture, drawCenter + ArmageddonIconOffset, null, Color.White, 0f, armageddonIconTexture.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+			}
+
+			// Armageddon active text.
+			if (!renderingText && mouseRectangle.Intersects(Utils.CenteredRectangle(drawCenter + ArmageddonIconOffset, armageddonIconTexture.Size())))
+			{
+				Main.instance.MouseText($"Armageddon mode is {(CalamityWorld.armageddon ? "active" : "not active")}.");
+				renderingText = true;
+			}
 
 			if (CalamityWorld.malice)
 				spriteBatch.Draw(maliceIconTexture, drawCenter + MaliceIconOffset, null, Color.White, 0f, maliceIconTexture.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+
+			// Malice active text.
+			if (!renderingText && mouseRectangle.Intersects(Utils.CenteredRectangle(drawCenter + MaliceIconOffset, armageddonIconTexture.Size())))
+			{
+				Main.instance.MouseText($"Malice mode is {(CalamityWorld.malice ? "active" : "not active")}.");
+				renderingText = true;
+			}
+
+			if (renderingText)
+				Main.instance.MouseTextHackZoom(string.Empty);
 		}
 	}
 }
