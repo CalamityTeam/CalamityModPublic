07499342d1cc1a9707823d4546cc1311e0f35bb2
Ozzatron <spazeglitch@gmail.com>
2021-07-31 01:52:50 -0400

    Chicken Cannon slight mechanics rework. Fires faster, easier to use.

--- Projectiles/Ranged/ChickenExplosion.cs
+++ Projectiles/Ranged/ChickenExplosion.cs
@@ -12,7 +12,7 @@ namespace CalamityMod.Projectiles.Ranged
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Explosion");
+            DisplayName.SetDefault("Kentucky Fried Explosion");
         }
 
         public override void SetDefaults()
