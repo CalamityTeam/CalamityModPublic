af2a2b2e1fbea9c806a6b5152ee7b41398a606df
Dominic <farfetchedking541@gmail.com>
2021-08-08 20:42:59 -0500

    Re-adding new stuff

--- Projectiles/Boss/AresGaussNukeProjectileBoom.cs
+++ Projectiles/Boss/AresGaussNukeProjectileBoom.cs
@@ -1,6 +1,7 @@
 using CalamityMod.Projectiles.BaseProjectiles;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 
 namespace CalamityMod.Projectiles.Boss
 {
@@ -23,10 +24,15 @@ namespace CalamityMod.Projectiles.Boss
 			projectile.tileCollide = false;
 			projectile.penetrate = -1;
 			projectile.timeLeft = Lifetime;
+			cooldownSlot = 1;
 		}
 
 		public override void PostAI() => Lighting.AddLight(projectile.Center, 0.2f, 0.1f, 0f);
 
 		public override bool CanHitPlayer(Player target) => CalamityUtils.CircularHitboxCollision(projectile.Center, CurrentRadius * projectile.scale * 0.5f, target.Hitbox);
+
+		public override void ModifyHitPlayer(Player target, ref int damage, ref bool crit) => target.Calamity().lastProjectileHit = projectile;
+
+		public override void OnHitPlayer(Player target, int damage, bool crit) => target.AddBuff(BuffID.OnFire, 480);
 	}
 }
