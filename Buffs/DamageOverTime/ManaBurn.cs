fa12165a57c9289c5a41433baf2f6229683202d2
Cameron Ewell <spazeglitch@gmail.com>
2021-09-12 00:27:56 -0400

    Improved Mana Burn tooltip
    I forgot to merge this as a suggestion so now it's a commit

--- Buffs/DamageOverTime/ManaBurn.cs
+++ Buffs/DamageOverTime/ManaBurn.cs
@@ -8,7 +8,7 @@ namespace CalamityMod.Buffs.DamageOverTime
         public override void SetDefaults()
         {
             DisplayName.SetDefault("Mana Burn");
-            Description.SetDefault("Volatile mana consumes you");
+            Description.SetDefault("The excess of mana sears your body and mind");
             Main.debuff[Type] = true;
             Main.pvpBuff[Type] = true;
             Main.buffNoSave[Type] = true;
