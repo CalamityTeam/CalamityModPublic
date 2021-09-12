b6117976aa655edf585e41ead1cb94afc90b8d59
Dominic <farfetchedking541@gmail.com>
2021-09-12 13:41:26 -0500

    Buffed Cosmic Viper Engine damage to 138

--- Items/Weapons/Summon/CosmicViperEngine.cs
+++ Items/Weapons/Summon/CosmicViperEngine.cs
@@ -20,7 +20,7 @@ namespace CalamityMod.Items.Weapons.Summon
 
         public override void SetDefaults()
         {
-            item.damage = 111;
+            item.damage = 138;
             item.mana = 10;
             item.width = 46;
             item.height = 28;
