f4f0ab0229f06d7786925e3ec9935ab062205aa2
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2021-10-05 10:34:58 -0500

    Statigel Dyes

---
+++ Items/Dyes/PinkStatigelDye.cs
@@ -0,0 +1,36 @@
+using CalamityMod.Items.Materials;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Graphics.Shaders;
+using Microsoft.Xna.Framework;
+
+namespace CalamityMod.Items.Dyes
+{
+    public class PinkStatigelDye : BaseDye
+    {
+        public override ArmorShaderData ShaderDataToBind => new ArmorShaderData(new Ref<Effect>(mod.GetEffect("Effects/Dyes/SlimeGodDyeShader")), "DyePass").
+            UseColor(new Color(249, 129, 185)).UseSecondaryColor(new Color(131, 58, 103)).UseImage("Images/Misc/Perlin");
+        public override void SafeSetStaticDefaults()
+        {
+            DisplayName.SetDefault("Pink Statigel Dye");
+        }
+
+		public override void SafeSetDefaults()
+		{
+            item.rare = ItemRarityID.LightRed;
+            item.value = CalamityGlobalItem.Rarity4BuyPrice;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.BottledWater, 2);
+            recipe.AddIngredient(ModContent.ItemType<PurifiedGel>());
+            recipe.AddTile(TileID.DyeVat);
+            recipe.SetResult(this, 2);
+            recipe.AddRecipe();
+        }
+    }
+}
