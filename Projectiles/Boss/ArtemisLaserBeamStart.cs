82a32f0f50c49cec9cdb533d03a5506230883548
Andrew Marheine <marhaj04@gmail.com>
2021-10-12 17:57:59 -0500

    1 - Artemis beam alpha fix.

--- Projectiles/Boss/ArtemisLaserBeamStart.cs
+++ Projectiles/Boss/ArtemisLaserBeamStart.cs
@@ -150,7 +150,7 @@ namespace CalamityMod.Projectiles.Boss
             Texture2D beamEnd = ModContent.GetTexture("CalamityMod/ExtraTextures/Lasers/AresLaserBeamEnd");
 
             float drawLength = LengthOfLaser;
-            Color color = new Color(250, 180, 100, 0);
+            Color color = new Color(250, 180, 100, 100);
 
             if (Time % 5 == 0)
             {
