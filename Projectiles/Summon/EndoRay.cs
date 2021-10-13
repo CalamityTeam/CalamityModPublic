2b408bbae43e93700a00add83e98d99d4560f330
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2021-10-12 23:23:36 -0500

    Endo Hydra beams ignore tiles

--- Projectiles/Summon/EndoRay.cs
+++ Projectiles/Summon/EndoRay.cs
@@ -24,6 +24,7 @@ namespace CalamityMod.Projectiles.Summon
             projectile.extraUpdates = 100;
             projectile.timeLeft = 265;
 			projectile.coldDamage = true;
+            projectile.tileCollide = false;
 			projectile.usesLocalNPCImmunity = true;
 			projectile.localNPCHitCooldown = 100;
         }
