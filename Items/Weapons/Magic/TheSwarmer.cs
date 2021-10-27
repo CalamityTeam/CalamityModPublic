edc0c5c89ffedf7111347db30e1deda806c4b29e
Merkalto <mercutio@wylfing.net>
2021-10-26 19:04:58 -0500

    Nerfed swarmer a bit, this does not need testing. Other plague items are gtg.

--- Items/Weapons/Magic/TheSwarmer.cs
+++ Items/Weapons/Magic/TheSwarmer.cs
@@ -15,7 +15,7 @@ namespace CalamityMod.Items.Weapons.Magic
 
         public override void SetDefaults()
         {
-            item.damage = 44;
+            item.damage = 40;
             item.magic = true;
             item.mana = 7;
             item.width = 60;
