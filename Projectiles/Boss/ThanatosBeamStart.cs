70979317e1f0a1538946214e266ea38870449d86
Andrew Marheine <marhaj04@gmail.com>
2021-10-31 12:17:26 -0500

    1 - Thanatos deathray no longer instantly hits you.

--- Projectiles/Boss/ThanatosBeamStart.cs
+++ Projectiles/Boss/ThanatosBeamStart.cs
@@ -9,7 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.IO;
 using Terraria;
-using Terraria.Enums;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Projectiles.Boss
@@ -160,8 +159,8 @@ namespace CalamityMod.Projectiles.Boss
 
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
-			// This should never happen, but just in case-
-			if (projectile.velocity == Vector2.Zero)
+			// This should never happen, but just in case.
+			if (projectile.velocity == Vector2.Zero || projectile.localAI[0] < 2f)
 				return false;
 
 			Color beamColor = LaserOverlayColor;
@@ -225,6 +224,8 @@ namespace CalamityMod.Projectiles.Boss
 		{
 			target.AddBuff(ModContent.BuffType<BrimstoneFlames>(), 360);
 		}
+
+		public override bool CanHitPlayer(Player target) => projectile.localAI[0] >= 2f;
 
 		public override void ModifyHitPlayer(Player target, ref int damage, ref bool crit)	
 		{
