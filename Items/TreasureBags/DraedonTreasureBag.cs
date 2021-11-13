7e1b192eed93a014dfd87a0b7ddb0f0504cd391b
Ozzatron <spazeglitch@gmail.com>
2021-11-12 23:18:52 -0500

    Gamer chair is a guaranteed drop

--- Items/TreasureBags/DraedonTreasureBag.cs
+++ Items/TreasureBags/DraedonTreasureBag.cs
@@ -2,11 +2,9 @@ using CalamityMod.Items.Accessories;
 using CalamityMod.Items.Armor.Vanity;
 using CalamityMod.Items.Materials;
 using CalamityMod.Items.Mounts;
-using CalamityMod.Items.Weapons.Magic;
 using CalamityMod.Items.Weapons.Melee;
 using CalamityMod.Items.Weapons.Ranged;
 using CalamityMod.Items.Weapons.Rogue;
-using CalamityMod.Items.Weapons.Summon;
 using CalamityMod.NPCs.ExoMechs.Ares;
 using CalamityMod.World;
 using Terraria;
@@ -15,36 +13,36 @@ using Terraria.ModLoader;
 
 namespace CalamityMod.Items.TreasureBags
 {
-    public class DraedonTreasureBag : ModItem
-    {
-        public override int BossBagNPC => ModContent.NPCType<AresBody>();
+	public class DraedonTreasureBag : ModItem
+	{
+		public override int BossBagNPC => ModContent.NPCType<AresBody>();
 
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Treasure Bag");
-            Tooltip.SetDefault("{$CommonItemTooltip.RightClickToOpen}");
-        }
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Treasure Bag");
+			Tooltip.SetDefault("{$CommonItemTooltip.RightClickToOpen}");
+		}
 
-        public override void SetDefaults()
-        {
-            item.maxStack = 999;
-            item.consumable = true;
-            item.width = 24;
-            item.height = 24;
-            item.rare = ItemRarityID.Cyan;
-            item.expert = true;
-        }
+		public override void SetDefaults()
+		{
+			item.maxStack = 999;
+			item.consumable = true;
+			item.width = 24;
+			item.height = 24;
+			item.rare = ItemRarityID.Cyan;
+			item.expert = true;
+		}
 
-        public override bool CanRightClick() => true;
+		public override bool CanRightClick() => true;
 
-        public override void PostUpdate() => CalamityUtils.ForceItemIntoWorld(item);
+		public override void PostUpdate() => CalamityUtils.ForceItemIntoWorld(item);
 
-        public override void OpenBossBag(Player player)
-        {
-            player.TryGettingDevArmor();
+		public override void OpenBossBag(Player player)
+		{
+			player.TryGettingDevArmor();
 
-            // Materials
-            DropHelper.DropItem(player, ModContent.ItemType<ExoPrism>(), 30, 40);
+			// Materials
+			DropHelper.DropItem(player, ModContent.ItemType<ExoPrism>(), 30, 40);
 
 			// Weapons
 			// Higher chance due to how the drops work
@@ -71,9 +69,9 @@ namespace CalamityMod.Items.TreasureBags
 				);
 			}
 
-            // Equipment
-            DropHelper.DropItem(player, ModContent.ItemType<DraedonsHeart>());
-            DropHelper.DropItemChance(player, ModContent.ItemType<ExoThrone>(), 5);
+			// Equipment
+			DropHelper.DropItem(player, ModContent.ItemType<DraedonsHeart>());
+			DropHelper.DropItem(player, ModContent.ItemType<ExoThrone>());
 
 			// Vanity
 			// Higher chance due to how the drops work
@@ -90,7 +88,7 @@ namespace CalamityMod.Items.TreasureBags
 			if (CalamityWorld.downedAres)
 				DropHelper.DropItemChance(player, ModContent.ItemType<AresMask>(), maskDropRate);
 
-            DropHelper.DropItemChance(player, ModContent.ItemType<DraedonMask>(), maskDropRate);
-        }
-    }
+			DropHelper.DropItemChance(player, ModContent.ItemType<DraedonMask>(), maskDropRate);
+		}
+	}
 }
