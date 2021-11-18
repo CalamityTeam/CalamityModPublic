7ed127966ff2b72566381bfbdc3f90c6e3a17aa1
ben-tk <45545085+ben-tk@users.noreply.github.com>
2021-11-17 22:25:55 -0500

    Made AEW count as dead

--- NPCs/Abyss/EidolonWyrmHeadHuge.cs
+++ NPCs/Abyss/EidolonWyrmHeadHuge.cs
@@ -1326,6 +1326,8 @@ namespace CalamityMod.NPCs.Abyss
 
 		public override void NPCLoot()
         {
+			CalamityGlobalNPC.SetNewBossJustDowned(npc);
+
             DropHelper.DropItem(npc, ModContent.ItemType<Voidstone>(), 80, 100);
             DropHelper.DropItem(npc, ModContent.ItemType<EidolicWail>());
             DropHelper.DropItem(npc, ModContent.ItemType<SoulEdge>());
@@ -1334,6 +1336,10 @@ namespace CalamityMod.NPCs.Abyss
             DropHelper.DropItemCondition(npc, ModContent.ItemType<Lumenite>(), CalamityWorld.downedCalamitas, 1, 50, 108);
             DropHelper.DropItemCondition(npc, ModContent.ItemType<Lumenite>(), CalamityWorld.downedCalamitas && Main.expertMode, 2, 15, 27);
             DropHelper.DropItemCondition(npc, ItemID.Ectoplasm, NPC.downedPlantBoss, 1, 21, 32);
+
+            // Mark Adult Eidolon Wyrm as defeated
+            CalamityWorld.downedAdultEidolonWyrm = true;
+            CalamityNetcode.SyncWorld();
         }
 
 		public override void HitEffect(int hitDirection, double damage)
