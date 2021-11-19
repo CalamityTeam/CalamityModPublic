584a32b7ab31dfb6e858eeb4bf1d00df4f93c1a8
Merkalto <mercutio@wylfing.net>
2021-11-18 18:30:18 -0600

    Arcanum of the Void is actually expert locked now. Increased Dark Plasma drop yield from the bag.

--- Items/TreasureBags/CeaselessVoidBag.cs
+++ Items/TreasureBags/CeaselessVoidBag.cs
@@ -35,7 +35,7 @@ namespace CalamityMod.Items.TreasureBags
         public override void OpenBossBag(Player player)
         {
             // Materials
-            DropHelper.DropItem(player, ModContent.ItemType<DarkPlasma>(), 3, 4);
+            DropHelper.DropItem(player, ModContent.ItemType<DarkPlasma>(), 4, 6);
 
             // Weapons
 			DropHelper.DropItemChance(player, ModContent.ItemType<MirrorBlade>(), DropHelper.BagWeaponDropRateInt);
