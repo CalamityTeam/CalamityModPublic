f369b767baf4d52bdcab6f4ac6f359b49c40072d
ben-tk <45545085+ben-tk@users.noreply.github.com>
2021-11-23 12:48:42 -0500

    tooltip update

--- Items/Weapons/Summon/EyeOfNight.cs
+++ Items/Weapons/Summon/EyeOfNight.cs
@@ -12,7 +12,7 @@ namespace CalamityMod.Items.Weapons.Summon
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Eye of Night");
-            Tooltip.SetDefault("Summons a diseased eyeball that fires cells which attach to enemies and explode");
+            Tooltip.SetDefault("Summons a diseased eyeball that fires cells which attach to enemies and inflict cursed flames");
         }
 
         public override void SetDefaults()
