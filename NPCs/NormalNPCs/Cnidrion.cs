65b3783925a9d452ed05d8ae0eef5be258d8c184
Andrew Marheine <marhaj04@gmail.com>
2021-11-29 16:01:48 -0600

    1 - Reduced Cnidrion spawn rate.

--- NPCs/NormalNPCs/Cnidrion.cs
+++ NPCs/NormalNPCs/Cnidrion.cs
@@ -46,7 +46,7 @@ namespace CalamityMod.NPCs.NormalNPCs
             {
                 return 0f;
             }
-            return 0.25f;
+            return 0.1f;
         }
 
         public override void FindFrame(int frameHeight)
