08e2bda7dcf5815244fcc4c348b18b91c3e2729c
Andrew Marheine <marhaj04@gmail.com>
2021-11-28 19:58:10 -0600

    1 - Reduced the damage of Ares Gauss Nuke Sparks.

--- Projectiles/Boss/AresGaussNukeProjectile.cs
+++ Projectiles/Boss/AresGaussNukeProjectile.cs
@@ -100,7 +100,7 @@ namespace CalamityMod.Projectiles.Boss
 					for (int k = 0; k < totalProjectiles; k++)
 					{
 						Vector2 velocity2 = spinningPoint.RotatedBy(radians * k);
-						Projectile.NewProjectile(projectile.Center, velocity2 + Vector2.Normalize(projectile.velocity) * -6f, type, (int)(projectile.damage * 0.6), 0f, Main.myPlayer);
+						Projectile.NewProjectile(projectile.Center, velocity2 + Vector2.Normalize(projectile.velocity) * -6f, type, (int)(projectile.damage * 0.5), 0f, Main.myPlayer);
 					}
 				}
 			}
