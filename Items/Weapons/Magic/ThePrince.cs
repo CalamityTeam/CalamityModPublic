bd77d14c3a8ded3705458be34affdc7fbe52c844
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2021-12-05 18:50:44 -0600

    Added the Prince

---
+++ Items/Weapons/Magic/ThePrince.cs
@@ -0,0 +1,68 @@
+using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.Magic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Magic
+{
+    public class ThePrince : ModItem
+    {
+        public const int FlameSplitCount = 6;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Prince");
+            Tooltip.SetDefault($"Casts a holy fireball that explodes into {FlameSplitCount} flames\n" +
+                               "So you're telling me that the prince exploded, and then turned into a flower?\n" +
+                               "-Dain, the sailor druid");
+            Item.staff[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 155;
+            item.knockBack = 4.25f;
+            item.shootSpeed = 23.5f;
+            item.magic = true;
+            item.noMelee = true;
+            item.mana = 12;
+            item.width = 102;
+            item.height = 112;
+            item.useTime = item.useAnimation = 21;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.autoReuse = true;
+            item.UseSound = SoundID.DD2_FlameburstTowerShot;
+            item.shoot = ModContent.ProjectileType<PrinceFlameLarge>();
+            item.value = CalamityGlobalItem.RarityTurquoiseBuyPrice;
+            item.rare = ItemRarityID.Purple;
+            item.Calamity().customRarity = CalamityRarity.Turquoise;
+        }
+
+        public override Vector2? HoldoutOrigin()
+        {
+            return new Vector2(20f, 20f);
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 velocity = new Vector2(speedX, speedY);
+            Vector2 flameSpawnPosition = player.RotatedRelativePoint(player.MountedCenter, true);
+            flameSpawnPosition += velocity.SafeNormalize(Vector2.Zero) * 105f;
+            Projectile.NewProjectile(flameSpawnPosition, velocity, type, damage, knockBack, player.whoAmI);
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<ArchAmaryllis>());
+            recipe.AddIngredient(ModContent.ItemType<DivineGeode>(), 20);
+            recipe.AddIngredient(ModContent.ItemType<UnholyEssence>(), 10);
+            recipe.AddIngredient(ModContent.ItemType<UeliaceBar>(), 7);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/Magic/PrinceFlameSmall.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Magic
+{
+    public class PrinceFlameSmall : ModProjectile
+    {
+        public ref float Time => ref projectile.ai[0];
+        public const int AttackDelay = 12;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Holy Fire");
+            Main.projFrames[projectile.type] = 4;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 2;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 16;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.timeLeft = 300;
+            projectile.penetrate = 1;
+            projectile.magic = true;
+        }
+
+        public override void AI()
+        {
+            Time++;
+            projectile.frameCounter++;
+            projectile.frame = projectile.frameCounter / 5 % Main.projFrames[projectile.type];
+            projectile.rotation = projectile.velocity.ToRotation() - MathHelper.PiOver2;
+
+            if (Time > AttackDelay)
+                CalamityGlobalProjectile.HomeInOnNPC(projectile, false, 700f, 14f, 32f);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            lightColor = Color.Lerp(lightColor, Color.White, 0.8f);
+            lightColor.A /= 3;
+            CalamityUtils.DrawAfterimagesCentered(projectile, ProjectileID.Sets.TrailingMode[projectile.type], lightColor, 1);
+            return false;
+        }
+
+        public override bool CanDamage() => Time > AttackDelay;
+    }
+}
