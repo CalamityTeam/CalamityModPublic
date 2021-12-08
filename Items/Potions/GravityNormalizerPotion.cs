c9b10fccbac8f1374c6dfed3e7e5c2db4b1a7e35
ben-tk <45545085+ben-tk@users.noreply.github.com>
2021-12-08 09:23:52 -0500

    Unified Potion stack size & made most recipes alchemy table friendly

--- Items/Potions/GravityNormalizerPotion.cs
+++ Items/Potions/GravityNormalizerPotion.cs
@@ -20,7 +20,7 @@ namespace CalamityMod.Items.Potions
             item.width = 28;
             item.height = 18;
             item.useTurn = true;
-            item.maxStack = 999;
+            item.maxStack = 30;
             item.rare = ItemRarityID.Orange;
             item.useAnimation = 17;
             item.useTime = 17;
@@ -39,8 +39,10 @@ namespace CalamityMod.Items.Potions
             recipe.AddIngredient(ModContent.ItemType<AstralJelly>());
             recipe.AddIngredient(ModContent.ItemType<AldebaranAlewife>());
             recipe.AddTile(TileID.AlchemyTable);
+            recipe.alchemy = true;
             recipe.SetResult(this);
             recipe.AddRecipe();
+			// Blood orb recipes don't get the Alchemy Table effect
             recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.BottledWater);
             recipe.AddIngredient(ModContent.ItemType<BloodOrb>(), 10);
