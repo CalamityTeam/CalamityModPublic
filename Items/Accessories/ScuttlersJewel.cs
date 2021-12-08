61eb94c0df4df144ede57d1df69257e1772643a2
Merkalto <mercutio@wylfing.net>
2021-12-07 21:50:51 -0600

    tooltip fix

--- Items/Accessories/ScuttlersJewel.cs
+++ Items/Accessories/ScuttlersJewel.cs
@@ -10,7 +10,7 @@ namespace CalamityMod.Items.Accessories
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Scuttler's Jewel");
-            Tooltip.SetDefault("Stealth strikes spawn a jewel spike when destroyed");
+            Tooltip.SetDefault("Stealth strike projectiles spawn a jewel spike when destroyed");
         }
 
         public override void SetDefaults()
