3f1ebd4f35f1189ff8d77a77c0f12b4ae86694ac
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2021-12-13 17:34:54 +0100

    Pumplerholdout has been rendered asudner
    Very sad very depressing

---
+++ Dusts/PumplerDust.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Ranged { 
+
+    public class PumplerDust : ModDust
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = "CalamityMod/Dusts/SmallSmoke";
+            return true;
+        }
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = true;
+            dust.frame = new Rectangle(0, 0, 24, 24);
+        }
+
+        public override Color? GetAlpha(Dust dust, Color lightColor)
+        {
+            Color gray = new Color(40, 40, 40);
+            Color ret;
+            ret = Color.Lerp(Color.Orange, gray, MathHelper.Clamp((float)(dust.alpha - 100) / 80, 0f, 1f));
+            return ret * ((255 - dust.alpha) / 255f);
+        }
+
+        public override bool Update(Dust dust)
+        {
+            dust.velocity *= 0.85f;
+
+            if (dust.alpha < 165)
+            {
+                Lighting.AddLight(dust.position, dust.color.ToVector3() * 0.1f);
+                dust.scale += 0.01f;
+                dust.alpha += 3;
+            }
+            else
+            {
+                dust.scale *= 0.975f;
+                dust.alpha += 2;
+            }
+
+            dust.position += dust.velocity;
+            if (dust.alpha >= 255)
+                dust.active = false;
+
+            return false;
+        }
+    }
+}
