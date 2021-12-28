329fca21ac8696b55539ff0ba33ae1f13e0e41c2
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2021-12-28 05:00:03 +0100

    Started working on it

---
+++ Items/Placeables/MetaTile.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Placeables
+{
+    public class MetaTile : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Superdirectional Tile Example");
+        }
+
+        public override void SetDefaults()
+        {
+            item.createTile = ModContent.TileType<Tiles.SuperDirectionalTile>();
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.width = 16;
+            item.height = 16;
+            item.maxStack = 999;
+            item.value = Item.sellPrice(silver: 10);
+            item.rare = ItemRarityID.Green;
+        }
+    }
+}
