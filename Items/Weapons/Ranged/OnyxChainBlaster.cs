bc6c7ed99057cce159dd63950f19f1d9c4675e4c
Andrew Marheine <marhaj04@gmail.com>
2022-01-11 18:26:22 -0600

    1 - Nerfed Clockwork Bow and Onyx Chain Blaster damage.

--- Items/Weapons/Ranged/OnyxChainBlaster.cs
+++ Items/Weapons/Ranged/OnyxChainBlaster.cs
@@ -16,7 +16,7 @@ namespace CalamityMod.Items.Weapons.Ranged
 
         public override void SetDefaults()
         {
-            item.damage = 51;
+            item.damage = 40;
             item.ranged = true;
             item.width = 64;
             item.height = 32;
