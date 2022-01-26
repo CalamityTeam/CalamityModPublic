1f020e99ff8d773605d78303fa6f5b8136b20561
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-01-26 20:02:19 +0100

    Split the holdouts into their own projectile files

--- Items/Weapons/Melee/BiomeBlade.cs
+++ Items/Weapons/Melee/BiomeBlade.cs
@@ -1,11 +1,7 @@
-using CalamityMod.Buffs.StatDebuffs;
-using CalamityMod.CalPlayer;
 using CalamityMod.DataStructures;
 using CalamityMod.Items.Materials;
-using CalamityMod.Items.Placeables;
 using CalamityMod.Particles;
 using CalamityMod.Projectiles.Melee;
-using Terraria.Graphics.Shaders;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -17,10 +13,6 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 using Terraria.ModLoader.IO;
-using static CalamityMod.Items.Weapons.Melee.BiomeBlade;
-using static CalamityMod.CalamityUtils;
-
-
 
 namespace CalamityMod.Items.Weapons.Melee
 {
@@ -316,191 +308,7 @@ namespace CalamityMod.Items.Weapons.Melee
 
             return true;
         }
-
     }
-
-    public class BrokenBiomeBladeHoldout : ModProjectile //Visuals
-    {
-        private Player Owner => Main.player[projectile.owner];
-
-        public bool OwnerCanUseItem => Owner.HeldItem == associatedItem ? (Owner.HeldItem.modItem as BiomeBlade).CanUseItem(Owner) : false;
-        public bool OwnerMayChannel => OwnerCanUseItem && Owner.Calamity().mouseRight && Owner.active && !Owner.dead && Owner.StandingStill() && !Owner.mount.Active && Owner.CheckSolidGround(1, 3);
-        public ref float ChanneledState => ref projectile.ai[0];
-        public ref float ChannelTimer => ref projectile.ai[1];
-        public ref float Initialized => ref projectile.localAI[0];
-
-        private Item associatedItem;
-        const int ChannelTime = 120;
-
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Broken Biome Blade");
-        }
-        public override string Texture => "CalamityMod/Items/Weapons/Melee/BiomeBlade";
-        public bool drawIndrawHeldProjInFrontOfHeldItemAndArms = true;
-        public override void SetDefaults()
-        {
-            projectile.width = projectile.height = 36;
-            projectile.aiStyle = -1;
-            projectile.friendly = true;
-            projectile.penetrate = -1;
-            projectile.timeLeft = 60;
-            projectile.tileCollide = false;
-            projectile.damage = 0;
-        }
-
-        public CurveSegment anticipation = new CurveSegment(EasingType.SineOut, 0f, 1f, 0.35f);
-        public CurveSegment thrust = new CurveSegment(EasingType.ExpIn, 0.85f, 1.35f, -1.45f);
-        public CurveSegment bounceback = new CurveSegment(EasingType.SineOut, 0.95f, -0.1f, 0.1f);
-        internal float SwordHeight() => PiecewiseAnimation(ChannelTimer / (float)ChannelTime, new CurveSegment[] { anticipation, thrust, bounceback });
-
-
-
-        public override void AI()
-        {
-
-            if (Initialized == 0f)
-            {
-                //If dropped, kill it instantly
-                if (Owner.HeldItem.type != ItemType<BiomeBlade>())
-                {
-                    projectile.Kill();
-                    return;
-                }
-
-                if (Owner.whoAmI == Main.myPlayer)
-                    Main.PlaySound(SoundID.DD2_DarkMageHealImpact);
-
-                associatedItem = Owner.HeldItem;
-                //Switch up the attunements
-                Attunement temporaryAttunementStorage = (associatedItem.modItem as BiomeBlade).mainAttunement;
-                (associatedItem.modItem as BiomeBlade).mainAttunement = (associatedItem.modItem as BiomeBlade).secondaryAttunement;
-                (associatedItem.modItem as BiomeBlade).secondaryAttunement = temporaryAttunementStorage;
-                Initialized = 1f;
-            }
-
-            if (!OwnerMayChannel && ChanneledState == 0f) //IF the channeling gets interrupted for any reason
-            {
-                projectile.Center = Owner.Top + new Vector2(18f, 0f);
-                ChanneledState = 1f;
-                projectile.timeLeft = 60;
-                return;
-            }
-
-            if (ChanneledState == 0f)
-            {
-                Owner.heldProj = projectile.whoAmI;
-
-                projectile.Center = Owner.Center + new Vector2(16f * Owner.direction, -30 * SwordHeight() + 10f) ;
-                projectile.rotation = Utils.AngleLerp(-MathHelper.PiOver4, MathHelper.PiOver4 + MathHelper.PiOver2, MathHelper.Clamp(((ChannelTimer - 20f) / 50f), 0f, 1f));
-                ChannelTimer++;
-                projectile.timeLeft = 60;
-
-                if (ChannelTimer >= ChannelTime)
-                {
-                    Attune((BiomeBlade)associatedItem.modItem);
-                    projectile.timeLeft = 120;
-                    ChanneledState = 2f; //State where it stays invisible doing nothing. Acts as a cooldown
-
-                    Color particleColor = (associatedItem.modItem as BiomeBlade).mainAttunement.tooltipColor;
-
-                    for (int i = 0; i <= 5; i++)
-                    {
-                        Vector2 displace = Vector2.UnitX * 20 * Main.rand.NextFloat(-1f, 1f);
-                        Particle Glow = new GenericBloom(Owner.Bottom + displace, -Vector2.UnitY * Main.rand.NextFloat(1f, 5f), particleColor, 0.02f + Main.rand.NextFloat(0f, 0.2f), 20 + Main.rand.Next(30));
-                        GeneralParticleHandler.SpawnParticle(Glow);
-                    }
-                    for (int i = 0; i <= 10; i++)
-                    {
-                        Vector2 displace = Vector2.UnitX * 16 * Main.rand.NextFloat(-1f, 1f);
-                        Particle Sparkle = new GenericSparkle(Owner.Bottom + displace, -Vector2.UnitY * Main.rand.NextFloat(1f, 5f), particleColor, particleColor, 0.5f + Main.rand.NextFloat(-0.2f, 0.2f), 20 + Main.rand.Next(30), 1, 2f);
-                        GeneralParticleHandler.SpawnParticle(Sparkle);
-                    }
-                }
-            }
-
-            if (ChanneledState == 1f)
-                projectile.position += Vector2.UnitY * -0.3f * (1f + projectile.timeLeft/60f);
-        }
-
-       public void Attune(BiomeBlade item)
-       {
-            bool jungle = Owner.ZoneJungle;
-            bool snow = Owner.ZoneSnow;
-            bool evil = Owner.ZoneCorrupt || Owner.ZoneCrimson;
-            bool desert = Owner.ZoneDesert;
-            bool hell = Owner.ZoneUnderworldHeight;
-            bool ocean = Owner.ZoneBeach;
-
-            Attunement attunement = Attunement.attunementArray[(int)AttunementID.Default];
-
-            if (desert || hell)
-            {
-                attunement = Attunement.attunementArray[(int)AttunementID.Hot];
-            }
-            if (snow)
-            {
-                attunement = Attunement.attunementArray[(int)AttunementID.Cold];
-            }
-            if (jungle || ocean)
-            {
-                attunement = Attunement.attunementArray[(int)AttunementID.Tropical];
-            }
-            if (evil)
-            {
-                attunement = Attunement.attunementArray[(int)AttunementID.Evil];
-            }
-
-            //If the owner already had the attunement , break out of it (And unswap)
-            if (item.secondaryAttunement == attunement)
-            {
-                Main.PlaySound(SoundID.DD2_LightningBugZap, projectile.Center);
-                item.secondaryAttunement = item.mainAttunement;
-                item.mainAttunement = attunement;
-                return;
-            }
-
-            Main.PlaySound(SoundID.DD2_MonkStaffGroundImpact, projectile.Center);
-            item.mainAttunement = attunement;
-        }       
-
-        public override void Kill(int timeLeft)
-        {
-            if (associatedItem == null)
-            {
-                return;
-            }
-
-            //If we swapped out the main attunement for the second one despite the second attunement being empty at the time, unswap them.
-            if ((associatedItem.modItem as BiomeBlade).mainAttunement == null && (associatedItem.modItem as BiomeBlade).secondaryAttunement != null)
-            {
-                (associatedItem.modItem as BiomeBlade).mainAttunement = (associatedItem.modItem as BiomeBlade).secondaryAttunement;
-                (associatedItem.modItem as BiomeBlade).secondaryAttunement = null;
-            }
-
-            //Cool particles
-        }
-
-        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
-        {
-            if (ChanneledState == 0f && ChannelTimer > 6f)
-                return base.PreDraw(spriteBatch, lightColor);
-
-            else if (ChanneledState == 1f)
-            {
-                Texture2D tex = GetTexture(Texture);
-                Vector2 squishyScale = new Vector2(Math.Abs((float)Math.Sin(MathHelper.Pi + MathHelper.TwoPi  * projectile.timeLeft / 30f)), 1f);
-                SpriteEffects flip = (float)Math.Sin(MathHelper.Pi + MathHelper.TwoPi * projectile.timeLeft / 30f) > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
-                spriteBatch.Draw(tex, projectile.position - Main.screenPosition, null, lightColor * (projectile.timeLeft / 60f), 0, tex.Size() / 2, squishyScale * (2f - (projectile.timeLeft / 60f)), flip, 0);
-
-                return false;
-            }
-
-            else
-                return false;
-        }
-    }
-
 }
 
 
