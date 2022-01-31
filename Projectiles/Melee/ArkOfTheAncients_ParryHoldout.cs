5ce10ba321b166e531951d67b7a853210babd140
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-01-31 03:51:32 +0100

    Ark of the boomers lmfao

---
+++ Projectiles/Melee/ArkOfTheAncients_ParryHoldout.cs
@@ -0,0 +1,231 @@
+﻿using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.CalPlayer;
+using CalamityMod.DataStructures;
+using CalamityMod.Particles;
+using CalamityMod.Items.Weapons.Melee;
+using Terraria.Graphics.Shaders;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod.Particles;
+using static Terraria.ModLoader.ModContent;
+using static CalamityMod.CalamityUtils;
+
+
+namespace CalamityMod.Projectiles.Melee
+{
+    public class ArkoftheAncientsParryHoldout : ModProjectile
+    {
+        public override string Texture => "CalamityMod/Items/Weapons/Melee/ArkOfTheAncients";
+
+        private bool initialized = false;
+        const float MaxTime = 160;
+        static float ParryTime = 15;
+        public Vector2 DistanceFromPlayer => projectile.velocity * 10 * (1f + ((float)Math.Sin(Timer / ParryTime * MathHelper.Pi) * 0.8f));
+        public float Timer => MaxTime - projectile.timeLeft;
+        public ref float AlreadyParried => ref projectile.ai[1];
+        public Player Owner => Main.player[projectile.owner];
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ark of the Ancients");
+        }
+        public override void SetDefaults()
+        {
+            projectile.melee = true;
+            projectile.width = projectile.height = 75;
+            projectile.width = projectile.height = 75;
+            projectile.tileCollide = false;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+        }
+
+        public override bool CanDamage() => Timer <= ParryTime && AlreadyParried == 0f;
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            //The hitbox is simplified into a line collision.
+            float collisionPoint = 0f;
+            float bladeLenght = 80f * projectile.scale;
+            return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Owner.Center + DistanceFromPlayer, Owner.Center + DistanceFromPlayer + (projectile.velocity * bladeLenght), 24, ref collisionPoint);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (AlreadyParried > 0)
+                return;
+
+            Owner.GiveIFrames(15);
+            ArkoftheAncients sword = (Owner.HeldItem.modItem as ArkoftheAncients);
+            sword.Charge = 10f;
+            AlreadyParried = 1f;
+
+            Main.PlaySound(SoundID.DD2_WitherBeastCrystalImpact);
+            CombatText.NewText(projectile.Hitbox, new Color(111, 247, 200), "Parry!", true);
+
+            Vector2 particleOrigin = target.Hitbox.Size().Length() < 140 ? target.Center : projectile.Center + projectile.rotation.ToRotationVector2() * 60f;
+            for (int i = 0; i < 10; i++)
+            {
+                Vector2 particleSpeed = Main.rand.NextVector2CircularEdge(1, 1) * Main.rand.NextFloat(2.6f, 4f);
+                Particle energyLeak = new SquishyLightParticle(particleOrigin, particleSpeed, Main.rand.NextFloat(0.3f, 0.6f), Color.Cyan, 60, 1, 1.5f, hueShift: 0.02f);
+                GeneralParticleHandler.SpawnParticle(energyLeak);
+            }
+        }
+
+        public override void AI()
+        {
+            if (!initialized) //Initialization
+            {
+                projectile.timeLeft = (int)MaxTime;
+                Main.PlaySound(SoundID.DD2_SkyDragonsFuryShot, projectile.Center);
+
+
+                //Take the direction the sword is swung. FUCK not controlling the swing direction more than just left/right :|
+                //The direction to mouseworld may need to be turned into the custom synced player mouse variables . not on the branch currently tho
+                projectile.velocity = Owner.DirectionTo(Main.MouseWorld);
+                projectile.velocity.Normalize();
+                projectile.rotation = projectile.velocity.ToRotation();
+
+                initialized = true;
+                projectile.netUpdate = true;
+                projectile.netSpam = 0;
+            }
+
+            //Manage position and rotation
+            projectile.Center = Owner.Center + DistanceFromPlayer ;
+            //rotation = projectile.rotation + MathHelper.SmoothStep(SwingWidth / 2 * SwingDirection, -SwingWidth / 2 * SwingDirection, Timer / MaxTime); 
+            projectile.scale = 1.4f + ((float)Math.Sin(Timer / MaxTime * MathHelper.Pi) * 0.6f); //SWAGGER
+
+            if (Timer > ParryTime)
+                return;
+
+            if (AlreadyParried == 0)
+            {
+                float collisionPoint = 0f;
+                float bladeLenght = 80f * projectile.scale;
+
+                for (int k = 0; k < Main.maxProjectiles; k++)
+                {
+                    Projectile proj = Main.projectile[k];
+
+                    if (proj.active && proj.hostile && proj.damage > 1 && proj.velocity.Length() * (proj.extraUpdates + 1) > 1f
+                        && Collision.CheckAABBvLineCollision(proj.Hitbox.TopLeft(), proj.Hitbox.Size(), Owner.Center + DistanceFromPlayer, Owner.Center + DistanceFromPlayer + (projectile.velocity * bladeLenght), 24, ref collisionPoint))
+                    {
+                        int diff = (proj.damage * 2) - 100;
+
+                        ArkoftheAncients sword = (Owner.HeldItem.modItem as ArkoftheAncients);
+                        if (sword != null)
+                            sword.Charge = 10;
+
+
+                        CombatText.NewText(projectile.Hitbox, new Color(111, 247, 200), "Parry!", true);
+
+                        //Reduce the projectile's damage by 100 for half a second.
+                        if (proj.Calamity().damageReduction < 100)
+                            proj.Calamity().damageReduction = 100;
+                        if (proj.Calamity().damageReductionTimer < 60)
+                            proj.Calamity().damageReductionTimer = 60;
+
+                        //Bounce off the player if they are in the air
+                        if (Owner.velocity.Y != 0)
+                            Owner.velocity += Vector2.Normalize(Owner.Center - proj.Center) * 5;
+
+                        Main.PlaySound(SoundID.DD2_WitherBeastCrystalImpact);
+                        AlreadyParried = 1f;
+
+                        break;
+                    }
+                }
+            }
+
+            //Make the owner look like theyre holding the sword bla bla
+            Owner.heldProj = projectile.whoAmI;
+            Owner.direction = Math.Sign(projectile.velocity.X);
+            Owner.itemRotation = projectile.rotation;
+            if (Owner.direction != 1)
+            {
+                Owner.itemRotation -= 3.14f;
+            }
+            Owner.itemRotation = MathHelper.WrapAngle(Owner.itemRotation);
+
+            if (AlreadyParried > 0)
+            {
+                AlreadyParried++;
+            }
+        }
+
+        //Animation keys
+        public CurveSegment anticipation = new CurveSegment(EasingType.SineBump, 0f, 0f, -0.15f);
+        public CurveSegment thrust = new CurveSegment(EasingType.PolyInOut, 0.2f, 0f, 0.9f, 3);
+        public CurveSegment hold = new CurveSegment(EasingType.SineBump, 0.35f, 0.9f, 0.1f);
+        public CurveSegment retract = new CurveSegment(EasingType.PolyInOut, 0.7f, 0.9f, -0.9f, 3);
+        internal float ThrustDisplaceRatio() => PiecewiseAnimation(Timer / MaxTime, new CurveSegment[] { anticipation, thrust, hold, retract });
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            //Stop drawing the sword. Draw a recharge bar instead
+            if (Timer > ParryTime)
+            {
+                if (Main.myPlayer == Owner.whoAmI)
+                {
+                    var barBG = GetTexture("CalamityMod/ExtraTextures/GenericBarFront");
+                    var barFG = GetTexture("CalamityMod/ExtraTextures/GenericBarBack");
+
+                    Vector2 drawPos = Owner.Center - Main.screenPosition + new Vector2(0, -36) - barBG.Size() / 2;
+                    Rectangle frame = new Rectangle(0, 0, (int)((Timer - ParryTime) / (MaxTime - ParryTime) * barFG.Width), barFG.Height);
+
+                    float opacity = Timer <= ParryTime + 25f ? (Timer - ParryTime) / 25f : (MaxTime - Timer <= 8) ? projectile.timeLeft / 8f : 1f;
+                    Color color = Main.hslToRgb((Main.GlobalTime * 0.6f) % 1, 1, 0.85f + (float)Math.Sin(Main.GlobalTime * 3f) * 0.1f);
+
+                    spriteBatch.Draw(barBG, drawPos, color * opacity);
+                    spriteBatch.Draw(barFG, drawPos, frame, color * opacity * 0.8f);
+
+
+                }
+                return false;
+            }
+            Texture2D sword = GetTexture("CalamityMod/Items/Weapons/Melee/ArkOfTheAncients");
+            Texture2D glowmask = GetTexture("CalamityMod/Items/Weapons/Melee/ArkOfTheAncientsGlow");
+
+            float drawRotation = projectile.rotation + MathHelper.PiOver4;
+            Vector2 drawOrigin = new Vector2(0f, sword.Height);
+            Vector2 drawOffset = Owner.Center + projectile.velocity * DistanceFromPlayer.Length() - Main.screenPosition;
+
+
+            spriteBatch.Draw(sword, drawOffset, null, lightColor, drawRotation, drawOrigin, projectile.scale, 0f, 0f);
+            spriteBatch.Draw(glowmask, drawOffset, null, Color.Lerp(lightColor, Color.White, 0.75f), drawRotation, drawOrigin, projectile.scale, 0f, 0f);
+
+            if (AlreadyParried > 0)
+            {
+                drawOrigin = new Vector2(0f, 36f);
+                Rectangle frame = new Rectangle(24, 0, 36, 36);
+                drawOffset = Owner.Center + projectile.velocity * (DistanceFromPlayer.Length() + 33) - Main.screenPosition;
+                spriteBatch.Draw(glowmask, drawOffset, frame, Main.hslToRgb(Main.GlobalTime % 1, 1, 0.8f) * (1 - AlreadyParried / ParryTime), drawRotation, drawOrigin, projectile.scale + AlreadyParried / ParryTime, 0f, 0f);
+            }
+
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            //Play a blip when it dies, to indicate to the player its ready to get used again
+            if (Main.myPlayer == Owner.whoAmI)
+                Main.PlaySound(SoundID.Item35);
+        }
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(initialized);
+        }
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            initialized = reader.ReadBoolean();
+        }
+    }
+}
