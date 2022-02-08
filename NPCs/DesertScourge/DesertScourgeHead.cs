e17211d82b51cd449c7f2b313d4734a93c289e05
mountaindrew10 <marhaj04@gmail.com>
2022-02-07 23:13:40 -0600

    1 - Adjusted some AEW numbers.

--- NPCs/DesertScourge/DesertScourgeHead.cs
+++ NPCs/DesertScourge/DesertScourgeHead.cs
@@ -10,7 +10,6 @@ using CalamityMod.Items.Weapons.Melee;
 using CalamityMod.Items.Weapons.Ranged;
 using CalamityMod.Items.Weapons.Rogue;
 using CalamityMod.Items.Weapons.Summon;
-using CalamityMod.NPCs.TownNPCs;
 using CalamityMod.World;
 using Microsoft.Xna.Framework;
 using System;
