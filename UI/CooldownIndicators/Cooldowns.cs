e73f4265184203f0fdcf5fe135ca236714a12e8c
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-02-16 17:44:49 +0100

    Both Scarf cooldowns share a name since they affect both scarves

--- UI/CooldownIndicators/Cooldowns.cs
+++ UI/CooldownIndicators/Cooldowns.cs
@@ -105,7 +105,7 @@ namespace CalamityMod.UI.CooldownIndicators
     {
         public override string SyncID => "CounterScarf";
         public override bool DisplayMe => true;
-        public override string Name => "Counter Scarf Cooldown";
+        public override string Name => "Scarf Cooldown";
         public override string Texture => "CalamityMod/UI/CooldownIndicators/CounterScarf";
         public override Color OutlineColor => Color.Lerp(new Color(255, 115, 178), new Color(255, 76, 76), (float)Math.Sin(Main.GlobalTime * 2f) * 0.5f + 0.5f);
         public override Color CooldownColorStart => new Color(194, 75, 97);
@@ -121,7 +121,7 @@ namespace CalamityMod.UI.CooldownIndicators
     {
         public override string SyncID => "EvasionScarf";
         public override bool DisplayMe => true;
-        public override string Name => "Evasion Scarf Cooldown";
+        public override string Name => "Scarf Cooldown";
         public override string Texture => "CalamityMod/UI/CooldownIndicators/EvasionScarf";
         public override Color OutlineColor => Color.Lerp(new Color(255, 194, 150), new Color(255, 160, 150), (float)Math.Sin(Main.GlobalTime * 2f) * 0.5f + 0.5f);
         public override Color CooldownColorStart => new Color(132, 23, 32);
