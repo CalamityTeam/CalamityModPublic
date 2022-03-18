19c6afd5d951c22de2c44bfa901654429bbac170
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-03-18 17:04:02 +0100

    Galaxia'd

--- Items/Weapons/Melee/FourSeasonsGalaxia.cs
+++ Items/Weapons/Melee/FourSeasonsGalaxia.cs
@@ -35,12 +35,12 @@ namespace CalamityMod.Items.Weapons.Melee
         public static int BaseDamage = 800;
 
         public static int PhoenixAttunement_BaseDamage = 800;
-        public static int PhoenixAttunement_LocalIFrames = 20; //Remember its got one extra update
+        public static int PhoenixAttunement_LocalIFrames = 30; //Remember its got one extra update
         public static float PhoenixAttunement_BoltDamageReduction = 0.5f;
-        public static float PhoenixAttunement_BoltThrowDamageMultiplier = 1.5f;
+        public static float PhoenixAttunement_BoltThrowDamageMultiplier = 1f;
         public static float PhoenixAttunement_BaseDamageReduction = 0.5f;
         public static float PhoenixAttunement_FullChargeDamageBoost = 2.1f;
-        public static float PhoenixAttunement_ThrowDamageBoost = 3.6f;
+        public static float PhoenixAttunement_ThrowDamageBoost = 3f;
 
         public static int PolarisAttunement_BaseDamage = 1200;
         public static int PolarisAttunement_FullChargeDamage = 1900;
