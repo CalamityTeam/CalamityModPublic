8b10aa8bfcc18a042713b9830b6010d0ff856b0a
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-03-26 18:32:34 +0100

    Tooltip editing paranoia

--- Items/Weapons/Melee/FourSeasonsGalaxia.cs
+++ Items/Weapons/Melee/FourSeasonsGalaxia.cs
@@ -90,6 +90,9 @@ namespace CalamityMod.Items.Weapons.Melee
 
         public override void ModifyTooltips(List<TooltipLine> list)
         {
+            if (list == null)
+                return;
+
             SafeCheckAttunements();
 
             Player player = Main.player[Main.myPlayer];
