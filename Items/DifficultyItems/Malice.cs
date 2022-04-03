74dd86eb59a80978afae0aea41249193e5916889
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-04-03 19:36:53 -0400

    malice.cs fixes

--- Items/DifficultyItems/Malice.cs
+++ Items/DifficultyItems/Malice.cs
@@ -46,21 +46,21 @@ namespace CalamityMod.Items.DifficultyItems
             Item.consumable = false;
         }
 
-        public override void UseStyle(Player player)
+        public override void UseStyle(Player player, Rectangle rectangle)
         {
             player.itemLocation += new Vector2(-32f * player.direction, player.gravDir).RotatedBy(player.itemRotation);
         }
 
         public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frameI, Color drawColor, Color itemColor, Vector2 origin, float scale)
         {
-            Texture2D texture = ModContent.Request<Texture2D>("CalamityMod/Items/DifficultyItems/Malice_Animated");
+            Texture2D texture = ModContent.Request<Texture2D>("CalamityMod/Items/DifficultyItems/Malice_Animated").Value;
             spriteBatch.Draw(texture, position, Item.GetCurrentFrame(ref frame, ref frameCounter, 8, 8), Color.White, 0f, origin, scale, SpriteEffects.None, 0);
             return false;
         }
 
         public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
         {
-            Texture2D texture = ModContent.Request<Texture2D>("CalamityMod/Items/DifficultyItems/Malice_Animated");
+            Texture2D texture = ModContent.Request<Texture2D>("CalamityMod/Items/DifficultyItems/Malice_Animated").Value;
             spriteBatch.Draw(texture, Item.position - Main.screenPosition, Item.GetCurrentFrame(ref frame, ref frameCounter, 8, 8), lightColor, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0);
             return false;
         }
