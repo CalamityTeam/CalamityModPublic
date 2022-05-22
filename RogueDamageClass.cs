40a2b8a14d0c7c3a0073f7aab8a6ace2fceadf9d
Dominic <farfetchedking541@gmail.com>
2022-05-22 14:50:28 -0500

    Added RogueDamageClass

---
+++ RogueDamageClass.cs
@@ -0,0 +1,18 @@
+﻿using Terraria.ModLoader;
+
+namespace CalamityMod.Items
+{
+    public class RogueDamageClass : DamageClass
+    {
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+        {
+            if (damageClass == Throwing || damageClass == Generic)
+                return StatInheritanceData.Full;
+
+            return StatInheritanceData.None;
+        }
+
+        public override bool GetEffectInheritance(DamageClass damageClass) =>
+            damageClass == Throwing;
+    }
+}
