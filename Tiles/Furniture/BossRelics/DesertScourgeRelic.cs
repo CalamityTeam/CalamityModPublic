a82b6c54503dbde8332f9f48d238ff56db898065
Ozzatron <spazeglitch@gmail.com>
2022-05-22 16:09:38 -0400

    BosRelics -> BossRelics typo

--- Tiles/Furniture/BossRelics/DesertScourgeRelic.cs
+++ Tiles/Furniture/BossRelics/DesertScourgeRelic.cs
@@ -6,7 +6,7 @@ namespace CalamityMod.Tiles.Furniture.BossRelics
 {
     public class DesertScourgeRelic : BaseBossRelic
     {
-        public override string RelicTextureName => "CalamityMod/Tiles/Furniture/BosRelics/DesertScourgeRelic";
+        public override string RelicTextureName => "CalamityMod/Tiles/Furniture/BossRelics/DesertScourgeRelic";
 
         public override int AssociatedItem => ModContent.ItemType<Items.Placeables.Furniture.BossRelics.DesertScourgeRelic>();
     }
