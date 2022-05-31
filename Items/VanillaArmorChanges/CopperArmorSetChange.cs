7f9a86099f0002c4f1249ad161ac6dcfc9724453
Ozzatron <spazeglitch@gmail.com>
2022-05-31 18:27:08 -0400

    copper individual pieces effects

--- Items/VanillaArmorChanges/CopperArmorSetChange.cs
+++ Items/VanillaArmorChanges/CopperArmorSetChange.cs
@@ -15,23 +15,32 @@ namespace CalamityMod.Items.VanillaArmorChanges
 
         public override string ArmorSetName => "Copper";
 
-        public const float FlatDamage = 2.0f;
-        public const float MoveSpeed = 0.1f;
-        public const int MiningSpeedPercentSetBonus = 25;
+        public const float HeadDamage = 0.05f;
+        public const float ChestCrit = 3f;
+        public const float LegsMoveSpeed = 0.05f;
+        public const float SetBonusFlatDamage = 2.0f;
+        public const float SetBonusMoveSpeed = 0.1f;
+        public const int SetBonusMiningSpeedPercent = 25;
+
+        public override void ApplyHeadPieceEffect(Player player) => player.GetDamage<GenericDamageClass>() += HeadDamage;
+
+        public override void ApplyBodyPieceEffect(Player player) => player.GetCritChance<GenericDamageClass>() += ChestCrit;
+
+        public override void ApplyLegPieceEffect(Player player) => player.moveSpeed += LegsMoveSpeed;
 
         public override void UpdateSetBonusText(ref string setBonusText)
         {
             StringBuilder sb = new StringBuilder(256);
             sb.Append("+2 flat damage to all attacks, +10% movement speed\n");
-            sb.Append(CalamityGlobalItem.MiningSpeedString(MiningSpeedPercentSetBonus));
+            sb.Append(CalamityGlobalItem.MiningSpeedString(SetBonusMiningSpeedPercent));
             setBonusText += sb.ToString();
         }
 
         public override void ApplyArmorSetBonus(Player player)
         {
-            player.GetDamage<GenericDamageClass>().Flat += FlatDamage;
-            player.moveSpeed += MoveSpeed;
-            player.pickSpeed -= MiningSpeedPercentSetBonus * 0.01f;
+            player.GetDamage<GenericDamageClass>().Flat += SetBonusFlatDamage;
+            player.moveSpeed += SetBonusMoveSpeed;
+            player.pickSpeed -= SetBonusMiningSpeedPercent * 0.01f;
         }
     }
 }
