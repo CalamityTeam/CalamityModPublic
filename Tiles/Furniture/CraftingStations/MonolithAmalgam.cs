73576d861f9e1730fe10d467610fb457fe3b6a72
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2022-06-07 18:43:05 -0500

    Fixed a glowmask issue

--- Tiles/Furniture/CraftingStations/MonolithAmalgam.cs
+++ Tiles/Furniture/CraftingStations/MonolithAmalgam.cs
@@ -34,7 +34,7 @@ namespace CalamityMod.Tiles.Furniture.CraftingStations
         {
             int xPos = Main.tile[i, j].TileFrameX;
             int yPos = Main.tile[i, j].TileFrameY;
-            Texture2D glowmask = ModContent.Request<Texture2D>("CalamityMod/Tiles/Furniture/CraftingStations/MonolithCraftingGlow").Value;
+            Texture2D glowmask = ModContent.Request<Texture2D>("CalamityMod/Tiles/Furniture/CraftingStations/MonolithAmalgamGlow").Value;
             Color drawColour = GetDrawColour(i, j, new Color(100, 100, 100, 100));
             Vector2 zero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
             Vector2 drawOffset = new Vector2(i * 16 - Main.screenPosition.X, j * 16 - Main.screenPosition.Y) + zero;
