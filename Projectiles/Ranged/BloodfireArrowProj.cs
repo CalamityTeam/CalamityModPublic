3465734a5fe1bb4e096f7e54588c8f604d7c6b7a
Ozzatron <spazeglitch@gmail.com>
2022-06-21 23:51:22 -0400

    lmao what happened to the usings here

--- Projectiles/Ranged/BloodfireArrowProj.cs
+++ Projectiles/Ranged/BloodfireArrowProj.cs
@@ -1,12 +1,12 @@
-﻿using CalamityMod.Buffs.DamageOverTime;
+﻿using System;
+using CalamityMod.Buffs.DamageOverTime;
 using CalamityMod.Dusts;
 using CalamityMod.Items.Weapons.Ranged;
 using Microsoft.Xna.Framework;
-using System;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.Audio;
 
 namespace CalamityMod.Projectiles.Ranged
 {
