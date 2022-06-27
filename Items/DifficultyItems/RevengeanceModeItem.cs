7b4d5b034a59f3b9805041d286715c76601f231c
Merkalto <mercutio@wylfing.net>
2022-06-26 21:07:03 -0500

    Fixed Rev item spawnrate tooltip.

--- Items/DifficultyItems/RevengeanceModeItem.cs
+++ Items/DifficultyItems/RevengeanceModeItem.cs
@@ -27,7 +27,7 @@ namespace CalamityMod.Items.DifficultyItems
                 "Getting hit while Adrenaline is maxed will reduce damage taken by a massive amount.\n" +
 
                 // General enemy lines
-                "All enemies spawn 15% more frequently.\n" +
+                "All enemies spawn 17.65% more frequently.\n" +
                 "Certain non-boss enemies and projectiles deal between 10% and 25% more damage.\n" +
                 "Makes certain enemies block life steal and nerfs the effectiveness of life steal.\n" +
 
