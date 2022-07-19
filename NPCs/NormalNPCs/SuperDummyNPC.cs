a8bae20541a30c933fcb750a72d97dc72a337cb9
GinYuH <unknown>
2022-07-19 03:13:45 -0700

    made every gif on the wiki outdated

--- NPCs/NormalNPCs/SuperDummyNPC.cs
+++ NPCs/NormalNPCs/SuperDummyNPC.cs
@@ -14,6 +14,7 @@ namespace CalamityMod.NPCs.NormalNPCs
         {
             this.HideFromBestiary();
             DisplayName.SetDefault("Super Dummy");
+            Main.npcFrameCount[NPC.type] = 11;
             NPCID.Sets.CantTakeLunchMoney[Type] = true;
         }
 
@@ -40,6 +41,51 @@ namespace CalamityMod.NPCs.NormalNPCs
         public override bool? DrawHealthBar(byte hbPosition, ref float scale, ref Vector2 position)
         {
             return false;
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            // Dummy AI, no way
+            NPC.ai[0] = hitDirection;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            // Start animating when hit. Continue animating as long as the animation isn't finished
+            if (NPC.justHit || NPC.frameCounter > 0 || (NPC.frame.Y != 0 && NPC.frame.Y != frameHeight * 4))
+            {
+                NPC.frameCounter += 1.0;
+            }
+            if (NPC.frameCounter > 6.0)
+            {
+                NPC.frameCounter = 0.0;
+                NPC.frame.Y += frameHeight;
+            }
+
+            // Hit from behind
+            if (NPC.ai[0] == -1)
+            {
+                if (NPC.justHit && NPC.frame.Y > frameHeight * 2)
+                {
+                    NPC.frame.Y = frameHeight;
+                }    
+                else if (NPC.frame.Y > frameHeight * 3)
+                {
+                    NPC.frame.Y = 0;
+                }
+            }
+            // Hit from in front
+            else
+            {
+                if (NPC.justHit && NPC.frame.Y > frameHeight * 7)
+                {
+                    NPC.frame.Y = frameHeight * 5;
+                }
+                else if (NPC.frame.Y > frameHeight * 10 || NPC.frame.Y < frameHeight * 4)
+                {
+                    NPC.frame.Y = frameHeight * 4;
+                }
+            }
         }
 
         public override bool CheckDead()
