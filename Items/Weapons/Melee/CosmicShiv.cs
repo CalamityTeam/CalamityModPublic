bb1f6104618c72f6f1a06f1b15657156b909d478
Merkalto <mercutio@wylfing.net>
2022-07-25 17:18:13 -0500

    Cosmic Shiv support

--- Items/Weapons/Melee/CosmicShiv.cs
+++ Items/Weapons/Melee/CosmicShiv.cs
@@ -1,12 +1,10 @@
-﻿using Terraria.DataStructures;
-using Microsoft.Xna.Framework;
-using Terraria;
+﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using CalamityMod.Buffs.DamageOverTime;
 using CalamityMod.Items.Materials;
-using CalamityMod.Projectiles.Melee;
+using CalamityMod.Projectiles.Melee.Shortswords;
 using CalamityMod.Tiles.Furniture.CraftingStations;
+
 
 namespace CalamityMod.Items.Weapons.Melee
 {
@@ -15,15 +13,14 @@ namespace CalamityMod.Items.Weapons.Melee
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Cosmic Shiv");
-            Tooltip.SetDefault("Definitely don't underestimate the power of shivs\n" +
-                "Fires a cosmic beam that homes in on enemies\n" +
+            Tooltip.SetDefault("Fires a cosmic beam that homes in on enemies\n" +
                 "Upon hitting an enemy, a barrage of offscreen objects home in on the enemy as well as raining stars");
             SacrificeTotal = 1;
         }
 
         public override void SetDefaults()
         {
-            Item.useStyle = ItemUseStyleID.Thrust;
+            Item.useStyle = ItemUseStyleID.Rapier;
             Item.useTurn = false;
             Item.useAnimation = 18;
             Item.useTime = 18;
@@ -37,53 +34,12 @@ namespace CalamityMod.Items.Weapons.Melee
             Item.UseSound = SoundID.Item1;
             Item.useTurn = true;
             Item.autoReuse = true;
-            Item.shoot = ModContent.ProjectileType<CosmicShivBall>();
-            Item.shootSpeed = 14f;
+            Item.shoot = ModContent.ProjectileType<CosmicShivProj>();
+            Item.shootSpeed = 2.4f;
 
             Item.value = CalamityGlobalItem.Rarity14BuyPrice;
             Item.Calamity().customRarity = CalamityRarity.DarkBlue;
-            Item.Calamity().donorItem = true;
-        }
-
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            Projectile.NewProjectile(source, position.X, position.Y, Item.shootSpeed * player.direction, 0f, type, damage, knockback, player.whoAmI);
-            return false;
-        }
-
-        public override void MeleeEffects(Player player, Rectangle hitbox)
-        {
-            Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 173);
-        }
-
-        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
-        {
-            for (int k = 0; k < 36; k++)
-            {
-                int dustID = Dust.NewDust(new Vector2(player.position.X, player.position.Y + 16f), player.width, player.height - 16, 173, 0f, 0f, 0, default, 1f);
-                Main.dust[dustID].velocity *= 3f;
-                Main.dust[dustID].scale *= 2f;
-            }
-
-            target.AddBuff(ModContent.BuffType<BrimstoneFlames>(), 120);
-            target.AddBuff(BuffID.Frostburn, 120);
-            target.AddBuff(ModContent.BuffType<HolyFlames>(), 120);
-            target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 120);
-        }
-
-        public override void OnHitPvp(Player player, Player target, int damage, bool crit)
-        {
-            for (int k = 0; k < 36; k++)
-            {
-                int dustID = Dust.NewDust(new Vector2(player.position.X, player.position.Y + 16f), player.width, player.height - 16, 173, 0f, 0f, 0, default, 1f);
-                Main.dust[dustID].velocity *= 3f;
-                Main.dust[dustID].scale *= 2f;
-            }
-
-            target.AddBuff(ModContent.BuffType<BrimstoneFlames>(), 120);
-            target.AddBuff(BuffID.Frostburn, 120);
-            target.AddBuff(ModContent.BuffType<HolyFlames>(), 120);
-            target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 120);
+            Item.Calamity().donorItem = true; //Yatagarasu#0001
         }
 
         public override void AddRecipes()
