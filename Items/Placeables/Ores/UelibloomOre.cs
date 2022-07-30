eecb33fcdfb20c48d384e5c38d1d76bcff59ef1c
Merkalto <mercutio@wylfing.net>
2022-07-29 20:04:12 -0500

    shushing face

--- Items/Placeables/Ores/UelibloomOre.cs
+++ Items/Placeables/Ores/UelibloomOre.cs
@@ -25,7 +25,7 @@ namespace CalamityMod.Items.Placeables.Ores
             Item.height = 10;
             Item.maxStack = 999;
             Item.Calamity().customRarity = CalamityRarity.Turquoise;
-            Item.value = Item.buyPrice(gold: 1);
+            Item.value = Item.sellPrice(gold: 1);
         }
     }
 }
