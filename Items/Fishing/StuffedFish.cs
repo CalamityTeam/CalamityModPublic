e6464f59d43eb7cc6da039213a26dc485ef2d120
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-08-04 09:11:26 +0200

    Stuffed fibsh

--- Items/Fishing/StuffedFish.cs
+++ Items/Fishing/StuffedFish.cs
@@ -23,54 +23,54 @@ namespace CalamityMod.Items.Fishing
             Item.value = Item.sellPrice(silver: 50);
         }
 
-        public override bool CanRightClick()
+        public override bool CanRightClick() => true;
+        public override void ModifyItemLoot(ItemLoot itemLoot)
         {
-            return true;
-        }
-
-        public override void RightClick(Player player)
-        {
-            // IEntitySource my beloathed
-            var s = player.GetSource_OpenItem(Item.type);
-
             int herbMin = 1;
             int herbMax = 3;
             int seedMin = 2;
             int seedMax = 5;
-            DropHelper.DropItemChance(s, player, ItemID.Daybloom, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.Moonglow, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.Waterleaf, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.Deathweed, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.Shiverthorn, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.Fireblossom, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.Blinkroot, 0.25f, herbMin, herbMax);
-            DropHelper.DropItemChance(s, player, ItemID.DaybloomSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.MoonglowSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.WaterleafSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.DeathweedSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.ShiverthornSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.FireblossomSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.BlinkrootSeeds, 0.2f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.GrassSeeds, 0.1f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.JungleGrassSeeds, 0.1f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.MushroomGrassSeeds, 0.1f, seedMin, seedMax);
-            DropHelper.DropItemChance(s, player, ItemID.PumpkinSeed, 0.05f, seedMin, seedMax);
-            DropHelper.DropItemCondition(s, player, ItemID.CorruptSeeds, !WorldGen.crimson, 0.05f, seedMin, seedMax);
-            DropHelper.DropItemCondition(s, player, ItemID.CrimsonSeeds, WorldGen.crimson, 0.05f, seedMin, seedMax);
-            DropHelper.DropItemCondition(s, player, ItemID.HallowedSeeds, Main.hardMode, 0.05f, seedMin, seedMax);
+
+            //Herbs
+            itemLoot.Add(ItemID.Daybloom, 4, herbMin, herbMax);
+            itemLoot.Add(ItemID.Moonglow, 4, herbMin, herbMax);
+            itemLoot.Add(ItemID.Waterleaf, 4, herbMin, herbMax);
+            itemLoot.Add(ItemID.Deathweed, 4, herbMin, herbMax);
+            itemLoot.Add(ItemID.Shiverthorn, 4, herbMin, herbMax);
+            itemLoot.Add(ItemID.Fireblossom, 4, herbMin, herbMax);
+            itemLoot.Add(ItemID.Blinkroot, 4, herbMin, herbMax);
+            //Herb seeds
+            itemLoot.Add(ItemID.DaybloomSeeds, 5, seedMin, seedMax);
+            itemLoot.Add(ItemID.MoonglowSeeds, 5, seedMin, seedMax);
+            itemLoot.Add(ItemID.WaterleafSeeds, 5, seedMin, seedMax);
+            itemLoot.Add(ItemID.ShiverthornSeeds, 5, seedMin, seedMax);
+            itemLoot.Add(ItemID.FireblossomSeeds, 5, seedMin, seedMax);
+            itemLoot.Add(ItemID.BlinkrootSeeds, 5, seedMin, seedMax);
+            //Misc seeds
+            itemLoot.Add(ItemID.GrassSeeds, 10, seedMin, seedMax);
+            itemLoot.Add(ItemID.JungleGrassSeeds, 10, seedMin, seedMax);
+            itemLoot.Add(ItemID.MushroomGrassSeeds, 10, seedMin, seedMax);
+            itemLoot.Add(ItemID.PumpkinSeed, 20, seedMin, seedMax);
+            //Spreadin biome seeds
+            itemLoot.AddIf(() => !WorldGen.crimson , ItemID.CorruptSeeds, 20, seedMin, seedMax);
+            itemLoot.AddIf(() => WorldGen.crimson , ItemID.CrimsonSeeds, 20, seedMin, seedMax);
+            itemLoot.AddIf(() => Main.hardMode , ItemID.HallowedSeeds, 20, seedMin, seedMax);
+
             Mod thorium = CalamityMod.Instance.thorium;
             if (thorium != null)
             {
-                DropHelper.DropItemChance(s, player, thorium.Find<ModItem>("MarineKelp").Type, 0.25f, herbMin, herbMax);
-                DropHelper.DropItemChance(s, player, thorium.Find<ModItem>("MarineKelpSeeds").Type, 0.1f, seedMin, seedMax);
+                itemLoot.Add(thorium.Find<ModItem>("MarineKelp").Type, 4, herbMin, herbMax);
+                itemLoot.Add(thorium.Find<ModItem>("MarineKelpSeeds").Type, 10, seedMin, seedMax);
             }
+
             Mod soa = CalamityMod.Instance.soa;
             if (soa != null)
             {
-                DropHelper.DropItemChance(s, player, soa.Find<ModItem>("Welkinbell").Type, 0.25f, herbMin, herbMax);
-                DropHelper.DropItemChance(s, player, soa.Find<ModItem>("WelkinbellSeeds").Type, 0.1f, seedMin, seedMax);
-                DropHelper.DropItemCondition(s, player, soa.Find<ModItem>("Illumifern").Type, Main.hardMode, 0.25f, herbMin, herbMax);
-                DropHelper.DropItemCondition(s, player, soa.Find<ModItem>("IllumifernSeeds").Type, Main.hardMode, 0.1f, seedMin, seedMax);
+                itemLoot.Add(soa.Find<ModItem>("Welkinbell").Type, 4, herbMin, herbMax);
+                itemLoot.Add(soa.Find<ModItem>("WelkinbellSeeds").Type, 10, seedMin, seedMax);
+
+                itemLoot.AddIf(() => Main.hardMode, soa.Find<ModItem>("Illumifern").Type, 4, herbMin, herbMax);
+                itemLoot.AddIf(() => Main.hardMode, soa.Find<ModItem>("IllumifernSeeds").Type, 10, seedMin, seedMax);
 
                 // TODO -- There is no way to determine if SoA's Abaddon is dead without reflection.
                 // Dan Yami has confirmed that downed calls will be added to SoA eventually.
