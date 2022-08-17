f6e25164b5fdfa9ffb199924b32bf76ecddac605
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-08-17 16:45:00 -0400

    fixed some bugs with Snow Coat and Gladiator armor set bonus text

--- Items/VanillaArmorChanges/GladiatorArmorSetChange.cs
+++ Items/VanillaArmorChanges/GladiatorArmorSetChange.cs
@@ -14,15 +14,13 @@ namespace CalamityMod.Items.VanillaArmorChanges
 
         public override string ArmorSetName => "Gladiator";
 
-        public override bool NeedsToCreateSetBonusTextManually => true;
-
         public const int HelmetRogueDamageBoostPercent = 3;
         public const int ChestplateRogueCritBoostPercent = 3;
         public const int LeggingRogueVelocityBoostPercent = 3;
 
         public override void UpdateSetBonusText(ref string setBonusText)
         {
-            setBonusText = "+3 defense\n" +
+            setBonusText += "\n+3 defense\n" +
                         "5% increased rogue damage and 10% increased velocity\n" +
                         "Rogue stealth builds while not attacking and not moving, up to a max of 70\n" +
                         "Once you have built max stealth, you will be able to perform a Stealth Strike\n" +
