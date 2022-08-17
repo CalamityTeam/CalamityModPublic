374094399dc97bbd2bfeacf040e862323d3009be
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-08-17 19:15:54 -0400

    Abyss Torches now increase luck in the Abyss and negatively affect torch luck outside the Abyss

--- Tiles/FurnitureAbyss/AbyssTorch.cs
+++ Tiles/FurnitureAbyss/AbyssTorch.cs
@@ -81,5 +81,11 @@ namespace CalamityMod.Tiles.FurnitureAbyss
             CalamityUtils.RightClickBreak(i, j);
             return true;
         }
+
+		public override float GetTorchLuck(Player player)
+		{
+			// Note: Total Torch luck never goes below zero
+			return player.ZoneAbyss ? 1f : -0.5f; // Abyss Torch gives maximum positive luck when in the Abyss, otherwise some negative luck
+		}
     }
 }
