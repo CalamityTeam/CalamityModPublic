22fadbdcb09d07730b537d8320079c9f10283cba
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-08-18 18:52:13 -0400

    Added placeholder map/bestiary backgrounds for the Sunken Sea and Brimstone Crag

--- BiomeManagers/BrimstoneCragsBiome.cs
+++ BiomeManagers/BrimstoneCragsBiome.cs
@@ -11,6 +11,7 @@ namespace CalamityMod.BiomeManagers
 
         public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
         public override string BestiaryIcon => "CalamityMod/BiomeManagers/BrimstoneCragsIcon";
+        public override string BackgroundPath => "Terraria/Images/MapBG3";
 
         public override void SetStaticDefaults()
         {
--- Scenes/AbyssLayer1Scene.cs
+++ Scenes/AbyssLayer1Scene.cs
@@ -5,7 +5,7 @@ namespace CalamityMod.Systems
 {
     public class AbyssLayer1Scene : ModSceneEffect
     {
-        public override bool IsSceneEffectActive(Player player) => player.Calamity().ZoneAbyssLayer1;
+        public override bool IsSceneEffectActive(Player player) => player.Calamity().ZoneAbyssLayer1 || player.Calamity().ZoneSunkenSea; // Placeholder for the Sunken Sea
 
         public override string MapBackground => "CalamityMod/Backgrounds/MapBackgrounds/AbyssBGLayer1";
     }
