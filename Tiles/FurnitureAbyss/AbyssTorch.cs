dba1ecd7c2bae7111f4d35d35e6da07102cf152c
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-08-24 19:56:24 -0400

    brightened Abyss torch

--- Tiles/FurnitureAbyss/AbyssTorch.cs
+++ Tiles/FurnitureAbyss/AbyssTorch.cs
@@ -46,9 +46,9 @@ namespace CalamityMod.Tiles.FurnitureAbyss
             Tile tile = Main.tile[i, j];
             if (tile.TileFrameX < 66)
             {
-                r = 0.45f;
-                g = 0.45f;
-                b = 1.8f;
+                r = 0.6f;
+                g = 0.6f;
+                b = 2.3f;
             }
         }
 
