88562f1be146aad6704cb4f3753feac9f44ab560
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-09-06 21:33:48 -0400

    added ritual candles and monolith assets

---
+++ Items/Placeables/FurnitureSacrilegious/LargeRitualCandle.cs
@@ -0,0 +1,59 @@
+using CalamityMod.Items.Materials;
+using CalamityMod.Rarities;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using CalamityMod.Tiles.FurnitureSacrilegious;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace CalamityMod.Items.Placeables.FurnitureSacrilegious
+{
+    public class LargeRitualCandle : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Large Ritual Candle");
+            Tooltip.SetDefault("Right click to place in a different style");
+            SacrificeTotal = 1;
+            ItemID.Sets.ItemsThatAllowRepeatedRightClick[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 50;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<LargeRitualCandleTile>();
+            Item.rare = ModContent.RarityType<Violet>();
+        }
+
+        public override bool AltFunctionUse(Player player) => true;
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == ItemAlternativeFunctionID.ActivatedAndUsed)
+            {
+				Item.placeStyle = 1;
+            }
+            else
+            {
+				Item.placeStyle = 0;
+            }
+            return base.CanUseItem(player);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(1).
+				AddIngredient(ModContent.ItemType<OccultBrickItem>(), 6).
+				AddIngredient(ModContent.ItemType<AshesofCalamity>()).
+				AddTile(ModContent.TileType<CosmicAnvil>()).
+				Register();
+        }
+    }
+}
