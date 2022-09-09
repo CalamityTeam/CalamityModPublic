f0018b71c41d1afa5dd07fd2f4b057536d364aa5
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-09-08 23:57:11 -0400

    spelling failure

--- Scenes/MusicScenes/LeviathanStartMusicScene.cs
+++ Scenes/MusicScenes/LeviathanStartMusicScene.cs
@@ -10,7 +10,7 @@ namespace CalamityMod.Systems
         public override SceneEffectPriority Priority => SceneEffectPriority.BossLow;
 
         public override int NPCType => ModContent.NPCType<LeviathanStart>();
-        public override int? MusicModMusic => CalamityMod.Instance.GetMusicFromMusicMod("AnahitaPreBoss");
+        public override int? MusicModMusic => CalamityMod.Instance.GetMusicFromMusicMod("AnahitaPreboss");
         public override int VanillaMusic => -1;
         public override int OtherworldMusic => -1;
         public override int MusicDistance => 1600;
