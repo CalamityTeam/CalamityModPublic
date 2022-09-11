b57ee43bb822a380871bef87334f56549c76aa27
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-09-11 16:01:54 -0400

    SCal now teleports to the center of the arena at the start of acceptance

--- NPCs/SupremeCalamitas/SupremeCalamitas.cs
+++ NPCs/SupremeCalamitas/SupremeCalamitas.cs
@@ -1034,6 +1034,13 @@ namespace CalamityMod.NPCs.SupremeCalamitas
                     NPC.noGravity = false;
                     NPC.damage = 0;
 
+                    // Teleport back to the arena on defeat
+                    if (giveUpCounter == 1200)
+                    {
+                        Dust.QuickDustLine(NPC.Center, initialRitualPosition, 500f, Color.Red);
+                        NPC.Center = initialRitualPosition;
+                    }
+
                     if (!canDespawn)
                         NPC.velocity.X *= 0.96f;
 
@@ -2539,10 +2546,6 @@ namespace CalamityMod.NPCs.SupremeCalamitas
 
         public override void OnKill()
         {
-            // Create a teleport line effect
-            Dust.QuickDustLine(NPC.Center, initialRitualPosition, 500f, Color.Red);
-            NPC.Center = initialRitualPosition;
-
             CalamityGlobalNPC.SetNewBossJustDowned(NPC);
 
             // Increase the player's SCal kill count
@@ -2584,10 +2587,11 @@ namespace CalamityMod.NPCs.SupremeCalamitas
                 normalOnly.Add(DropHelper.PerPlayer(ModContent.ItemType<Calamity>()));
 
                 // SCal vanity set (This drops all at once, or not at all)
-                normalOnly.Add(ItemDropRule.Common(ModContent.ItemType<AshenHorns>(), 7).
-                    OnSuccess(ItemDropRule.Common(ModContent.ItemType<SCalMask>())).
-                    OnSuccess(ItemDropRule.Common(ModContent.ItemType<SCalRobes>())).
-                    OnSuccess(ItemDropRule.Common(ModContent.ItemType<SCalBoots>())));
+				var scalVanitySet = ItemDropRule.Common(ModContent.ItemType<AshenHorns>(), 7);
+				scalVanitySet.OnSuccess(ItemDropRule.Common(ModContent.ItemType<SCalMask>()));
+				scalVanitySet.OnSuccess(ItemDropRule.Common(ModContent.ItemType<SCalRobes>()));
+				scalVanitySet.OnSuccess(ItemDropRule.Common(ModContent.ItemType<SCalBoots>()));
+				normalOnly.Add(scalVanitySet);
             }
 
             // One of the only Death-exclusive drops in the mod, as requested by Leviathan: Levi pet and Gael's Greatsword
@@ -2859,7 +2863,7 @@ namespace CalamityMod.NPCs.SupremeCalamitas
                 NPC.position.Y = NPC.position.Y - (NPC.height / 2);
                 for (int num621 = 0; num621 < 40; num621++)
                 {
-                    int num622 = Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, (int)CalamityDusts.Brimstone, 0f, 0f, 100, default, 2f);
+                    int num622 = Dust.NewDust(NPC.position, NPC.width, NPC.height, (int)CalamityDusts.Brimstone, 0f, 0f, 100, default, 2f);
                     Main.dust[num622].velocity *= 3f;
                     if (Main.rand.NextBool(2))
                     {
@@ -2869,10 +2873,10 @@ namespace CalamityMod.NPCs.SupremeCalamitas
                 }
                 for (int num623 = 0; num623 < 70; num623++)
                 {
-                    int num624 = Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, (int)CalamityDusts.Brimstone, 0f, 0f, 100, default, 3f);
+                    int num624 = Dust.NewDust(NPC.position, NPC.width, NPC.height, (int)CalamityDusts.Brimstone, 0f, 0f, 100, default, 3f);
                     Main.dust[num624].noGravity = true;
                     Main.dust[num624].velocity *= 5f;
-                    num624 = Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, (int)CalamityDusts.Brimstone, 0f, 0f, 100, default, 2f);
+                    num624 = Dust.NewDust(NPC.position, NPC.width, NPC.height, (int)CalamityDusts.Brimstone, 0f, 0f, 100, default, 2f);
                     Main.dust[num624].velocity *= 2f;
                 }
             }
