74c6314853e0eb423cd964cb7942c3dce5833cae
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-09-20 11:06:02 -0400

    Hadarian Wing tooltip now gets the actual key inputs for the tooltip

--- Items/Accessories/Wings/HadarianWings.cs
+++ Items/Accessories/Wings/HadarianWings.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameInput;
 using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -36,7 +37,10 @@ namespace CalamityMod.Items.Accessories.Wings
         }
         public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
-			string hoverText = Player.Settings.HoverControl == Player.Settings.HoverControlMode.Hold ? "Hold DOWN and JUMP to hover" : "Press DOWN to toggle hover\nPress UP to deactivate hover";
+			string down = PlayerInput.GenerateInputTag_ForCurrentGamemode(true, "Down");
+			string jump = PlayerInput.GenerateInputTag_ForCurrentGamemode(true, "Jump");
+			string up = PlayerInput.GenerateInputTag_ForCurrentGamemode(true, "Up");
+			string hoverText = Player.Settings.HoverControl == Player.Settings.HoverControlMode.Hold ? "Hold " + down + " and " + jump + " to hover" : "Press " + down + " to toggle hover\nPress " + up + " to deactivate hover";
 
 			TooltipLine hover = tooltips.FirstOrDefault(x => x.Mod == "Terraria" && x.Name == "Tooltip1");
 			if (hover != null)
