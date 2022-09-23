82ff933866fbf8a70a21d8f0124ba28596da11bb
GinYuH <unknown>
2022-09-23 07:33:45 -0700

    block resprite

--- Tiles/ScoriaBrick.cs
+++ Tiles/ScoriaBrick.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
 using Terraria.GameContent.Metadata;
 using Terraria.ModLoader;
@@ -11,8 +13,13 @@ namespace CalamityMod.Tiles
     {
         int subsheetHeight = 72;
         int subsheetWidth = 216;
+        internal static Texture2D GlowTexture;
         public override void SetStaticDefaults()
         {
+            if (!Main.dedServ)
+            {
+                GlowTexture = ModContent.Request<Texture2D>("CalamityMod/Tiles/ScoriaBrickGlow", AssetRequestMode.ImmediateLoad).Value;
+            }
             Main.tileLighted[Type] = true;
             Main.tileSolid[Type] = true;
             Main.tileBlockLight[Type] = true;
@@ -36,17 +43,37 @@ namespace CalamityMod.Tiles
 
         public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset)
         {
-            int xPos = i % 1;
-            int yPos = j % 2;
-
-            frameXOffset = xPos * subsheetWidth;
-            frameYOffset = yPos * subsheetHeight;
+            int frameOffset = (i + j) % 2;
+            frameYOffset = frameOffset * AnimationFrameHeight;
         }
 
         public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
         {
             TileFraming.CompactFraming(i, j, resetFrame);
             return false;
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            // If the cached textures don't exist for some reason, don't bother using them.
+            if (GlowTexture is null)
+                return;
+
+            Tile tile = CalamityUtils.ParanoidTileRetrieval(i, j);
+            int xPos = tile.TileFrameX;
+            int frameOffset = (i + j) % 2 * AnimationFrameHeight;
+            int yPos = tile.TileFrameY + frameOffset;
+            Vector2 drawOffset = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+            Vector2 drawPosition = new Vector2(i * 16 - Main.screenPosition.X, j * 16 - Main.screenPosition.Y) + drawOffset;
+
+            if (!tile.IsHalfBlock && tile.Slope == 0)
+            {
+                spriteBatch.Draw(GlowTexture, drawPosition, new Rectangle?(new Rectangle(xPos, yPos, 18, 18)), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            }
+            else if (tile.IsHalfBlock)
+            {
+                spriteBatch.Draw(GlowTexture, drawPosition + new Vector2(0f, 8f), new Rectangle?(new Rectangle(xPos, yPos, 18, 8)), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            }
         }
     }
 }
