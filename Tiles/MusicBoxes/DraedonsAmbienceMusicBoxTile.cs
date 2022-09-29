5d8025031e82d010dfaa2e8d837a426c5a011502
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-09-29 17:06:36 -0400

    draedon's music box now emits some music note "gores"

--- Tiles/MusicBoxes/DraedonsAmbienceMusicBoxTile.cs
+++ Tiles/MusicBoxes/DraedonsAmbienceMusicBoxTile.cs
@@ -1,10 +1,11 @@
 using CalamityMod.Items.Placeables.MusicBoxes;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
-using Terraria.DataStructures;
 
 namespace CalamityMod.Tiles.MusicBoxes
 {
@@ -37,5 +38,27 @@ namespace CalamityMod.Tiles.MusicBoxes
             player.cursorItemIconEnabled = true;
             player.cursorItemIconID = ModContent.ItemType<DraedonsAmbienceMusicBox>();
         }
+
+		public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+		{
+			if (Main.gamePaused || !Main.instance.IsActive || Lighting.UpdateEveryFrame && !Main.rand.NextBool(4))
+				return;
+
+			if (Main.tile[i, j].TileFrameX == 36 && Main.tile[i, j].TileFrameY % 36 == 0 && (int)Main.timeForVisualEffects % 7 == 0 && Main.rand.NextBool(3))
+			{
+				int goreType = Main.rand.Next(570, 573);
+				Vector2 position = new Vector2(i * 16 + 8, j * 16 - 8);
+				Vector2 velocity = new Vector2(Main.WindForVisuals * 2f, -0.5f);
+				velocity.X *= 1f + Main.rand.NextFloat(-0.5f, 0.5f);
+				velocity.Y *= 1f + Main.rand.NextFloat(-0.5f, 0.5f);
+				if (goreType == 572)
+					position.X -= 8f;
+
+				if (goreType == 571)
+					position.X -= 4f;
+
+				Gore.NewGore(new EntitySource_TileUpdate(i, j), position, velocity, goreType, 0.8f);
+			}
+		}
     }
 }
