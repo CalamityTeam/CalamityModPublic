8e7cf8a128862547c476210449ca3026d4070a30
Dominic <farfetchedking541@gmail.com>
2022-10-01 17:05:49 -0500

    Requested SSV changes by testers

--- Projectiles/Magic/EnormousConsumingVortex.cs
+++ Projectiles/Magic/EnormousConsumingVortex.cs
@@ -23,6 +23,14 @@ namespace CalamityMod.Projectiles.Magic
             set => Projectile.ai[0] = value;
         }
 
+        public bool HasBeenReleased
+        {
+            get => Projectile.ai[1] == 1f;
+            set => Projectile.ai[1] = value.ToInt();
+        }
+
+        public const int ExplodeTime = 45;
+
         public const float StartingScale = 0.0004f;
 
         public const float IdealScale = 2.7f;
@@ -55,8 +63,26 @@ namespace CalamityMod.Projectiles.Magic
 
         public override void AI()
         {
-            // If the player is no longer able to channel the vortex, kill it.
-            if (!Owner.channel || Owner.noItems || Owner.CCed)
+            // If the player is no longer able to channel the vortex and it hasn't been released yet, release it.
+            if ((!Owner.channel || Owner.noItems || Owner.CCed) && !HasBeenReleased)
+            {
+                if (Time >= SubsumingVortex.LargeVortexChargeupTime)
+                {
+                    if (Main.myPlayer == Projectile.owner)
+                    {
+                        Projectile.velocity = Projectile.SafeDirectionTo(Main.MouseWorld) * SubsumingVortex.ReleaseSpeed;
+                        Projectile.damage = (int)(Projectile.damage * SubsumingVortex.ReleaseDamageFactor);
+                        HasBeenReleased = true;
+                        Projectile.netUpdate = true;
+                    }
+                }
+                else
+                    Projectile.Kill();
+                return;
+            }
+
+            // Disappear if released, too far from the target, and hasn't exploded yet.
+            if (HasBeenReleased && Projectile.timeLeft > ExplodeTime && !Projectile.WithinRange(Owner.Center, 2000f))
             {
                 Projectile.Kill();
                 return;
@@ -74,9 +100,9 @@ namespace CalamityMod.Projectiles.Magic
                     GeneralParticleHandler.SpawnParticle(exoEnergy);
                 }
 
-                // Fire vortices at nearby target.
+                // Fire vortices at nearby target if not fully charged yet.
                 NPC potentialTarget = Projectile.Center.ClosestNPCAt(SubsumingVortex.SmallVortexTargetRange - 100f);
-                if (potentialTarget != null && Time % SubsumingVortex.VortexReleaseRate == SubsumingVortex.VortexReleaseRate - 1)
+                if (potentialTarget != null && Time % SubsumingVortex.VortexReleaseRate == SubsumingVortex.VortexReleaseRate - 1 && Time < SubsumingVortex.LargeVortexChargeupTime)
                 {
                     // CheckMana returns true if the mana cost can be paid..
                     bool allowContinuedUse = Owner.CheckMana(Owner.ActiveItem(), -1, true, false);
@@ -98,7 +124,7 @@ namespace CalamityMod.Projectiles.Magic
             }
 
             // Create swirling exo energy.
-            if (Main.rand.NextBool())
+            if (Main.rand.NextBool() && Projectile.Opacity > 0.6f)
             {
                 float dustSpawnChance = Utils.Remap(Time, 12f, SubsumingVortex.VortexShootDelay + 8f, 0.25f, 0.7f);
                 for (int i = 0; i < 3; i++)
@@ -108,21 +134,25 @@ namespace CalamityMod.Projectiles.Magic
                         float spawnOffsetAngle = Main.rand.NextFloat(MathHelper.TwoPi);
                         float hue = (float)Math.Sin(spawnOffsetAngle + Time / 26f) * 0.5f + 0.5f;
                         float spawnOffsetFactor = Main.rand.NextFloat(0.3f, 0.95f);
+                        float energyScale = Projectile.scale * Main.rand.NextFloat(0.18f, 0.3f);
+                        if (energyScale > 1f)
+                            energyScale = 1f;
+
                         Vector2 energySpawnPosition = Projectile.Center + spawnOffsetAngle.ToRotationVector2() * Projectile.Size * spawnOffsetFactor;
                         Vector2 energyVelocity = (spawnOffsetAngle - MathHelper.PiOver2).ToRotationVector2() * (Main.rand.NextFloat(5f, 10f) * spawnOffsetFactor) * dustSpawnChance;
                         Color energyColor = CalamityUtils.MulticolorLerp(hue, CalamityUtils.ExoPalette);
-                        SquishyLightParticle exoEnergy = new(energySpawnPosition, energyVelocity, Projectile.scale * Main.rand.NextFloat(0.18f, 0.3f), energyColor, 32, 1f, 1.5f);
+                        SquishyLightParticle exoEnergy = new(energySpawnPosition, energyVelocity, energyScale, energyColor, 32, 1f, 1.5f);
                         GeneralParticleHandler.SpawnParticle(exoEnergy);
                     }
                 }
             }
 
-            // Hover above the target.
-            if (Main.myPlayer == Projectile.owner)
+            // Hover in front of the owner.
+            if (Main.myPlayer == Projectile.owner && !HasBeenReleased)
             {
                 // Smoothly approach a sinusoidal offset as time goes on.
-                float verticalOffset = Utils.Remap(Time, 0f, 90f, -30f, (float)Math.Cos(Projectile.timeLeft / 32f) * 30f - 325f);
-                Vector2 hoverDestination = Owner.Top + Vector2.UnitY * verticalOffset;
+                float verticalOffset = Utils.Remap(Time, 0f, 90f, -30f, (float)Math.Cos(Projectile.timeLeft / 32f) * 30f);
+                Vector2 hoverDestination = Owner.Top + new Vector2(Owner.direction * Projectile.scale * 30f, verticalOffset);
                 hoverDestination += (Main.MouseWorld - hoverDestination) * SubsumingVortex.GiantVortexMouseDriftFactor;
 
                 Vector2 idealVelocity = Vector2.Zero.MoveTowards(hoverDestination - Projectile.Center, 32f);
@@ -132,9 +162,14 @@ namespace CalamityMod.Projectiles.Magic
             }
 
             // Re-determine the hitbox size.
-            Projectile.Opacity = Utils.GetLerpValue(0f, 20f, Time, true);
-            Projectile.scale = Utils.Remap(Time, 0f, 45f, StartingScale, IdealScale);
+            Projectile.Opacity = Utils.GetLerpValue(0f, 20f, Time, true) * Utils.GetLerpValue(0f, ExplodeTime, Projectile.timeLeft, true);
+            Projectile.scale = Utils.Remap(Time, 0f, SubsumingVortex.LargeVortexChargeupTime, StartingScale, IdealScale);
+            Projectile.scale *= Utils.Remap(Projectile.timeLeft, ExplodeTime, 1f, 1f, 5.4f);
             Projectile.ExpandHitboxBy((int)(Projectile.scale * 62f));
+
+            // Slow down.
+            if (Projectile.timeLeft < ExplodeTime)
+                Projectile.velocity *= 0.8f;
 
             AdjustPlayerValues();
 
@@ -147,6 +182,16 @@ namespace CalamityMod.Projectiles.Magic
             Owner.heldProj = Projectile.whoAmI;
             Owner.itemTime = 2;
             Owner.itemAnimation = 2;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (!HasBeenReleased || Projectile.timeLeft < ExplodeTime)
+                return;
+
+            SoundEngine.PlaySound(SubsumingVortex.ExplosionSound with { Volume = 1.3f }, Projectile.Center);
+            Projectile.timeLeft = ExplodeTime;
+            Projectile.netUpdate = true;
         }
 
         // Draw these vortices behind other projectiles to ensure that they do not obstruct SCal's projectiles.
@@ -170,7 +215,7 @@ namespace CalamityMod.Projectiles.Magic
             {
                 float spinDirection = (i % 2f == 0f).ToDirectionInt();
                 Vector2 drawOffset = (MathHelper.TwoPi * i / CalamityUtils.ExoPalette.Length + Main.GlobalTimeWrappedHourly * spinDirection * 4f).ToRotationVector2() * Projectile.scale * 15f;
-                Main.spriteBatch.Draw(worleyNoise, drawPosition + drawOffset, null, CalamityUtils.ExoPalette[i], spinDirection * spinRotation, worleyNoise.Size() * 0.5f, scale, 0, 0f);
+                Main.spriteBatch.Draw(worleyNoise, drawPosition + drawOffset, null, CalamityUtils.ExoPalette[i] * Projectile.Opacity, spinDirection * spinRotation, worleyNoise.Size() * 0.5f, scale, 0, 0f);
             }
 
             Main.spriteBatch.ExitShaderRegion();
