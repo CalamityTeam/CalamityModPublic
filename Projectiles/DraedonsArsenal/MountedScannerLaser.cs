40dad6f33e9850434dce4ade6bd00fdc9251106f
Dominic <farfetchedking541@gmail.com>
2022-10-08 23:37:50 -0500

    What the hell

--- Projectiles/DraedonsArsenal/MountedScannerLaser.cs
+++ Projectiles/DraedonsArsenal/MountedScannerLaser.cs
@@ -95,5 +95,15 @@ namespace CalamityMod.Projectiles.DraedonsArsenal
                 }
             }
         }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Projectile.damage = (int)(Projectile.damage * 0.6);
+        }
+
+        public override void OnHitPvp(Player target, int damage, bool crit)
+        {
+            Projectile.damage = (int)(Projectile.damage * 0.6);
+        }
     }
 }
