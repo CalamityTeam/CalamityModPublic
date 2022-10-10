dab096b7bd1f0104f4dd5ac5f5f003fc8d365019
ben-tk <45545085+ben-tk@users.noreply.github.com>
2022-10-10 15:04:31 -0400

    Fixed Calamitas showing the Shop icon with the Dialogue Panel Rework mod

--- ModSupport/WeakReferenceSupport.cs
+++ ModSupport/WeakReferenceSupport.cs
@@ -172,6 +172,7 @@ namespace CalamityMod
             BossChecklistSupport();
             FargosSupport();
             CensusSupport();
+            DialogueTweakSupport();
             SummonersAssociationSupport();
         }
 
@@ -1040,6 +1041,15 @@ namespace CalamityMod
                 censusMod.Call("TownNPCCondition", NPCType<FAP>(), "Have [i:" + ItemType<FabsolsVodka>() + "] in your inventory in Hardmode");
                 censusMod.Call("TownNPCCondition", NPCType<DILF>(), "Defeat Cryogen");
                 censusMod.Call("TownNPCCondition", NPCType<WITCH>(), "Defeat Supreme Calamitas");
+            }
+        }
+
+        private static void DialogueTweakSupport()
+        {
+            Mod dialogueMod = GetInstance<CalamityMod>().dialogueTweak;
+            if (dialogueMod != null)
+            {
+                dialogueMod.Call("ReplaceShopButtonIcon", NPCType<WITCH>(), "Head");
             }
         }
 
