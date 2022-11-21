858d6948170e344038036582b0f3e48331404541
Altixal <73398287+Altixal@users.noreply.github.com>
2022-11-22 06:59:05 +0700

    Pissed the proper way

--- Projectiles/Magic/EvergladeSprayProjectile.cs
+++ Projectiles/Magic/EvergladeSprayProjectile.cs
@@ -44,8 +44,7 @@ namespace CalamityMod.Projectiles.Magic
             {
                 float num152 = Projectile.velocity.X / 3f * (float)num151;
                 float num153 = Projectile.velocity.Y / 3f * (float)num151;
-                int num154 = 14;
-                int num155 = Dust.NewDust(new Vector2(Projectile.position.X + (float)num154, Projectile.position.Y + (float)num154), Projectile.width - num154 * 2, Projectile.height - num154 * 2, 157, 0f, 0f, 100, default, 1f);
+                int num155 = Dust.NewDust(Projectile.Center, Projectile.width, Projectile.height, 157, 0f, 0f, 100, default, 1f);
                 Dust dust = Main.dust[num155];
                 dust.noGravity = true;
                 dust.velocity *= 0.1f;
@@ -55,8 +54,7 @@ namespace CalamityMod.Projectiles.Magic
             }
             if (Main.rand.NextBool(8))
             {
-                int num156 = 16;
-                int num157 = Dust.NewDust(new Vector2(Projectile.position.X + (float)num156, Projectile.position.Y + (float)num156), Projectile.width - num156 * 2, Projectile.height - num156 * 2, 157, 0f, 0f, 100, default, 0.5f);
+                int num157 = Dust.NewDust(Projectile.Center, Projectile.width, Projectile.height, 157, 0f, 0f, 100, default, 0.5f);
                 Main.dust[num157].velocity *= 0.25f;
                 Main.dust[num157].velocity += Projectile.velocity * 0.5f;
             }
