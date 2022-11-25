9d14116b9a60a1f4c6ca76c68c05128567c181fd
Altixal <73398287+Altixal@users.noreply.github.com>
2022-11-25 16:33:35 +0700

    Hallowed Ores spawned from a greater height

--- NPCs/CalamityGlobalNPCLoot.cs
+++ NPCs/CalamityGlobalNPCLoot.cs
@@ -1829,7 +1829,7 @@ namespace CalamityMod.NPCs
             {
                 string key = "Mods.CalamityMod.HardmodeOreTier4Text";
                 Color messageColor = new Color(50, 255, 130);
-                CalamityUtils.SpawnOre(ModContent.TileType<HallowedOre>(), 12E-05, 0.7f, 0.9f, 3, 8, TileID.Pearlstone, TileID.HallowHardenedSand, TileID.HallowSandstone, TileID.HallowedIce);
+                CalamityUtils.SpawnOre(ModContent.TileType<HallowedOre>(), 12E-05, 0.55f, 0.9f, 3, 8, TileID.Pearlstone, TileID.HallowHardenedSand, TileID.HallowSandstone, TileID.HallowedIce);
                 CalamityUtils.DisplayLocalizedText(key, messageColor);
             }
         }
