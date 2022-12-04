68372b6d27e4e7e760b93e01f867648c889319a0
mountaindrew10 <marhaj04@gmail.com>
2022-12-04 03:09:43 -0600

    Buffed Contaminated Bile Irradiated duration to 8 seconds.

--- Projectiles/Rogue/BileExplosion.cs
+++ Projectiles/Rogue/BileExplosion.cs
@@ -1,4 +1,4 @@
-using CalamityMod.Dusts;
+﻿using CalamityMod.Dusts;
 using CalamityMod.Buffs.StatDebuffs;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -27,13 +27,16 @@ namespace CalamityMod.Projectiles.Rogue
 
         public override void AI()
         {
-            //projectile.ai[0] == 1f means spawned by Skyfin Bombers SS
+            // projectile.ai[0] == 1f means spawned by Skyfin Bombers SS
             Projectile.position = Projectile.Center;
+
             if (Projectile.Calamity().stealthStrike || Projectile.ai[0] == 1f)
                 Projectile.width = Projectile.height = 120;
             else
                 Projectile.width = Projectile.height = 70;
+
             Projectile.position -= Projectile.Size / 2f;
+
             for (int i = 0; i < 15; i++)
             {
                 Dust dust = Dust.NewDustPerfect(Projectile.Center, (int)CalamityDusts.SulfurousSeaAcid);
@@ -46,15 +49,14 @@ namespace CalamityMod.Projectiles.Rogue
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
             if (Projectile.ai[0] != 1f)
-            {
                 target.immune[Projectile.owner] = 9;
-            }
-            target.AddBuff(ModContent.BuffType<Irradiated>(), 180);
+
+            target.AddBuff(ModContent.BuffType<Irradiated>(), 480);
         }
 
         public override void OnHitPvp(Player target, int damage, bool crit)
         {
-            target.AddBuff(ModContent.BuffType<Irradiated>(), 180);
+            target.AddBuff(ModContent.BuffType<Irradiated>(), 480);
         }
     }
 }
