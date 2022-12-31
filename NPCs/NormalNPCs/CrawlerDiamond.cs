d30eac4a9a334d1e473660deae25beae5f8ad0ca
Altixal <73398287+Altixal@users.noreply.github.com>
2022-12-31 10:08:47 +0700

    gores

--- NPCs/NormalNPCs/CrawlerDiamond.cs
+++ NPCs/NormalNPCs/CrawlerDiamond.cs
@@ -113,6 +113,13 @@ namespace CalamityMod.NPCs.NormalNPCs
                 {
                     Dust.NewDust(NPC.position, NPC.width, NPC.height, 91, hitDirection, -1f, 0, default, 1f);
                 }
+                if (Main.netMode != NetmodeID.Server)
+                {
+                    Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("CrawlerDiamond").Type, 1f);
+                    Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("CrawlerDiamond2").Type, 1f);
+                    Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("Crawler").Type, 1f);
+                    Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("Crawler2").Type, 1f);
+                }
             }
         }
 
