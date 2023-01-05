ef599849b90d49f4fbd774a59e7bcd7dcc529582
Altixal <73398287+Altixal@users.noreply.github.com>
2023-01-05 11:57:48 +0700

    Hydra cleanup (a bit lazy for a rework rn)

--- Items/Weapons/Ranged/Hydra.cs
+++ Items/Weapons/Ranged/Hydra.cs
@@ -1,6 +1,5 @@
 ﻿using CalamityMod.CalPlayer;
-using CalamityMod.Items.Ammo.FiniteUse;
-using CalamityMod.Projectiles.Typeless.FiniteUse;
+using CalamityMod.Projectiles.Ranged;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
@@ -8,7 +7,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Audio;
 
-namespace CalamityMod.Items.Weapons.Typeless.FiniteUse
+namespace CalamityMod.Items.Weapons.Ranged
 {
     public class Hydra : ModItem
     {
@@ -17,14 +16,14 @@ namespace CalamityMod.Items.Weapons.Typeless.FiniteUse
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Hydra");
-            Tooltip.SetDefault("Uses Explosive Shotgun Shells\n" +
-                "Can be used once per boss battle");
+            Tooltip.SetDefault("Fires a spread of explosive bullets");
             SacrificeTotal = 1;
         }
 
         public override void SetDefaults()
         {
             Item.damage = 120;
+            Item.DamageType = DamageClass.Ranged;
             Item.width = 66;
             Item.height = 30;
             Item.useTime = 33;
@@ -37,39 +36,14 @@ namespace CalamityMod.Items.Weapons.Typeless.FiniteUse
             Item.UseSound = FireSound;
             Item.autoReuse = true;
             Item.shootSpeed = 12f;
-            Item.shoot = ModContent.ProjectileType<ExplosiveShotgunShell>();
-            Item.useAmmo = ModContent.ItemType<ExplosiveShells>();
-            if (CalamityPlayer.areThereAnyDamnBosses)
-                Item.Calamity().timesUsed = 1;
-        }
-
-		public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup)
-		{
-			itemGroup = (ContentSamples.CreativeHelper.ItemGroup)CalamityResearchSorting.ClasslessWeapon;
-		}
-
-        public override bool OnPickup(Player player)
-        {
-            if (CalamityPlayer.areThereAnyDamnBosses)
-                Item.Calamity().timesUsed = 1;
-
-            return true;
-        }
-
-        public override bool CanUseItem(Player player)
-        {
-            return Item.Calamity().timesUsed < 1;
+            Item.shoot = ModContent.ProjectileType<RealmRavagerBullet>();
+            Item.useAmmo = AmmoID.Bullet;
+            Item.Calamity().canFirePointBlankShots = true;
         }
 
         public override Vector2? HoldoutOffset()
         {
             return new Vector2(-5, 0);
-        }
-
-        public override void UpdateInventory(Player player)
-        {
-            if (!CalamityPlayer.areThereAnyDamnBosses)
-                Item.Calamity().timesUsed = 0;
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
@@ -80,17 +54,6 @@ namespace CalamityMod.Items.Weapons.Typeless.FiniteUse
                 float SpeedY = velocity.Y + Main.rand.Next(-40, 41) * 0.05f;
                 Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
             }
-
-            if (CalamityPlayer.areThereAnyDamnBosses)
-            {
-                player.HeldItem.Calamity().timesUsed++;
-                for (int i = 0; i < Main.InventorySlotsTotal; i++)
-                {
-                    if (player.inventory[i].type == Item.type && player.inventory[i] != player.HeldItem)
-                        player.inventory[i].Calamity().timesUsed++;
-                }
-            }
-
             return false;
         }
 
