8ef31a68d5353e253f5ad0067a22464e9bce6138
Memes <114233643+Mr-Memes@users.noreply.github.com>
2023-01-06 16:07:42 +0100

    Improved Cinder Blossom Staff.

--- Projectiles/Summon/Cinder.cs
+++ Projectiles/Summon/Cinder.cs
@@ -1,18 +1,17 @@
-﻿using CalamityMod.Buffs.DamageOverTime;
+﻿using System.Security.Cryptography.X509Certificates;
 using Microsoft.Xna.Framework;
-using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+
 namespace CalamityMod.Projectiles.Summon
 {
     public class Cinder : ModProjectile
     {
+        public Player Owner => Main.player[Projectile.owner];
+        
         public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
 
-        public const float FallAcceleration = 0.185f;
-        public const float FallSpeedMax = 16;
-        public const float FallDelay = 300;
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Cinder");
@@ -21,9 +20,10 @@ namespace CalamityMod.Projectiles.Summon
 
         public override void SetDefaults()
         {
-            Projectile.width = 6;
+            Projectile.width = 12;
             Projectile.height = 12;
             Projectile.friendly = true;
+            Projectile.tileCollide = false;
             Projectile.penetrate = 3;
             Projectile.timeLeft = 300;
             Projectile.usesIDStaticNPCImmunity = true;
@@ -34,61 +34,17 @@ namespace CalamityMod.Projectiles.Summon
 
         public override void AI()
         {
-            if (Projectile.velocity.X != Projectile.velocity.X)
-            {
-                Projectile.velocity.X *= -0.1f;
-            }
-            if (Projectile.velocity.X != Projectile.velocity.X)
-            {
-                Projectile.velocity.X *= -0.5f;
-            }
-            if (Projectile.velocity.Y != Projectile.velocity.Y && Projectile.velocity.Y > 1f)
-            {
-                Projectile.velocity.Y *= -0.5f;
-            }
-            Projectile.ai[0]++;
-            if (Projectile.ai[0] > 5f)
-            {
-                Projectile.ai[0] = 5f;
-                if (Projectile.velocity.Y == 0f && Projectile.velocity.X != 0f)
-                {
-                    Projectile.velocity.X *= 0.97f;
-                    if (Math.Abs(Projectile.velocity.X) < 0.01)
-                    {
-                        Projectile.velocity.X = 0f;
-                        Projectile.netUpdate = true;
-                    }
-                }
-            }
+            SpawnDust();
+            Lighting.AddLight(Projectile.Center, Color.Orange.ToVector3());
+        }
 
-            if (Projectile.ai[0] >= FallDelay && Projectile.velocity.Y < FallSpeedMax)
-            {
-                Projectile.velocity.Y += FallAcceleration;
-            }
-
-            Projectile.rotation += Projectile.velocity.X * 0.1f;
-            int idx = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 6, 0f, 0f, 100, default, 1f);
-            Main.dust[idx].position += new Vector2(2f);
-            Main.dust[idx].scale += Main.rand.NextFloat(0.5f);
-            Main.dust[idx].noGravity = true;
-            Main.dust[idx].velocity.Y -= 2f;
-            if (Main.rand.NextBool(2))
-            {
-                idx = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 6, 0f, 0f, 100, default, 1f);
-                Main.dust[idx].position += new Vector2(2f);
-                Main.dust[idx].scale += 0.3f + Main.rand.NextFloat(0.5f);
-                Main.dust[idx].noGravity = true;
-                Main.dust[idx].velocity.Y -= 2f;
-            }
-            if (Projectile.velocity.Y < 0.25f && Projectile.velocity.Y > 0.15f)
-            {
-                Projectile.velocity.X *= 0.8f;
-            }
-            Projectile.rotation = -Projectile.velocity.X * 0.05f;
-            if (Projectile.velocity.Y > 16f)
-            {
-                Projectile.velocity.Y = 16f;
-            }
+        public void SpawnDust()
+        {
+            int bootlegTexture = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 1f);
+            Main.dust[bootlegTexture].position += new Vector2(2f);
+            Main.dust[bootlegTexture].scale += 0.3f + Main.rand.NextFloat(0.5f);
+            Main.dust[bootlegTexture].noGravity = true;
+            Main.dust[bootlegTexture].velocity.Y -= 2f;
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity) => false;
--- Projectiles/Summon/CinderBlossom.cs
+++ Projectiles/Summon/CinderBlossom.cs
@@ -4,15 +4,21 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+
 namespace CalamityMod.Projectiles.Summon
 {
     public class CinderBlossom : ModProjectile
     {
-        public float Time
+        public Player Owner => Main.player[Projectile.owner];
+
+        public CalamityPlayer moddedOwner => Owner.Calamity();
+        
+        public float DelayBetweenShooting
         {
             get => Projectile.ai[0];
             set => Projectile.ai[0] = value;
         }
+
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Cinder Blossom");
@@ -22,73 +28,75 @@ namespace CalamityMod.Projectiles.Summon
 
         public override void SetDefaults()
         {
-            Projectile.width = 36;
-            Projectile.height = 40;
+            Projectile.width = 42;
+            Projectile.height = 42;
             Projectile.netImportant = true;
             Projectile.friendly = true;
             Projectile.ignoreWater = true;
             Projectile.minionSlots = 1f;
-            Projectile.timeLeft = 18000;
             Projectile.penetrate = -1;
             Projectile.tileCollide = false;
-            Projectile.timeLeft *= 5;
             Projectile.minion = true;
             Projectile.DamageType = DamageClass.Summon;
         }
 
         public override void AI()
         {
-            bool isCorrectMinion = Projectile.type == ModContent.ProjectileType<CinderBlossom>();
-            Player player = Main.player[Projectile.owner];
-            CalamityPlayer modPlayer = player.Calamity();
-            player.AddBuff(ModContent.BuffType<CinderBlossomBuff>(), 3600);
-            if (isCorrectMinion)
+            CheckMinionExistance(); // Checks if the minion can still exist.
+            EffectWhenSpawned(); // Makes a dust effect when it spawns
+            
+            NPC potentialTarget = Projectile.Center.MinionHoming(1200f, Owner);
+            if (Projectile.owner == Main.myPlayer)
             {
-                if (player.dead)
+                if (potentialTarget != null)
                 {
-                    modPlayer.cinderBlossom = false;
+                    if (DelayBetweenShooting % 35f == 0f) // Shoots once every 35 frames.
+                    {
+                        Vector2 velocity = CalamityUtils.CalculatePredictiveAimToTarget(Projectile.Center, potentialTarget, 20f);
+                        int p = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, velocity, ModContent.ProjectileType<Cinder>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                        if (Main.projectile.IndexInRange(p))
+                            Main.projectile[p].originalDamage = Projectile.originalDamage;
+                        Projectile.netUpdate = true;
+                    }
+                    DelayBetweenShooting++;
+                    DelayBetweenShooting = (DelayBetweenShooting == 36f) ? 0f : DelayBetweenShooting; // The timer cannot go above 35. 
                 }
-                if (modPlayer.cinderBlossom)
+            }
+
+            Projectile.Center = Owner.Center - Vector2.UnitY * 60f;
+            Projectile.rotation += MathHelper.ToRadians((potentialTarget is not null) ? 3f * (potentialTarget.Center.X - Projectile.Center.X > 0).ToDirectionInt() : 1.5f * Owner.direction);
+            // The projectile normally will spin in the direction of the player.
+            // If an enemy is detected, it will spin faster, in the direction of where the enemy is.
+            Lighting.AddLight(Projectile.Center, Color.Orange.ToVector3());
+        }
+
+        public void CheckMinionExistance()
+        {
+            Owner.AddBuff(ModContent.BuffType<CinderBlossomBuff>(), 1);
+            if (Projectile.type == ModContent.ProjectileType<CinderBlossom>())
+            {
+                if (Owner.dead)
+                {
+                    moddedOwner.cinderBlossom = false;
+                }
+                if (moddedOwner.cinderBlossom)
                 {
                     Projectile.timeLeft = 2;
                 }
             }
-            Projectile.Center = player.Center + Vector2.UnitY * (player.gfxOffY - 60f);
-            if (player.gravDir == -1f)
-                Projectile.position.Y += 120f;
-            Projectile.position.X = (int)Projectile.position.X;
-            Projectile.position.Y = (int)Projectile.position.Y;
-            Projectile.position = Projectile.position.Floor();
-            Projectile.rotation += MathHelper.ToRadians(1.5f) * player.direction;
+        }
 
+        public void EffectWhenSpawned()
+        {
             if (Projectile.localAI[0] == 0f)
             {
-                Initialize(player);
-                Projectile.localAI[0] = 1f;
-            }
-            if (Projectile.owner == Main.myPlayer)
-            {
-                NPC potentialTarget = Projectile.Center.MinionHoming(700f, player);
-                if (potentialTarget != null)
+                for (int i = 0; i < 36; i++)
                 {
-                    if (Time++ % 35f == 34f && Collision.CanHit(Projectile.position, Projectile.width, Projectile.height, potentialTarget.position, potentialTarget.width, potentialTarget.height))
-                    {
-                        Vector2 velocity = Projectile.SafeDirectionTo(potentialTarget.Center) * Main.rand.NextFloat(10f, 18f);
-                        int p = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, velocity, ModContent.ProjectileType<Cinder>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
-                        if (Main.projectile.IndexInRange(p))
-                            Main.projectile[p].originalDamage = Projectile.originalDamage;
-                    }
+                    Dust dust = Dust.NewDustPerfect(Projectile.Center, 6);
+                    dust.noGravity = true;
+                    dust.velocity = Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi) * Main.rand.NextFloat(2f, 6f);
                 }
-            }
-            Lighting.AddLight(Projectile.Center, Color.Orange.ToVector3());
-        }
-        public void Initialize(Player player)
-        {
-            for (int i = 0; i < 36; i++)
-            {
-                Dust dust = Dust.NewDustPerfect(Projectile.Center, 6);
-                dust.noGravity = true;
-                dust.velocity = Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi) * Main.rand.NextFloat(2f, 6f);
+                Projectile.localAI[0] = 1f;
             }
         }
 
