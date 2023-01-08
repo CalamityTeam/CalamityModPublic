f5b257b9666c379fd927b80790ee6f9caa7b715f
GinYuH <>
2023-01-07 20:01:40 -0800

    did this commit or not

--- Projectiles/Typeless/TerminusHoldout.cs
+++ Projectiles/Typeless/TerminusHoldout.cs
@@ -159,7 +159,7 @@ namespace CalamityMod.Projectiles.Typeless
 
         public override bool PreDraw(ref Color lightColor)
         {
-            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+            Texture2D texture = Main.getGoodWorld ? ModContent.Request<Texture2D>("CalamityMod/Items/SummonItems/Terminus_GFB").Value : ModContent.Request<Texture2D>(Texture).Value;
             Vector2 baseDrawPosition = Projectile.Center - Main.screenPosition;
             Vector2 origin = texture.Size() * 0.5f;
             Color baseColor = Color.Lerp(Projectile.GetAlpha(lightColor), Color.White, Utils.GetLerpValue(40f, 120f, Time, true));
