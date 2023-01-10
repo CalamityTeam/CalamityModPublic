6ae3a2189ab2c44784731600b3155fed0d4741f3
Altixal <73398287+Altixal@users.noreply.github.com>
2023-01-10 21:43:48 +0700

    Wulfrum furniture but it doesn't look good

---
+++ Tiles/FurnitureWulfrum/WulfrumBathtub.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+namespace CalamityMod.Tiles.FurnitureWulfrum
+{
+    public class WulfrumBathtub : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            this.SetUpBathtub();
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Bathtub");
+            AddMapEntry(new Color(71, 95, 114), name);
+            AnimationFrameHeight = 54;
+        }
+
+        public override bool CanExplode(int i, int j) => false;
+
+        public override bool CreateDust(int i, int j, ref int type)
+        {
+            Dust.NewDust(new Vector2(i, j) * 16f, 16, 16, 107, 0f, 0f, 1, new Color(255, 255, 255), 1f);
+            return false;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 32, ModContent.ItemType<Items.Placeables.FurnitureWulfrum.WulfrumBathtub>());
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            int xFrameOffset = Main.tile[i, j].TileFrameX;
+            int yFrameOffset = Main.tile[i, j].TileFrameY;
+            Texture2D glowmask = ModContent.Request<Texture2D>("CalamityMod/Tiles/FurnitureWulfrum/WulfrumBathtubGlow").Value;
+            Vector2 drawOffest = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+            Vector2 drawPosition = new Vector2(i * 16 - Main.screenPosition.X, j * 16 - Main.screenPosition.Y) + drawOffest;
+            Color drawColour = Color.White;
+            Tile trackTile = Main.tile[i, j];
+            if (!trackTile.IsHalfBlock && trackTile.Slope == 0)
+                spriteBatch.Draw(glowmask, drawPosition, new Rectangle(xFrameOffset, yFrameOffset, 18, 18), drawColour, 0.0f, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            else if (trackTile.IsHalfBlock)
+                spriteBatch.Draw(glowmask, drawPosition + new Vector2(0f, 8f), new Rectangle(xFrameOffset, yFrameOffset, 18, 8), drawColour, 0.0f, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+        }
+    }
+}
