b80abc81ebd44a2d284a9a3b74ec9cdfcb7b564a
Altixal <73398287+Altixal@users.noreply.github.com>
2023-01-14 10:44:59 +0700

    Added the drill itself to the Cavern Lab

---
+++ Tiles/DraedonStructures/OnyxExcavatorTile.cs
@@ -0,0 +1,33 @@
+using CalamityMod.Items.Mounts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalamityMod.Tiles.DraedonStructures
+{
+    public class OnyxExcavatorTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+            TileObjectData.newTile.Width = 8;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Onyx Excavator");
+            AddMapEntry(new Color(128, 0, 128), name);
+            TileID.Sets.DisableSmartCursor[Type] = true;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 16, ModContent.ItemType<OnyxExcavatorKey>());
+        }
+    }
+}
