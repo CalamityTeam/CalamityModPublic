22e74f3b9954c24424fc100ffa7fc09a24c1f188
AquaSG <macvine9@gmail.com>
2023-01-25 01:30:06 -0500

    onyx turret

---
+++ Projectiles/Turret/OnyxShot.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace CalamityMod.Projectiles.Turret
+{
+    public class OnyxShot : ModProjectile
+    {
+        public bool ableToHit = true;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Onyx Shot");
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 4;
+            Projectile.height = 4;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = 2;
+            Projectile.extraUpdates = 3;
+            Projectile.timeLeft = 60;
+            Projectile.hide = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+        public override void AI()
+        {
+            if (Projectile.localAI[0] > 0f)
+                Projectile.hide = false;
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.localAI[0]++;
+        }
+        public override bool? CanDamage() => ableToHit ? (bool?)null : false;
+        public override Color? GetAlpha(Color drawColor) => Color.White;
+        public override bool PreDraw(ref Color lightColor)
+        {
+            CalamityUtils.DrawAfterimagesFromEdge(Projectile, 0, lightColor);
+            return false;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Projectile.timeLeft = 3;
+            ableToHit = false;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            return true;
+        }
+    }
+}
---
+++ Projectiles/Turret/OnyxShotBuffer.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+
+namespace CalamityMod.Projectiles.Turret
+{
+    public class OnyxShotBuffer : ModProjectile
+    {
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Onyx Shot");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 6;
+            Projectile.height = 6;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.alpha = 255;
+            Projectile.penetrate = 1;
+            Projectile.extraUpdates = 1;
+            Projectile.timeLeft = 1;
+        }
+
+
+        public override void AI()
+        {
+            if (Projectile.localAI[0] == 0f)
+            {
+                SoundEngine.PlaySound(SoundID.Item36 with { Volume = 0.65f }, Projectile.Center);
+
+                Projectile.localAI[0] = 1f;
+            }
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                var source = Main.player[Main.myPlayer].GetSource_FromThis();
+                for (int i = -1; i < 2; i++)
+                    Projectile.NewProjectile(source, Projectile.Center + new Vector2(3f, 0f), Projectile.velocity.RotatedBy(Main.rand.NextFloat(0.035f, 0.11f) * i), ModContent.ProjectileType<OnyxShot>(), Projectile.damage, Projectile.knockBack, Main.myPlayer);
+                Projectile.Kill();
+            }
+        }
+
+        public override bool? CanDamage() => false;
+    }
+}
