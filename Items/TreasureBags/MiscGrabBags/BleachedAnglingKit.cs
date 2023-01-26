f87ddec4f4a3d9d779923e4c7a0348ec3402d624
mountaindrew10 <marhaj04@gmail.com>
2023-01-25 19:50:28 -0600

    Adjusted Angling Kit drop rates due to the new Angler Quest Reward System.

--- Items/TreasureBags/MiscGrabBags/BleachedAnglingKit.cs
+++ Items/TreasureBags/MiscGrabBags/BleachedAnglingKit.cs
@@ -35,36 +35,36 @@ namespace CalamityMod.Items.TreasureBags.MiscGrabBags
         {
             // Different drop rates on Normal and Expert, so define normal first, then expert
             var normalOnly = itemLoot.DefineNormalOnlyDropSet();
-            normalOnly.Add(ItemID.AnglerTackleBag, 18);
-            normalOnly.Add(ItemID.HighTestFishingLine, 12);
-            normalOnly.Add(ItemID.TackleBox, 12);
-            normalOnly.Add(ItemID.AnglerEarring, 12);
-            normalOnly.Add(ItemID.FishermansGuide, 9);
-            normalOnly.Add(ItemID.WeatherRadio, 9);
-            normalOnly.Add(ItemID.Sextant, 9);
-            normalOnly.Add(ItemID.AnglerHat, 4);
-            normalOnly.Add(ItemID.AnglerVest, 4);
-            normalOnly.Add(ItemID.AnglerPants, 4);
-            normalOnly.Add(ItemID.FishingPotion, 4, 2, 3);
-            normalOnly.Add(ItemID.SonarPotion, 4, 2, 3);
-            normalOnly.Add(ItemID.CratePotion, 4, 2, 3);
-            normalOnly.Add(ItemID.GoldenBugNet, 15);
+            normalOnly.Add(ItemID.AnglerTackleBag, 72);
+            normalOnly.Add(ItemID.HighTestFishingLine, 48);
+            normalOnly.Add(ItemID.TackleBox, 48);
+            normalOnly.Add(ItemID.AnglerEarring, 48);
+            normalOnly.Add(ItemID.FishermansGuide, 36);
+            normalOnly.Add(ItemID.WeatherRadio, 36);
+            normalOnly.Add(ItemID.Sextant, 36);
+            normalOnly.Add(ItemID.AnglerHat, 16);
+            normalOnly.Add(ItemID.AnglerVest, 16);
+            normalOnly.Add(ItemID.AnglerPants, 16);
+            normalOnly.Add(ItemID.FishingPotion, 3, 2, 3);
+            normalOnly.Add(ItemID.SonarPotion, 3, 2, 3);
+            normalOnly.Add(ItemID.CratePotion, 3, 2, 3);
+            normalOnly.Add(ItemID.GoldenBugNet, 60);
 
             var expertPlus = itemLoot.DefineConditionalDropSet(new Conditions.IsExpert());
-            expertPlus.Add(ItemID.AnglerTackleBag, 15);
-            expertPlus.Add(ItemID.HighTestFishingLine, 10);
-            expertPlus.Add(ItemID.TackleBox, 10);
-            expertPlus.Add(ItemID.AnglerEarring, 10);
-            expertPlus.Add(ItemID.FishermansGuide, 8);
-            expertPlus.Add(ItemID.WeatherRadio, 8);
-            expertPlus.Add(ItemID.Sextant, 8);
-            expertPlus.Add(ItemID.AnglerHat, 2);
-            expertPlus.Add(ItemID.AnglerVest, 2);
-            expertPlus.Add(ItemID.AnglerPants, 2);
+            expertPlus.Add(ItemID.AnglerTackleBag, 58);
+            expertPlus.Add(ItemID.HighTestFishingLine, 38);
+            expertPlus.Add(ItemID.TackleBox, 38);
+            expertPlus.Add(ItemID.AnglerEarring, 38);
+            expertPlus.Add(ItemID.FishermansGuide, 28);
+            expertPlus.Add(ItemID.WeatherRadio, 28);
+            expertPlus.Add(ItemID.Sextant, 28);
+            expertPlus.Add(ItemID.AnglerHat, 12);
+            expertPlus.Add(ItemID.AnglerVest, 12);
+            expertPlus.Add(ItemID.AnglerPants, 12);
             expertPlus.Add(ItemID.FishingPotion, 2, 2, 3);
             expertPlus.Add(ItemID.SonarPotion, 2, 2, 3);
             expertPlus.Add(ItemID.CratePotion, 2, 2, 3);
-            expertPlus.Add(ItemID.GoldenBugNet, 12);
+            expertPlus.Add(ItemID.GoldenBugNet, 48);
         }
     }
 }
--- Items/TreasureBags/MiscGrabBags/SandyAnglingKit.cs
+++ Items/TreasureBags/MiscGrabBags/SandyAnglingKit.cs
@@ -35,34 +35,34 @@ namespace CalamityMod.Items.TreasureBags.MiscGrabBags
         {
             // Different drop rates on Normal and Expert, so define normal first, then expert
             var normalOnly = itemLoot.DefineNormalOnlyDropSet();
-            normalOnly.Add(ItemID.HighTestFishingLine, 15);
-            normalOnly.Add(ItemID.TackleBox, 15);
-            normalOnly.Add(ItemID.AnglerEarring, 15);
-            normalOnly.Add(ItemID.FishermansGuide, 10);
-            normalOnly.Add(ItemID.WeatherRadio, 10);
-            normalOnly.Add(ItemID.Sextant, 10);
-            normalOnly.Add(ItemID.AnglerHat, 5);
-            normalOnly.Add(ItemID.AnglerVest, 5);
-            normalOnly.Add(ItemID.AnglerPants, 5);
-            normalOnly.Add(ItemID.FishingPotion, 5, 2, 3);
-            normalOnly.Add(ItemID.SonarPotion, 5, 2, 3);
-            normalOnly.Add(ItemID.CratePotion, 5, 2, 3);
-            normalOnly.AddIf(() => NPC.downedBoss3, ItemID.GoldenBugNet, 20);
+            normalOnly.Add(ItemID.HighTestFishingLine, 60);
+            normalOnly.Add(ItemID.TackleBox, 60);
+            normalOnly.Add(ItemID.AnglerEarring, 60);
+            normalOnly.Add(ItemID.FishermansGuide, 40);
+            normalOnly.Add(ItemID.WeatherRadio, 40);
+            normalOnly.Add(ItemID.Sextant, 40);
+            normalOnly.Add(ItemID.AnglerHat, 20);
+            normalOnly.Add(ItemID.AnglerVest, 20);
+            normalOnly.Add(ItemID.AnglerPants, 20);
+            normalOnly.Add(ItemID.FishingPotion, 3, 2, 3);
+            normalOnly.Add(ItemID.SonarPotion, 3, 2, 3);
+            normalOnly.Add(ItemID.CratePotion, 3, 2, 3);
+            normalOnly.AddIf(() => NPC.downedBoss3, ItemID.GoldenBugNet, 80);
 
             var expertPlus = itemLoot.DefineConditionalDropSet(new Conditions.IsExpert());
-            expertPlus.Add(ItemID.HighTestFishingLine, 12);
-            expertPlus.Add(ItemID.TackleBox, 12);
-            expertPlus.Add(ItemID.AnglerEarring, 12);
-            expertPlus.Add(ItemID.FishermansGuide, 9);
-            expertPlus.Add(ItemID.WeatherRadio, 9);
-            expertPlus.Add(ItemID.Sextant, 9);
-            expertPlus.Add(ItemID.AnglerHat, 4);
-            expertPlus.Add(ItemID.AnglerVest, 4);
-            expertPlus.Add(ItemID.AnglerPants, 4);
-            expertPlus.Add(ItemID.FishingPotion, 4, 2, 3);
-            expertPlus.Add(ItemID.SonarPotion, 4, 2, 3);
-            expertPlus.Add(ItemID.CratePotion, 4, 2, 3);
-            expertPlus.AddIf(() => NPC.downedBoss3, ItemID.GoldenBugNet, 18);
+            expertPlus.Add(ItemID.HighTestFishingLine, 48);
+            expertPlus.Add(ItemID.TackleBox, 48);
+            expertPlus.Add(ItemID.AnglerEarring, 48);
+            expertPlus.Add(ItemID.FishermansGuide, 32);
+            expertPlus.Add(ItemID.WeatherRadio, 32);
+            expertPlus.Add(ItemID.Sextant, 32);
+            expertPlus.Add(ItemID.AnglerHat, 16);
+            expertPlus.Add(ItemID.AnglerVest, 16);
+            expertPlus.Add(ItemID.AnglerPants, 16);
+            expertPlus.Add(ItemID.FishingPotion, 2, 2, 3);
+            expertPlus.Add(ItemID.SonarPotion, 2, 2, 3);
+            expertPlus.Add(ItemID.CratePotion, 2, 2, 3);
+            expertPlus.AddIf(() => NPC.downedBoss3, ItemID.GoldenBugNet, 64);
         }
     }
 }
