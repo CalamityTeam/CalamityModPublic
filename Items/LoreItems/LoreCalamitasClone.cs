035d629db6825c043befe5b8ff56321893611983
Memes <114233643+Mr-Memes@users.noreply.github.com>
2023-02-01 07:26:21 +0100

    No fancy apostrophes.

--- Items/LoreItems/LoreCalamitasClone.cs
+++ Items/LoreItems/LoreCalamitasClone.cs
@@ -10,7 +10,7 @@ namespace CalamityMod.Items.LoreItems
     {
         public override string Lore =>
 @"I had seen this monster roaming the night in the past, and thought nothing of it.
-With its technology, it was certainly one of Draedon’s creations.
+With its technology, it was certainly one of Draedon's creations.
 But, to think it was housing a clone of the Witch… Detestable.
 Surely Calamitas would want nothing to do with such a project.
 I know not how it wields her brimstone magic. Perhaps some day one of us may find answers.";
--- Items/LoreItems/LoreDevourerofGods.cs
+++ Items/LoreItems/LoreDevourerofGods.cs
@@ -13,11 +13,11 @@ namespace CalamityMod.Items.LoreItems
 @"The infamous, otherworldly glutton, in the flesh. His imposing title was self-granted, but truth made it stick.
 He is a formidable foe, capable of swallowing Gods whole, absorbing their essence in its entirety.
 I ordered Draedon to armor his gargantuan form, so he could safely best even great Gods in single combat.
-Fittingly, he had a serpent’s tongue. He manipulated me incessantly, driving me to awful acts.
+Fittingly, he had a serpent's tongue. He manipulated me incessantly, driving me to awful acts.
 I recruited him out of desperation. My war had dragged on for decades, and I would do anything to have it end.
 It was then my negligence was born. My descent began the moment recruiting this scoundrel crossed my mind.
 His absence of loyalty was clear as day, even at the time. However, I suspect it goes beyond that.
-The Devourer’s alien capabilities and domineering tactics hint that his allegiance lay elsewhere.
+The Devourer's alien capabilities and domineering tactics hint that his allegiance lay elsewhere.
 Is he but one soldier, of a malevolence far beyond…?";
 
         public override void SetStaticDefaults()
