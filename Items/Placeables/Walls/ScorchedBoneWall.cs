8c4f4154efcd028063f2da516a495f779025325c
Heart-Plus-Up <85328817+Heart-Plus-Up@users.noreply.github.com>
2023-02-06 22:53:21 -0700

    Bone wall

---
+++ Items/Placeables/Walls/ScorchedBoneWall.cs
@@ -0,0 +1,37 @@
+﻿using Terraria.ModLoader;
+using WallTiles = CalamityMod.Walls;
+using Terraria.ID;
+
+namespace CalamityMod.Items.Placeables.Walls
+{
+    public class ScorchedBoneWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Scorched Bone Wall");
+            SacrificeTotal = 400;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 18;
+            Item.maxStack = 999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 7;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createWall = ModContent.WallType<WallTiles.ScorchedBoneWall>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(4).
+            AddIngredient(ModContent.ItemType<ScorchedBone>()).
+            AddTile(TileID.WorkBenches).
+            Register();
+        }
+    }
+}
