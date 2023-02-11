9923a5fae4c314036511922e035cbd5f30a7732e
Heart-Plus-Up <85328817+Heart-Plus-Up@users.noreply.github.com>
2023-02-10 23:24:56 -0700

    Big Grass

--- Tiles/Crags/LavaPistil.cs
+++ Tiles/Crags/LavaPistil.cs
@@ -1,5 +1,6 @@
 ﻿using CalamityMod.Dusts;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent.Metadata;
@@ -9,7 +10,7 @@ using Terraria.ObjectData;
 
 namespace CalamityMod.Tiles.Crags
 {
-    public class CinderBlossomTallPlants : ModTile
+    public class LavaPistil : ModTile
     {
         public override void SetStaticDefaults()
         {
@@ -20,8 +21,9 @@ namespace CalamityMod.Tiles.Crags
             Main.tileLavaDeath[Type] = true;
             Main.tileWaterDeath[Type] = true;
             Main.tileFrameImportant[Type] = true;
-			TileID.Sets.ReplaceTileBreakUp[Type] = true;
-			TileID.Sets.SwaysInWindBasic[Type] = true;
+            Main.tileLighted[Type] = true;
+            TileID.Sets.ReplaceTileBreakUp[Type] = true;
+			TileID.Sets.SwaysInWindBasic[Type] = false;
 			TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Plant"]);
 
             TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
@@ -36,8 +38,14 @@ namespace CalamityMod.Tiles.Crags
 
         public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY)
         {
-            offsetY = -30;
-            height = 48;
+            offsetY = -128;
+            height = 144;
+        }
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 255f / 255f;
+            g = 206f / 255f;
+            b = 126f / 255f;
         }
     }
 }
