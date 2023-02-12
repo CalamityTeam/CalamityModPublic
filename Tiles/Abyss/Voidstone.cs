c97daa5ed38087f97194a5f97b10254864227059
Heart-Plus-Up <85328817+Heart-Plus-Up@users.noreply.github.com>
2023-02-11 21:31:34 -0700

    Added Tenebris :eyes:

--- Tiles/Abyss/Voidstone.cs
+++ Tiles/Abyss/Voidstone.cs
@@ -47,6 +47,30 @@ namespace CalamityMod.Tiles.Abyss
             return false;
         }
 
+        public override void RandomUpdate(int i, int j)
+        {
+            Tile tile = Main.tile[i, j];
+            Tile up = Main.tile[i, j - 1];
+            Tile up2 = Main.tile[i, j - 2];
+
+            //place Tenebris
+            if (WorldGen.genRand.Next(25) == 0 && !up.HasTile && !up2.HasTile && up.LiquidAmount > 0 && up2.LiquidAmount > 0 && !tile.LeftSlope && !tile.RightSlope && !tile.IsHalfBlock)
+            {
+                up.TileType = (ushort)ModContent.TileType<TenebrisRemnant>();
+                up.HasTile = true;
+                up.TileFrameY = 0;
+
+                //6 different frames, choose a random one
+                up.TileFrameX = (short)(WorldGen.genRand.Next(6) * 18);
+                WorldGen.SquareTileFrame(i, j - 1, true);
+
+                if (Main.netMode == NetmodeID.Server) 
+                {
+                    NetMessage.SendTileSquare(-1, i, j - 1, 3, TileChangeType.None);
+                }
+            }
+        }
+
         public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset)
         {
             int uniqueAnimationFrameX = 0;
