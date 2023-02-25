55bb7587f1dd6188895cfa922d17effe1b744ba5
Ozzatron <spazeglitch@gmail.com>
2023-02-25 16:43:20 -0500

    Tesla cannon charge use 0.9 -> 0.75

--- Items/Weapons/DraedonsArsenal/TeslaCannon.cs
+++ Items/Weapons/DraedonsArsenal/TeslaCannon.cs
@@ -54,7 +54,7 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 
             modItem.UsesCharge = true;
             modItem.MaxCharge = 250f;
-            modItem.ChargePerUse = 0.9f;
+            modItem.ChargePerUse = 0.75f;
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
