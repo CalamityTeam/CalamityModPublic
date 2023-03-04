c630a46e8cde81849660d2a05a1fe350660e0132
Memes <114233643+Mr-Memes@users.noreply.github.com>
2023-03-04 22:08:24 +0100

    Improved Sand Sharknado Staff
    1 - Made Sand Sharknado's Projectiles predictive.
    2 - Restructured the code a little bit.
    3 - Removed some pointless statements.

--- Projectiles/Summon/MiniSandShark.cs
+++ Projectiles/Summon/MiniSandShark.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
-using Terraria;
+﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Audio;
@@ -17,78 +14,35 @@ namespace CalamityMod.Projectiles.Summon
 
         public override void SetDefaults()
         {
-            Projectile.width = 32;
-            Projectile.height = 32;
+            Projectile.timeLeft = 300;
+
+            Projectile.width = Projectile.height = 32;
+
+            Projectile.netImportant = true;
             Projectile.minion = true;
             Projectile.friendly = true;
             Projectile.ignoreWater = true;
-            Projectile.alpha = 255;
-            Projectile.minionSlots = 0f;
-            Projectile.extraUpdates = 1;
-            Projectile.timeLeft = 300;
             Projectile.DamageType = DamageClass.Summon;
         }
 
         public override void AI()
         {
-            Projectile.alpha -= 40;
-            if (Projectile.alpha < 0)
-            {
-                Projectile.alpha = 0;
-            }
-            if (Projectile.ai[1] == 0f)
-            {
-                Projectile.ai[1] = 1f;
-                SoundEngine.PlaySound(SoundID.NPCDeath19, Projectile.position);
-            }
-            if (Projectile.ai[0] >= 45f)
-            {
-                Projectile.ai[0] = 45f;
-                Projectile.velocity.Y = Projectile.velocity.Y + 0.025f;
-            }
-            if (Projectile.velocity.X < 0f)
-            {
-                Projectile.spriteDirection = -1;
-                Projectile.rotation = (float)Math.Atan2((double)-(double)Projectile.velocity.Y, (double)-(double)Projectile.velocity.X);
-            }
-            else
-            {
-                Projectile.spriteDirection = 1;
-                Projectile.rotation = (float)Math.Atan2((double)Projectile.velocity.Y, (double)Projectile.velocity.X);
-            }
+            Projectile.rotation = Projectile.velocity.ToRotation(); // The projectile looks towards where it's going.
+            
+            Projectile.netUpdate = true;
         }
 
-        public override bool PreDraw(ref Color lightColor)
+        public override void Kill(int timeLeft) // When the projectile hits an enemy, it'll make a dust impact effect.
         {
-            SpriteEffects spriteEffects = Projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
-            Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, 0, texture.Width, texture.Height)), Projectile.GetAlpha(lightColor), Projectile.rotation, texture.Size() / 2f, Projectile.scale, spriteEffects, 0);
-            return false;
-        }
-
-        public override void Kill(int timeLeft)
-        {
-            Projectile.position = Projectile.Center;
-            Projectile.width = Projectile.height = 64;
-            Projectile.position.X = Projectile.position.X - (float)(Projectile.width / 2);
-            Projectile.position.Y = Projectile.position.Y - (float)(Projectile.height / 2);
-            Projectile.maxPenetrate = -1;
-            Projectile.penetrate = -1;
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 10;
-            Projectile.damage /= 2;
-            Projectile.Damage();
             SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
-            int num226 = 36;
-            for (int num227 = 0; num227 < num226; num227++)
+            for (int dustIndex = 0; dustIndex < 36; dustIndex++)
             {
-                Vector2 vector6 = Vector2.Normalize(Projectile.velocity) * new Vector2((float)Projectile.width / 2f, (float)Projectile.height) * 0.75f;
-                vector6 = vector6.RotatedBy((double)((float)(num227 - (num226 / 2 - 1)) * 6.28318548f / (float)num226), default) + Projectile.Center;
-                Vector2 vector7 = vector6 - Projectile.Center;
-                int num228 = Dust.NewDust(vector6 + vector7, 0, 0, 85, vector7.X * 1.5f, vector7.Y * 1.5f, 100, default, 1.2f);
-                Main.dust[num228].noGravity = true;
-                Main.dust[num228].noLight = true;
-                Main.dust[num228].velocity = vector7;
+                Dust.NewDust(Projectile.Center,
+                    Projectile.width,
+                    Projectile.height,
+                    85,
+                    Projectile.velocity.X / 2f,
+                    Projectile.velocity.Y / 2f);
             }
         }
     }
