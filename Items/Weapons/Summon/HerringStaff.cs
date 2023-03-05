d0d75a466234f8cc998f7b6ea07af744dba16e33
Memes <114233643+Mr-Memes@users.noreply.github.com>
2023-03-05 01:59:00 +0100

    Herring Staff remastered.
    1 - Made Herring Staff spawn 1 herring per minion slot.
    2 - On use, it spawns an invisible minion and 3 decoration herrings.
    3 - Uses Local I-Frames now.

--- Items/Weapons/Summon/HerringStaff.cs
+++ Items/Weapons/Summon/HerringStaff.cs
@@ -12,35 +12,35 @@ namespace CalamityMod.Items.Weapons.Summon
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Herring Staff");
-            Tooltip.SetDefault("Summons a herring to fight for you\n" +
-            "Each herring takes only half of a minion slot");
+            Tooltip.SetDefault("Summons a group of herrings to fight for you");
             SacrificeTotal = 1;
         }
 
         public override void SetDefaults()
         {
             Item.damage = 17;
+            Item.knockBack = 1.25f;
             Item.mana = 10;
+
+            Item.shoot = ModContent.ProjectileType<HerringAI>();
+
             Item.width = Item.height = 48;
             Item.useTime = Item.useAnimation = 30;
-            Item.useStyle = ItemUseStyleID.Swing;
-            Item.noMelee = true;
-            Item.knockBack = 1.25f;
-            Item.value = CalamityGlobalItem.Rarity3BuyPrice;
-            Item.rare = ItemRarityID.Orange;
-            Item.UseSound = SoundID.Item21;
-            Item.autoReuse = true;
-            Item.shoot = ModContent.ProjectileType<HerringMinion>();
+
             Item.DamageType = DamageClass.Summon;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item21;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = CalamityGlobalItem.Rarity3BuyPrice;
+            Item.autoReuse = true;
+            Item.noMelee = true;
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
             if (player.altFunctionUse != 2)
             {
-                position = Main.MouseWorld;
-                velocity = Vector2.Zero;
-                int p = Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+                int p = Projectile.NewProjectile(source, Main.MouseWorld, Vector2.Zero, type, damage, knockback, player.whoAmI);
                 if (Main.projectile.IndexInRange(p))
                     Main.projectile[p].originalDamage = Item.damage;
             }
