d33e6dbc03c47c49b1cbb983f93a1b06955c8a10
Cameron Ewell <spazeglitch@gmail.com>
2023-03-13 11:18:56 -0400

    Invisible herring AI now has sane display name

--- Projectiles/Summon/HerringAI.cs
+++ Projectiles/Summon/HerringAI.cs
@@ -19,7 +19,7 @@ namespace CalamityMod.Projectiles.Summon
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Herring AI");
+            DisplayName.SetDefault("Herring School");
             ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;
             ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
         }
