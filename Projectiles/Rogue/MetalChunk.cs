1c05a5eaf659b8d626eed3ecf7fb491888e16e3c
mountaindrew10 <marhaj04@gmail.com>
2023-07-02 17:53:28 -0500

    Metal Monstrosity should no longer deal ranged damage.

--- Projectiles/Rogue/MetalChunk.cs
+++ Projectiles/Rogue/MetalChunk.cs
@@ -23,16 +23,17 @@ namespace CalamityMod.Projectiles.Rogue
 
         public override void AI()
         {
-            //Gravity
+            // Gravity
             Projectile.velocity.Y += 0.11f;
             if (Projectile.velocity.Y > 16f)
                 Projectile.velocity.Y = 16f;
-            //Rotation
+
+            // Rotation
             Projectile.rotation += 0.14f * Projectile.direction;
             if (Projectile.Calamity().stealthStrike)
             {
                 Projectile.ai[0]++;
-                if(Projectile.ai[0] >= 10f)
+                if (Projectile.ai[0] >= 10f)
                 {
                     Vector2 speed = new Vector2(Main.rand.NextFloat(-6f, 6f), Main.rand.NextFloat(-6f, 6f));
                     Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, speed, ModContent.ProjectileType<MetalShard>(), (int)(Projectile.damage * 0.3f), 0f, Projectile.owner, 0f, 0f);
@@ -48,10 +49,11 @@ namespace CalamityMod.Projectiles.Rogue
             {
                 Vector2 S1 = new Vector2(-Projectile.velocity.X, -Projectile.velocity.Y).RotatedBy(MathHelper.ToRadians(Main.rand.Next(-45, 46)));
                 int proj = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, S1, ProjectileID.SpikyBall, (int)(Projectile.damage * 0.3), 0f, Projectile.owner, 0f, 0f);
+                Main.projectile[proj].DamageType = RogueDamageClass.Instance;
                 Main.projectile[proj].timeLeft = 600;
                 Main.projectile[proj].usesLocalNPCImmunity = true;
                 Main.projectile[proj].localNPCHitCooldown = 20;
-                S1 = new Vector2(-Projectile.velocity.X * 0.7f, -Projectile.velocity.Y*0.7f).RotatedBy(MathHelper.ToRadians(Main.rand.Next(-45, 46)));
+                S1 = new Vector2(-Projectile.velocity.X * 0.7f, -Projectile.velocity.Y * 0.7f).RotatedBy(MathHelper.ToRadians(Main.rand.Next(-45, 46)));
                 Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, S1, ModContent.ProjectileType<MetalShard>(), (int)(Projectile.damage * 0.3), 0f, Projectile.owner, 0f, 0f);
             }
             //Dust
