6c13b68b3209ad2c3a6426b04762e50da9559ed0
Ozzatron <spazeglitch@gmail.com>
2023-07-11 17:29:08 -0400

    archerfish cleanup. knockback boost is now flat, so water rings deal consistent KB regardless of ammo

--- Items/Weapons/Ranged/Archerfish.cs
+++ Items/Weapons/Ranged/Archerfish.cs
@@ -38,18 +38,16 @@ namespace CalamityMod.Items.Weapons.Ranged
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
+            // Replaces standard bullets with water jets.
             if (type == ProjectileID.Bullet)
                 Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<ArcherfishShot>(), damage, knockback, player.whoAmI);
             else
                 Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
 
-                Projectile.NewProjectile(source,
-                position,
-                velocity * 0.5f,
-                ModContent.ProjectileType<ArcherfishRing>(),
-                (int)(damage * 0.5f),
-                knockback * 3f,
-                player.whoAmI);
+            // Always fires a close range water blast.
+            int waterRingDamage = (int)(damage * 0.5f);
+            float boostedKB = knockback + 5f;
+            Projectile.NewProjectile(source, position, velocity * 0.5f, ModContent.ProjectileType<ArcherfishRing>(), waterRingDamage, boostedKB, player.whoAmI);
 
             return false;
         }
