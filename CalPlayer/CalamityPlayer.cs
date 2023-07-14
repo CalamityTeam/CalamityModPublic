b643db56337b72ee86e3e33b086901baa72471e2
Memes <114233643+Mr-Memes@users.noreply.github.com>
2023-07-14 15:45:46 +0200

    Added Astral Vengeance.

--- CalPlayer/CalamityPlayer.cs
+++ CalPlayer/CalamityPlayer.cs
@@ -979,6 +979,7 @@ namespace CalamityMod.CalPlayer
         public bool AresCannons = false;
         public bool celestialDragons = false;
         public bool KalandraMirror = false;
+        public bool astralVengeance = false;
 
         public List<DeadMinionProperties> PendingProjectilesToRespawn = new List<DeadMinionProperties>();
 
@@ -2017,6 +2018,7 @@ namespace CalamityMod.CalPlayer
             AresCannons = false;
             celestialDragons = false;
             KalandraMirror = false;
+            astralVengeance = false;
 
             disableVoodooSpawns = false;
             disablePerfCystSpawns = false;
