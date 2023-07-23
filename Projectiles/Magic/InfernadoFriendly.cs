b47bc7874bd4ebfc6edff56e361b9d8074e1eef0
Storm2103 <snowstorm2013@gmail.com>
2023-07-22 20:23:02 -0600

    The Wand nerf, we'll see if it gets its iframes also ramped up like Dragon Scales

--- Projectiles/Magic/InfernadoFriendly.cs
+++ Projectiles/Magic/InfernadoFriendly.cs
@@ -27,8 +27,8 @@ namespace CalamityMod.Projectiles.Magic
             Projectile.penetrate = -1;
             Projectile.alpha = 255;
             Projectile.timeLeft = 500;
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 10;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 10;
             Projectile.DamageType = DamageClass.Magic;
         }
 
