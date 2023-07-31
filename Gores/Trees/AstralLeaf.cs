a6b60977a16d0ce045d23ef28a47552902667758
Mortis3 <53282720+DylanDoe21@users.noreply.github.com>
2023-07-31 17:10:43 -0400

    leaf gore fix

--- Gores/Trees/AstralLeaf.cs
+++ Gores/Trees/AstralLeaf.cs
@@ -5,9 +5,14 @@ namespace CalamityMod.Gores.Trees
 {
 	public class AstralLeaf : ModGore
 	{
-		public override void SetStaticDefaults()
-		{			
-			GoreID.Sets.SpecialAI[Type] = 3;
+		public override void OnSpawn(Gore gore, IEntitySource source)
+		{
+			ChildSafety.SafeGore[gore.type] = true;
+			gore.velocity = new Vector2(Main.rand.NextFloat() - 0.5f, Main.rand.NextFloat() * MathHelper.TwoPi);
+			gore.numFrames = 8;
+			gore.frame = (byte)Main.rand.Next(8);
+			gore.frameCounter = (byte)Main.rand.Next(8);
+			UpdateType = 910;
 		}
 	}
 }
