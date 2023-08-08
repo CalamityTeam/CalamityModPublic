585e3d3dde7be4115c6997a06701bcb78e68602c
mountaindrew10 <marhaj04@gmail.com>
2023-08-07 22:32:50 -0500

    More optimizations.

--- Particles/SnowflakeSparkle.cs
+++ Particles/SnowflakeSparkle.cs
@@ -9,13 +9,17 @@ namespace CalamityMod.Particles
     public class SnowflakeSparkle : Particle
     {
         public override string Texture => "CalamityMod/Particles/HalfIceStar";
+
         public override bool UseAdditiveBlend => true;
+
         public override bool UseCustomDraw => true;
+
         public override bool SetLifetime => true;
 
         private float Spin;
         private float opacity;
         private Color Bloom;
+
         private Color LightColor => Bloom * opacity;
         private float BloomScale;
         private int Spokes;
@@ -46,15 +50,18 @@ namespace CalamityMod.Particles
         {
             Texture2D spokesTexture = ModContent.Request<Texture2D>(Texture).Value;
             Texture2D bloomTexture = ModContent.Request<Texture2D>("CalamityMod/Particles/BloomCircle").Value;
-            //Ajust the bloom's texture to be the same size as the star's
+            // Ajust the bloom's texture to be the same size as the star's.
             float properBloomSize = (float)spokesTexture.Height / (float)bloomTexture.Height;
+            float halvedOpacity = opacity * 0.5f;
 
-            spriteBatch.Draw(bloomTexture, Position - Main.screenPosition, null, Bloom * opacity * 0.5f, 0, bloomTexture.Size()/2f, Scale * BloomScale * properBloomSize, SpriteEffects.None, 0);
+            spriteBatch.Draw(bloomTexture, Position - Main.screenPosition, null, Bloom * halvedOpacity, 0, bloomTexture.Size() / 2f, Scale * BloomScale * properBloomSize, SpriteEffects.None, 0);
 
+            Color spokeColor = Color * halvedOpacity;
+            Vector2 origin = spokesTexture.Size() / 2f;
             for (int i = 0; i < Spokes; i++)
             {
-                float rotation = Rotation + MathHelper.Lerp(0f, MathHelper.Pi, i/(float)Spokes);
-                spriteBatch.Draw(spokesTexture, Position - Main.screenPosition, null, Color * opacity * 0.5f, Rotation + rotation, spokesTexture.Size() / 2f, Scale, SpriteEffects.None, 0);
+                float rotation = Rotation + MathHelper.Lerp(0f, MathHelper.Pi, i / (float)Spokes);
+                spriteBatch.Draw(spokesTexture, Position - Main.screenPosition, null, spokeColor, Rotation + rotation, origin, Scale, SpriteEffects.None, 0);
             }
         }
     }
