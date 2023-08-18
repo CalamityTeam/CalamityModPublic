7317ab04cb263d328fc601b29fd945c304c52c78
AquaSG <macvine9@gmail.com>
2023-08-18 14:33:56 -0400

    0 slots wow (useless)

--- Projectiles/Summon/GhostFire.cs
+++ Projectiles/Summon/GhostFire.cs
@@ -30,7 +30,6 @@ namespace CalamityMod.Projectiles.Summon
             Projectile.penetrate = 200;
             Projectile.extraUpdates = 3;
             Projectile.timeLeft = 600;
-            Projectile.minionSlots = 0f;
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = 50;
             Projectile.DamageType = DamageClass.Summon;
