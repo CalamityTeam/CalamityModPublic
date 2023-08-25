664b0925351db8ae38cde2d0ffda609f73fce1c7
CongratsIsTrash <142704082+CongratsIsTrash@users.noreply.github.com>
2023-08-25 18:26:48 -0400

    Empyrean Knives and Poseidon balance Poseidon is definitely not final

--- Items/Weapons/Magic/Poseidon.cs
+++ Items/Weapons/Magic/Poseidon.cs
@@ -12,13 +12,13 @@ namespace CalamityMod.Items.Weapons.Magic
         public new string LocalizationCategory => "Items.Weapons.Magic";
         public override void SetDefaults()
         {
-            Item.damage = 62;
+            Item.damage = 52;
             Item.DamageType = DamageClass.Magic;
             Item.mana = 12;
             Item.width = 28;
             Item.height = 32;
-            Item.useTime = 45;
-            Item.useAnimation = 45;
+            Item.useTime = 49;
+            Item.useAnimation = 49;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 6f;
@@ -27,13 +27,12 @@ namespace CalamityMod.Items.Weapons.Magic
             Item.rare = ItemRarityID.Pink;
             Item.autoReuse = true;
             Item.shoot = ModContent.ProjectileType<PoseidonTyphoon>();
-            Item.shootSpeed = 20f;
+            Item.shootSpeed = 18f;
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
             int typhoon = Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 0f, 0f);
-            Main.projectile[typhoon].penetrate = Main.rand.Next(4,11);
             return false;
         }
     }
