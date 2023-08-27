98071d5ad34659e174026d2b7227c6679f900198
CongratsIsTrash <142704082+CongratsIsTrash@users.noreply.github.com>
2023-08-27 15:51:36 -0400

    Gruesome Eminence, take 2

--- Projectiles/Magic/SpiritCongregation.cs
+++ Projectiles/Magic/SpiritCongregation.cs
@@ -60,7 +60,7 @@ namespace CalamityMod.Projectiles.Magic
             Projectile.tileCollide = false;
             Projectile.ignoreWater = true;
             Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 10;
+            Projectile.localNPCHitCooldown = 18;
             Projectile.timeLeft = 90000;
         }
 
