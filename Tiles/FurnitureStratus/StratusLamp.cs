13e1ad96d7cd5aa5f2399576b6b9cb4862bef96b
Mishiro Usui <MishiroUsuiDev@users.noreply.github.com>
2023-10-02 21:24:13 +0100

    Some furniture fixes

--- Tiles/FurnitureStratus/StratusLamp.cs
+++ Tiles/FurnitureStratus/StratusLamp.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CalamityMod.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Tiles.FurnitureStratus
@@ -39,6 +43,20 @@ namespace CalamityMod.Tiles.FurnitureStratus
         public override void HitWire(int i, int j)
         {
             CalamityUtils.LightHitWire(Type, i, j, 1, 3);
+        }
+        
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            CalamityUtils.DrawFlameEffect(ModContent.Request<Texture2D>("CalamityMod/Tiles/FurnitureStratus/StratusLampFlame").Value, i, j);
+        }
+        
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+        {
+            Tile tile = Main.tile[i, j];
+            if (tile.TileFrameY == 0 && tile.TileFrameX < 18)
+            {
+                CalamityUtils.DrawFlameSparks(DustID.BlueFairy, 18, i, j);
+            }
         }
     }
 }
