ca4d62e92653c1ce653472fd1c6c4ed00f041714
Altixal <73398287+Altixal@users.noreply.github.com>
2023-10-02 19:53:45 +0700

    atlantis comedy bug

--- Projectiles/Magic/AtlantisSpear.cs
+++ Projectiles/Magic/AtlantisSpear.cs
@@ -86,7 +86,7 @@ namespace CalamityMod.Projectiles.Magic
             }
 
             // Prevent recursion: the segments that are being spawned here will deliberately be set higher than total segments
-            if (Projectile.ai[0] > TotalSegments)
+            if (Projectile.ai[0] > TotalSegments || Main.myPlayer != Projectile.owner)
                 return;
 
             // Spawn two ungrowing segments to either side on death
