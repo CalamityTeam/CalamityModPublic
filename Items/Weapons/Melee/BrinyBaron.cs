575689391befe8787e9f15ecc9632661ae3865fc
Done2222 <asafomri4@gmail.com>
2023-10-12 21:32:00 +0300

    briny baron buff (i apparently forgot to do this a while ago, better late than never ig)

--- Items/Weapons/Melee/BrinyBaron.cs
+++ Items/Weapons/Melee/BrinyBaron.cs
@@ -17,9 +17,9 @@ namespace CalamityMod.Items.Weapons.Melee
 
         public override void SetDefaults()
         {
-            Item.damage = 180;
+            Item.damage = 175;
             Item.knockBack = 4f;
-            Item.useAnimation = Item.useTime = 30;
+            Item.useAnimation = Item.useTime = 21;
             Item.DamageType = DamageClass.Melee;
             Item.useTurn = true;
             Item.autoReuse = true;
