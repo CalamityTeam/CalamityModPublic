567bcfa9188a6add516ca43a47be33ca5ef32ef8
Altixal <73398287+Altixal@users.noreply.github.com>
2023-10-16 01:57:37 +0700

    revised meowthrower :3

---
+++ Projectiles/Ranged/MeowCreature.cs
@@ -0,0 +1,76 @@
+using CalamityMod.Particles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Ranged
+{
+    public class MeowCreature : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Ranged";
+
+        public static int Lifetime => 180;
+        internal PrimitiveTrail TrailDrawer;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 36;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 24;
+            Projectile.friendly = true;
+            Projectile.penetrate = 1;
+            Projectile.MaxUpdates = 2;
+            Projectile.timeLeft = Lifetime;
+            Projectile.DamageType = DamageClass.Ranged;
+        }
+
+        public override void AI()
+        {
+            CalamityUtils.HomeInOnNPC(Projectile, false, 480f, 16f, 20f);
+            Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(Main.rand.NextBool() ? SoundID.Item58 : SoundID.Item57, Projectile.Center);
+
+            // Circular spread of clouds
+            for (int i = 0; i < 12; i++)
+            {
+                Vector2 smokeVel = Main.rand.NextVector2Circular(16f, 16f);
+                Color smokeColor = Main.hslToRgb(Main.rand.NextFloat(), 1f, 0.8f);
+                Particle smoke = new MediumMistParticle(Projectile.Center, smokeVel, smokeColor, Color.Black, Main.rand.NextFloat(0.6f, 1.6f), 220 - Main.rand.Next(60), 0.1f);
+                GeneralParticleHandler.SpawnParticle(smoke);
+            }
+        }
+
+        internal float WidthFunction(float completionRatio) => (1f - completionRatio) * Projectile.scale * 10f;
+        internal Color ColorFunction(float completionRatio)
+        {
+            float hue = completionRatio * Projectile.timeLeft / (float)Lifetime;
+            Color trailColor = Main.hslToRgb(hue, 1f, 0.8f);
+            return trailColor * Projectile.Opacity;
+        }
+
+        // The creature glows
+        public override void PostDraw(Color lightColor)
+        {
+            if (TrailDrawer is null)
+                TrailDrawer = new PrimitiveTrail(WidthFunction, ColorFunction);
+
+            TrailDrawer.Draw(Projectile.oldPos.Where(oldPos => oldPos != Vector2.Zero), Projectile.Size * 0.5f - Main.screenPosition, 30);
+
+            Texture2D glow = ModContent.Request<Texture2D>(Texture).Value;
+            Main.EntitySpriteDraw(glow, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, glow.Size() * 0.5f, Projectile.scale, SpriteEffects.None);
+        }
+    }
+}
--- Projectiles/Ranged/MeowFire.cs
+++ Projectiles/Ranged/MeowFire.cs
@@ -42,15 +42,15 @@ namespace CalamityMod.Projectiles.Ranged
             
             // Main smokes composing of blue or pink
             float smokeRot = MathHelper.ToRadians(3f); // *Rate of rotation per frame, not a constant rotation
-            Color smokeColor = Color.Lerp(PinkFire ? Color.DeepPink : Color.DodgerBlue, PinkFire ? Color.Pink : Color.PowderBlue, 0.4f + 0.3f * MathF.Sin(Main.GlobalTimeWrappedHourly * 5f));
-            Particle smoke = new HeavySmokeParticle(Projectile.Center, Projectile.velocity * 0.5f, smokeColor, 12, Projectile.scale * Main.rand.NextFloat(0.6f, 1.2f), 0.8f, smokeRot, required: true);
+            Color smokeColor = Color.Lerp(PinkFire ? Color.DeepPink : Color.DodgerBlue, PinkFire ? Color.Pink : Color.PowderBlue, 0.5f + 0.3f * MathF.Sin(Main.GlobalTimeWrappedHourly * 5f));
+            Particle smoke = new HeavySmokeParticle(Projectile.Center, Projectile.velocity * 0.5f, smokeColor, 12, Projectile.scale * Main.rand.NextFloat(0.6f, 1.2f), 0.25f, smokeRot, true, required: true);
             GeneralParticleHandler.SpawnParticle(smoke);
 
             // Overlay the glow on top, a bit brighter
-            if (Main.rand.NextBool(5))
+            if (Main.rand.NextBool(8))
             {
-                Color glowColor = Color.Lerp(smokeColor, Color.White, 0.4f);
-                Particle smokeGlow = new HeavySmokeParticle(Projectile.Center, Projectile.velocity * 0.5f, glowColor, 9, Projectile.scale * Main.rand.NextFloat(0.4f, 0.7f), 0.8f, smokeRot, true, 0.005f, true);
+                Color glowColor = Color.Lerp(smokeColor, Color.White, 0.2f);
+                Particle smokeGlow = new HeavySmokeParticle(Projectile.Center, Projectile.velocity * 0.5f, glowColor, 8, Projectile.scale * Main.rand.NextFloat(0.4f, 0.7f), 0.15f, smokeRot, true, 0.005f, true);
                 GeneralParticleHandler.SpawnParticle(smokeGlow);
             }
 
@@ -59,19 +59,5 @@ namespace CalamityMod.Projectiles.Ranged
 
         // Circular hitbox adjusted for the size of the smoke particles (which is 52 here)
         public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) => CalamityUtils.CircularHitboxCollision(Projectile.Center, 52 * Projectile.scale * 0.5f, targetHitbox);
-
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            SoundEngine.PlaySound(PinkFire ? SoundID.Item58 : SoundID.Item57, Projectile.Center);
-
-            // Circular spread of clouds on hit
-            for (int i = 0; i < 12; i++)
-            {
-                Vector2 smokeVel = Main.rand.NextVector2Circular(16f, 16f);
-                Color smokeColor = PinkFire ? Color.Pink : Color.PowderBlue;
-                Particle smoke = new MediumMistParticle(Projectile.Center, smokeVel, smokeColor, Color.Black, Main.rand.NextFloat(0.6f, 1.6f), 200 - Main.rand.Next(60), 0.1f);
-                GeneralParticleHandler.SpawnParticle(smoke);
-            }
-        }
     }
 }
