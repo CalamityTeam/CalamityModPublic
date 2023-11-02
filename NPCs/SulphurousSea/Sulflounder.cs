722a460fb1bedaa684348f085d7adb67d2d179f9
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2023-11-02 11:55:03 -0400

    Plague, Primordial Wyrm, Sulphurous Sea

--- NPCs/SulphurousSea/Sulflounder.cs
+++ NPCs/SulphurousSea/Sulflounder.cs
@@ -56,10 +56,10 @@ namespace CalamityMod.NPCs.SulphurousSea
         public override void AI()
         {
             NPC.spriteDirection = (NPC.direction > 0) ? 1 : -1;
-            int num = 200;
+            int alphaControl = 200;
             if (NPC.ai[2] == 0f)
             {
-                NPC.alpha = num;
+                NPC.alpha = alphaControl;
                 NPC.TargetClosest(true);
                 if (!Main.player[NPC.target].dead && (Main.player[NPC.target].Center - NPC.Center).Length() < 170f &&
                     Collision.CanHit(NPC.position, NPC.width, NPC.height, Main.player[NPC.target].position, Main.player[NPC.target].width, Main.player[NPC.target].height))
@@ -76,7 +76,7 @@ namespace CalamityMod.NPCs.SulphurousSea
             {
                 if (NPC.alpha > 0)
                 {
-                    NPC.alpha -= num / 16;
+                    NPC.alpha -= alphaControl / 16;
                     if (NPC.alpha < 0)
                     {
                         NPC.alpha = 0;
