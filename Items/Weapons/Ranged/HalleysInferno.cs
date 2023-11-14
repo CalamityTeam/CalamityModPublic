f1308ff81102a1d77ed325891d506624d82e1195
Ozzatron <spazeglitch@gmail.com>
2023-11-14 15:33:21 -0500

    Halley's Inferno rework. Now clockwork, bursts of 5. Damage 1450 -> 488. Hitbox substantially larger. Goes through walls.

--- Items/Weapons/Ranged/HalleysInferno.cs
+++ Items/Weapons/Ranged/HalleysInferno.cs
@@ -18,11 +18,17 @@ namespace CalamityMod.Items.Weapons.Ranged
         public static readonly SoundStyle Hit = new("CalamityMod/Sounds/Item/HalleysInfernoHit") { Volume = 0.5f };
         public override void SetDefaults()
         {
-            Item.damage = 1450;
+            Item.damage = 488;
             Item.knockBack = 5.5f;
             Item.DamageType = DamageClass.Ranged;
-            Item.useTime = Item.useAnimation = 39;
+
+            // Burst of 5, one every 5 frames for 25 total. Cooldown of 39 frames.
+            Item.useTime = 5;
+            Item.useAnimation = 25;
+            Item.reuseDelay = 39;
+            Item.useLimitPerAnimation = 5;
             Item.autoReuse = true;
+
             Item.useAmmo = AmmoID.Gel;
             Item.shootSpeed = 12f;
             Item.shoot = ModContent.ProjectileType<HalleysComet>();
--- Projectiles/Ranged/HalleysComet.cs
+++ Projectiles/Ranged/HalleysComet.cs
@@ -17,13 +17,14 @@ namespace CalamityMod.Projectiles.Ranged
 
         public override void SetDefaults()
         {
-            Projectile.width = Projectile.height = 12;
+            Projectile.width = Projectile.height = 32;
             Projectile.friendly = true;
+            Projectile.tileCollide = false;
             Projectile.ignoreWater = true;
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.penetrate = 5;
-            Projectile.MaxUpdates = 10;
-            Projectile.timeLeft = 15 * Projectile.MaxUpdates;
+            Projectile.MaxUpdates = 15;
+            Projectile.timeLeft = 20 * Projectile.MaxUpdates;
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = -1;
         }
@@ -32,13 +33,25 @@ namespace CalamityMod.Projectiles.Ranged
         {
             Projectile.rotation = Projectile.velocity.ToRotation();
             Time++;
-            if (Time > 6f)
+
+            // Render the needle beam as a pair of severely stretched spark particles.
+            // This has to be done only on specific extra updates or it'll instantly overload the particle limit.
+            bool isDrawingUpdate = Projectile.numUpdates % 3 == 0;
+            if (Time > 6f && isDrawingUpdate)
             {
-                SparkParticle spark = new SparkParticle(Projectile.Center, Projectile.velocity, false, 11, 1.9f, Color.MediumBlue);
+                Color outerSparkColor = new Color(8, 35, 156);
+                float scaleBoost = MathHelper.Clamp(Time * 0.005f, 0f, 2f);
+                float outerSparkScale = 3.2f + scaleBoost;
+                SparkParticle spark = new SparkParticle(Projectile.Center, Projectile.velocity, false, 7, outerSparkScale, outerSparkColor);
                 GeneralParticleHandler.SpawnParticle(spark);
-                SparkParticle spark2 = new SparkParticle(Projectile.Center, Projectile.velocity, false, 11, 1f, Color.Aqua);
+
+                Color innerSparkColor = new Color(184, 215, 245);
+                float innerSparkScale = 1.6f + scaleBoost;
+                SparkParticle spark2 = new SparkParticle(Projectile.Center, Projectile.velocity, false, 7, innerSparkScale, innerSparkColor);
                 GeneralParticleHandler.SpawnParticle(spark2);
             }
+
+            // 5th update: Spawn two pulse rings and some dust as firing vfx
             else if (Time == 5f)
             {
                 Particle pulse = new DirectionalPulseRing(Projectile.Center, Projectile.velocity * 0.75f, Color.Aqua, new Vector2(1f, 2.5f), Projectile.rotation, 0.2f, 0.03f, 20);
@@ -58,14 +71,17 @@ namespace CalamityMod.Projectiles.Ranged
                 }
             }
 
-            Lighting.AddLight(Projectile.Center, Color.MediumBlue.ToVector3() * 0.4f);
+            if (Projectile.FinalExtraUpdate())
+                Lighting.AddLight(Projectile.Center, Color.MediumBlue.ToVector3() * 0.4f);
         }
 
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
             target.AddBuff(ModContent.BuffType<Nightwither>(), 450);
             SoundEngine.PlaySound(HalleysInferno.Hit, Projectile.Center);
-            for (int i = 0; i <= 13; i++)
+
+            // Dust emission on hit
+            for (int i = 0; i < 14; i++)
             {
                 Dust dust = Dust.NewDustPerfect(Projectile.Center, Main.rand.NextBool(3) ? 172 : 206, Projectile.velocity);
                 dust.scale = Main.rand.NextFloat(1.1f, 1.9f);
@@ -77,6 +93,16 @@ namespace CalamityMod.Projectiles.Ranged
         public override void OnHitPlayer(Player target, Player.HurtInfo info)
         {
             target.AddBuff(ModContent.BuffType<Nightwither>(), 450);
+            SoundEngine.PlaySound(HalleysInferno.Hit, Projectile.Center);
+
+            // Dust emission on hit
+            for (int i = 0; i < 14; i++)
+            {
+                Dust dust = Dust.NewDustPerfect(Projectile.Center, Main.rand.NextBool(3) ? 172 : 206, Projectile.velocity);
+                dust.scale = Main.rand.NextFloat(1.1f, 1.9f);
+                dust.velocity = Projectile.velocity.RotatedByRandom(0.5f) * Main.rand.NextFloat(0.2f, 2.1f);
+                dust.noGravity = true;
+            }
         }
     }
 }
