5dc9377f859e1b39dcd0fd9d94f41855e56396b9
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2023-11-27 11:24:53 -0500

    Serpentine balancing?

--- Items/Weapons/Magic/Serpentine.cs
+++ Items/Weapons/Magic/Serpentine.cs
@@ -13,13 +13,13 @@ namespace CalamityMod.Items.Weapons.Magic
         public new string LocalizationCategory => "Items.Weapons.Magic";
         public override void SetDefaults()
         {
-            Item.damage = 14;
+            Item.damage = 10;
             Item.DamageType = DamageClass.Magic;
             Item.mana = 20;
             Item.width = 28;
             Item.height = 32;
-            Item.useTime = 20;
-            Item.useAnimation = 20;
+            Item.useTime = 35;
+            Item.useAnimation = 35;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 3f;
