05868eeda6315ac936bd61a639bd0102bfe5e084
Ozzatron <spazeglitch@gmail.com>
2023-12-01 20:29:52 -0500

    Removed NPC name Matt Walsh for donor 522970788203069442 by dev request

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -209,7 +209,7 @@ namespace CalamityMod.NPCs
         {
             "Stan Pines",
             "Slap Battles", // <@!923504188615450654> (gravityglider.)
-            "Matt Walsh", // <@!522970788203069442> (phallguy)
+            /* NAME REMOVED: "Matt Walsh", */ // <@!522970788203069442> (phallguy)
         };
         private static readonly string[] TruffleNames =
         {
