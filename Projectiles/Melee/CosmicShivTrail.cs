b8d2c071c6fee539fb29fc51e4dd43da4d23b5c1
YataYT <151573037+YataYT@users.noreply.github.com>
2023-12-27 15:44:33 -0600

    Rework from Yatagarasu (patreon donor for Cosmic Shiv), which now shoots a homing star trail to a target, spawning an invisible, non-damaging aura that sticks to the target, continously spawning extremely fast blades for 5 seconds. I didn't add a limit for how many auras can exist at once, but I added limits to how many can be applied at once. Galaxy Star is no longer a thing, but the texture is used for Cosmic Shiv Trail now, so I renamed the file to match.
    My intention with this weapon is to be a slow firing projectile-focused weapon that can stack up a bit on one target, and be able to keep up with Yharon, hence the accelerating homing and high speed blades. Feel free to nerf the penetration of the blades if they're too op against Thanatos.
    
    This one was a big step-up from my previous projects (which was basically just making very basic weapons or slightly modifying ExampleMod code, and I'm overall extremely happy with how it went. Code might be pretty bad since I only had 2 weeks of modding experience by the time I finished this, so the code might need some cleaning up, and if the draws are too laggy, feel free to nerf the values a bit (although I tweaked with them a lot to get them looking the best)

---
+++ Projectiles/Melee/CosmicShivTrail.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod.Buffs.DamageOverTime;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection.Metadata;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics.Contracts;
+using Terraria.GameContent;
+using Terraria.Audio;
+
+namespace CalamityMod.Projectiles.Melee
+{
+    public class CosmicShivTrail : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Melee";
+
+        public NPC target = null;
+        public const float MaxDistanceToTarget = 540f;
+        public const float ExplosionDamageMultiplier = 2f;
+        public const int FadeInTime = 12;
+
+        public static List<Color> DustColors = new List<Color>
+        {
+            new Color(69, 69, 222),
+            new Color(99, 66, 212),
+            new Color(130, 64, 214),
+            new Color(154, 75, 219),
+            new Color(165, 62, 201)
+        };
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 15;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 24;
+            Projectile.height = 24;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.penetrate = 1;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 220;
+            Projectile.scale = 0.7f;
+            Projectile.Opacity = 0f;
+            Projectile.extraUpdates = 1;
+        }
+
+        public override void AI()
+        {
+            int QuarterSpriteWidth = 48 / 3;
+            int QuarterSpriteHeight = 48 / 3;
+
+            int QuarterProjWidth = Projectile.width / 3;
+            int QuarterProjHeight = Projectile.height / 3;
+
+            DrawOriginOffsetX = 0;
+            DrawOffsetX = -(QuarterSpriteWidth - QuarterProjWidth);
+            DrawOriginOffsetY = -(QuarterSpriteHeight - QuarterProjHeight);
+
+
+            Lighting.AddLight(Projectile.Center, Projectile.GetAlpha(Color.White).ToVector3());
+
+            Projectile.rotation += Utils.GetLerpValue(-8f, 12f, Projectile.velocity.Length(), clamped: true);
+
+            if (Projectile.ai[0] > 2f)  // After two frames
+            {
+                Projectile.Opacity = Utils.GetLerpValue(2f, FadeInTime, Projectile.ai[0], clamped: true);   // Fade in
+
+                if (Main.rand.Next(0, 2) == 0)      // Dust trail
+                {
+                    float scale = Main.rand.NextFloat(1.6f, 2.1f);
+                    Color randomColor = DustColors[Main.rand.Next(0, DustColors.Count)];
+                    Dust dust = Dust.NewDustDirect(Projectile.position, 1, 1, DustID.RainbowMk2, 0, 0, 255, randomColor * 0.6f, scale);
+                    dust.noGravity = true;
+                }
+            }
+
+            if (Projectile.ai[0] % 15 == 0) // every 0.25 seconds
+            {
+                target = Projectile.Center.ClosestNPCAt(MaxDistanceToTarget);
+            }
+
+            if (target is not null)
+            {
+                Vector2 targetVelocity = (target.Center - Projectile.Center).SafeNormalize(Vector2.Zero);
+                targetVelocity *= Projectile.velocity.Length();
+                Projectile.velocity = Vector2.Lerp(Projectile.velocity, targetVelocity, 0.12f);               
+            }
+
+            // lifespan timer
+            Projectile.ai[0]++;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            // Summons invisible aura that sticks to enemies, spawning blades
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<CosmicShivAura>(), Projectile.damage, Projectile.knockBack, Projectile.owner, target.whoAmI);
+            target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 60);
+        }
+
+        // pretty much entirely from the Oracle circular damage code
+        private void CircularDamage(float radius)
+        {
+            if (Projectile.owner != Main.myPlayer)
+                return;
+            Player owner = Main.player[Projectile.owner];
+
+            for (int i = 0; i < Main.npc.Length; ++i)
+            {
+                NPC target = Main.npc[i];
+                if (!target.active || target.dontTakeDamage || target.friendly)
+                    continue;
+
+                // Shock any valid target within range. Check all four corners of their hitbox.
+                float d1 = Vector2.Distance(Projectile.Center, target.Hitbox.TopLeft());
+                float d2 = Vector2.Distance(Projectile.Center, target.Hitbox.TopRight());
+                float d3 = Vector2.Distance(Projectile.Center, target.Hitbox.BottomLeft());
+                float d4 = Vector2.Distance(Projectile.Center, target.Hitbox.BottomRight());
+                float dist = MathHelper.Min(d1, d2);
+                dist = MathHelper.Min(dist, d3);
+                dist = MathHelper.Min(dist, d4);
+
+                if (dist <= radius)
+                {
+                    int damage = (int)(Projectile.damage * ExplosionDamageMultiplier);
+                    bool crit = Main.rand.Next(100) <= owner.GetCritChance<MeleeDamageClass>() + 4;
+                    target.StrikeNPC(target.CalculateHitInfo(damage, 0, crit, 0));
+
+                    if (Main.netMode != NetmodeID.SinglePlayer)
+                        NetMessage.SendData(MessageID.DamageNPC, -1, -1, null, i, damage, 0f, 0f, crit ? 1 : 0, 0, 0);
+                }
+            }
+        }
+
+        // Very similar to CosmicShivBlade PreDraw
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Rectangle sourceRectangle = texture.Frame(1, Main.projFrames[Type], 0, Projectile.frame);
+            Vector2 origin = sourceRectangle.Size() / 2f;
+            float rotation = Projectile.rotation;
+            Color color = Projectile.GetAlpha(Color.White);
+            float scaleMult = 1f;
+
+            for (int i = 0; i < Projectile.oldPos.Length; i++)
+            {
+                Vector2 drawPos = Projectile.oldPos[i] + (Projectile.Size / 2f) - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY);
+                color *= ((float)(Projectile.oldPos.Length - (i / 4)) / (float)Projectile.oldPos.Length);
+                Main.spriteBatch.Draw(texture, drawPos, new Rectangle?(sourceRectangle), color * 0.4f, rotation * -1f, origin, Projectile.scale * 1.8f * scaleMult, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(texture, drawPos, new Rectangle?(sourceRectangle), color, rotation, origin, Projectile.scale * scaleMult, SpriteEffects.None, 0f);
+                scaleMult *= 0.9f;
+            }
+            
+            return false; 
+
+        }
+
+        // Lots of trig here, use desmos to comprehend its full power
+        public override void OnKill(int timeLeft)
+        {
+            float rand2PI = Main.rand.NextFloat(0, MathHelper.TwoPi);       // Random rotation offset
+            int rosePetalCount = Main.rand.Next(5, 8);
+            int bigWavyPetalCount = Main.rand.Next(7, 11);
+            int wavyPetalCount = Main.rand.Next(6, 10);
+            float speed = Main.rand.Next(6, 11);                            // Size
+
+            int rand = Main.rand.Next(0, 10);   // 10% chance rose, 10% chance big wavy shape, and 80% chance small undetail wavy shape
+            if (rand == 0) { 
+                for (float k = 0f; k < MathHelper.TwoPi; k += 0.03f)
+                {
+                    float scale = Main.rand.NextFloat(1.1f, 1.4f);
+                    float randomWhitingValue = Main.rand.NextFloat(0.0f, 0.2f);
+                    Color color = Color.Lerp(DustColors[Main.rand.Next(0, DustColors.Count)], Color.White, randomWhitingValue);
+
+                    Vector2 velocity = k.ToRotationVector2() * (2f + (float)(Math.Sin((double)(rand2PI + k * (float)rosePetalCount)) + 1.0) * speed) * Main.rand.NextFloat(0.95f, 1.05f);
+                    Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, new Vector2?(velocity), 0, color, scale);
+                    dust.noGravity = true;
+                    dust.fadeIn = -1f;
+
+                    Dust dust2 = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, new Vector2?(velocity) * 0.75f, 0, color, scale);
+                    dust2.noGravity = true;
+                    dust2.fadeIn = -1f;
+                }
+            } else if (rand == 1) {
+                for (float k = 0f; k < MathHelper.TwoPi; k += 0.08f)
+                {
+                    float scale = Main.rand.NextFloat(1.2f, 1.6f);
+                    float randomWhitingValue = Main.rand.NextFloat(0.0f, 0.2f);
+                    Color color = Color.Lerp(DustColors[Main.rand.Next(0, DustColors.Count)], Color.White, randomWhitingValue);
+
+                    Vector2 velocity = k.ToRotationVector2() * (float)(Math.Cos((double)(k * (float)bigWavyPetalCount) + rand2PI) + 5.1f) * speed / 4;
+                    Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, new Vector2?(velocity), 0, color, scale);
+                    dust.noGravity = true;
+                    dust.fadeIn = -1f;
+
+                    Dust dust2 = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, new Vector2?(velocity) * 0.75f, 0, color, scale);
+                    dust2.noGravity = true;
+                    dust2.fadeIn = -1f;
+                }
+            } else {
+                for (float k = 0f; k < MathHelper.TwoPi; k += 0.2f)
+                {
+                    float scale = Main.rand.NextFloat(1f, 1.2f);
+                    float randomWhitingValue = Main.rand.NextFloat(0.5f, 0.7f);
+                    Color color = Color.Lerp(DustColors[Main.rand.Next(0, DustColors.Count)], Color.White, randomWhitingValue);
+
+                    Vector2 velocity = k.ToRotationVector2() * (0.4f * (float)(Math.Sin((double)(k * (float)wavyPetalCount) + rand2PI) + 2.1f) * (speed / 2));
+                    Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, new Vector2?(velocity), 0, color * 0.3f, scale);
+                    dust.noGravity = true;
+                    dust.fadeIn = -1f;
+                }
+            }
+
+            CircularDamage(80f);
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return new Color(238, 171, 255, 25) * Projectile.Opacity;
+        }
+    }
+}
--- Projectiles/Melee/Shortswords/CosmicShivProj.cs
+++ Projectiles/Melee/Shortswords/CosmicShivProj.cs
@@ -1,9 +1,14 @@
 ﻿using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.Buffs.StatDebuffs;
 using CalamityMod.Items.Weapons.Melee;
 using CalamityMod.Projectiles.BaseProjectiles;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
+using Terraria.GameContent;
+using Terraria.Graphics;
+using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -14,6 +19,8 @@ namespace CalamityMod.Projectiles.Melee.Shortswords
     {
         public override LocalizedText DisplayName => CalamityUtils.GetItemName<CosmicShiv>();
         public override string Texture => "CalamityMod/Items/Weapons/Melee/CosmicShiv";
+        public bool MeleeEffect = false;
+        public int NumHits = 0;
 
         public override void SetDefaults()
         {
@@ -29,12 +36,12 @@ namespace CalamityMod.Projectiles.Melee.Shortswords
             Projectile.hide = true;
             Projectile.ownerHitCheck = true;
             Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 8;
+            Projectile.localNPCHitCooldown = -1;
         }
 
         public override Action<Projectile> EffectBeforePullback => (proj) =>
         {
-            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity * 14f, ModContent.ProjectileType<CosmicShivBall>(), Projectile.damage, Projectile.knockBack, Projectile.owner, 0f, 0f);
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity * 3f, ModContent.ProjectileType<CosmicShivTrail>(), Projectile.damage, Projectile.knockBack, Projectile.owner, 0f, 0f);
         };
 
         public override void SetVisualOffsets()
@@ -50,31 +57,65 @@ namespace CalamityMod.Projectiles.Melee.Shortswords
             DrawOriginOffsetY = -(HalfSpriteHeight - HalfProjHeight);
         }
 
-        public override void ExtraBehavior()
-        {
-            Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, 173);
-        }
-
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
-            for (int k = 0; k < 36; k++)
+            float rand2PI = Main.rand.NextFloat(MathHelper.TwoPi);  // Random rotation offset
+            int petalCount = Main.rand.Next(5, 8);                  // Number of star points
+            float speed = Main.rand.Next(18, 24);                   // Size of star
+
+            // Bool check so it only performs this effect once per strike, at a 50% chance
+            if (!MeleeEffect && Main.rand.NextBool())
             {
-                int dustID = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height -16, 173, 0f, 0f, 0, default, 1f);
-            Main.dust[dustID].velocity *= 3f;
-            Main.dust[dustID].scale *= 2f;
+                for (float k = 0f; k < MathHelper.TwoPi; k += 0.08f)
+                {
+                    float scale = Main.rand.NextFloat(1.5f, 1.9f);
+                    float randomWhitingValue = Main.rand.NextFloat(0.0f, 0.2f);
+                    Color color = Color.Lerp(CosmicShivTrail.DustColors[Main.rand.Next(0, CosmicShivTrail.DustColors.Count)], Color.White, randomWhitingValue);  // Just for even more variety in colors idk
+                    Vector2 velocity = StarPolarEquation(petalCount, k, rand2PI) * speed * 2;
+                    Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, velocity, 0, color, scale);
+                    dust.noGravity = true;
+                    dust.fadeIn = -1f;      // I don't know if this does anything but it looks like the dust fades out faster with this
+
+                    Vector2 velocity2 = StarPolarEquation(petalCount, k - 0.04f, rand2PI) * speed * 2 * 0.9f;   // Inner star
+                    Dust dust2 = Dust.NewDustPerfect(Projectile.Center, DustID.FireworksRGB, velocity2, 0, color, scale);
+                    dust2.noGravity = true;
+                    dust2.fadeIn = -1f;
+                }
             }
+
+            if (NumHits < 3) {      // Just to avoid insanity from stacking so many super dummies or hitting many worm segments at once
+                // 2x bonus for true melee hits (spawns an additional aura plus the one aura from the cosmic shiv ball)
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<CosmicShivAura>(), Projectile.damage, Projectile.knockBack, Projectile.owner, target.whoAmI);
+                NumHits++;
+            }
+
             target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 120);
         }
 
         public override void OnHitPlayer(Player target, Player.HurtInfo info)
         {
-            for (int k = 0; k < 36; k++)
-            {
-                int dustID = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height - 16, 173, 0f, 0f, 0, default, 1f);
-                Main.dust[dustID].velocity *= 3f;
-                Main.dust[dustID].scale *= 2f;
-            }
-            target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 120);
+            // Congrats on trying to PvP with this
+            target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 216000);
+            target.AddBuff(ModContent.BuffType<VulnerabilityHex>(), 216000);
+            target.AddBuff(ModContent.BuffType<WhisperingDeath>(), 216000);
+            target.AddBuff(ModContent.BuffType<CrushDepth>(), 216000);
+            target.AddBuff(ModContent.BuffType<Nightwither>(), 216000);
+            target.AddBuff(ModContent.BuffType<VulnerabilityHex>(), 216000);
+            target.AddBuff(ModContent.BuffType<GlacialState>(), 600);           // Not that evil
+            target.AddBuff(BuffID.Stoned, 600);                                 // But just in case you're immune to glacial state
+        }
+
+        public Vector2 StarPolarEquation(int pointCount, float angle, float offset)     // For OnHitNPC(), Thanks Dominic
+        {
+            // There should be a star point that looks directly upward. However, that isn't the case for odd star counts with the equation below.
+            // To address this, a -90 degree rotation is performed.
+            // Refer to desmos to view the resulting shape this creates. It's basically a black box of trig otherwise.
+            float sqrt3 = 1.732051f;
+            float numerator = MathF.Cos(MathHelper.Pi * (pointCount + 1f) / pointCount);
+            float starAdjustedAngle = MathF.Asin(MathF.Cos(pointCount * angle + offset)) * 2f;
+            float denominator = MathF.Cos((starAdjustedAngle + MathHelper.PiOver2 * pointCount) / (pointCount * 2f));
+            Vector2 result = angle.ToRotationVector2() * numerator / denominator / sqrt3;
+            return result;
         }
     }
 }
