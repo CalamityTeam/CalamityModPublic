ace4c977cf972eeb56a801c8dca987d099e4382f
Flowaria <wnsghgj1234@naver.com>
2024-08-06 13:32:45 +0900

    Fixed Issue where Cosmolight use visual can appear even with boss is alive

--- Items/Tools/ClimateChange/Cosmolight.cs
+++ Items/Tools/ClimateChange/Cosmolight.cs
@@ -32,16 +32,13 @@ namespace CalamityMod.Items.Tools.ClimateChange
             itemGroup = (ContentSamples.CreativeHelper.ItemGroup)CalamityResearchSorting.ToolsOther;
         }
 
-        public override bool CanUseItem(Player player)
+        public override bool CanUseItem(Player player) => !CalamityPlayer.areThereAnyDamnBosses;
+
+        public override bool? UseItem(Player player)
         {
             //Only SinglePlayer or DedServ should change time to prevent unwanted race condition
             if (Main.netMode == NetmodeID.MultiplayerClient)
                 return true;
-
-            if (CalamityPlayer.areThereAnyDamnBosses)
-                return false;
-
-
 
             // Early Morning -> Noon
             if (Main.dayTime && Main.time < NoonCutoff)
