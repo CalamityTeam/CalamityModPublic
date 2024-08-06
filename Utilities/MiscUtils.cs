9725e1b939155ade212e63a254aa5c5470b02afb
Flowaria <wnsghgj1234@naver.com>
2024-08-06 13:39:05 +0900

    TorrentialTear: removed duplicated WorldSync

--- Utilities/MiscUtils.cs
+++ Utilities/MiscUtils.cs
@@ -195,7 +195,7 @@ namespace CalamityMod
             sfx.Volume = MathHelper.Clamp(sfx.Volume * volumeMultiplier, 0f, 1f);
         }
 
-        public static void StartRain(bool torrentialTear = false, bool maxSeverity = false)
+        public static void StartRain(bool torrentialTear = false, bool maxSeverity = false, bool worldSync = true)
         {
             int framesInDay = 86400;
             int framesInHour = framesInDay / 24;
@@ -245,7 +245,20 @@ namespace CalamityMod
             Main.raining = true;
             if (torrentialTear)
                 TorrentialTear.AdjustRainSeverity(maxSeverity);
-            CalamityNetcode.SyncWorld();
+
+            if (worldSync)
+                CalamityNetcode.SyncWorld();
+        }
+
+        public static void StopRain(bool clearWeather = false, bool worldSync = true)
+        {
+            if (clearWeather)
+                Main.StopRain();
+            else
+                Main.raining = false;
+            
+            if (worldSync)
+                CalamityNetcode.SyncWorld();
         }
 
         public static void StartSandstorm()
