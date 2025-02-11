8dcbe75ebcb64121ddfc846ecb4051a06b580674
mountaindrew10 <marhaj04@gmail.com>
2025-02-11 03:34:13 -0600

    1 - Removed Yharon plush promotional messages. 2 - Actually killed coco's feather.

--- Items/TreasureBags/MiscGrabBags/StarterBag.cs
+++ Items/TreasureBags/MiscGrabBags/StarterBag.cs
@@ -110,16 +110,6 @@ namespace CalamityMod.Items.TreasureBags.MiscGrabBags
 
             itemLoot.AddIf(getsOracleHeadphones, ModContent.ItemType<OracleHeadphones>());
 
-            // Bird dev item
-            // Name specific: "bird"
-            static bool getsSakuraFeather(DropAttemptInfo info)
-            {
-                string playerName = info.player.name;
-                return playerName == "bird";
-            }
-
-            itemLoot.AddIf(getsSakuraFeather, ModContent.ItemType<CocosFeather>());
-
             // Fabsol dev item
             // Name specific: "Fabsol" or "Cirrus"
             static bool getsCrystalHeartVodka(DropAttemptInfo info)
