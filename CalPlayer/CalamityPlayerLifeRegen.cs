84bd8b5d284253481300ac45b51e6d036462327f
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-05-26 17:46:53 -0400

    Ampoule + Radiance life regen buff

--- CalPlayer/CalamityPlayerLifeRegen.cs
+++ CalPlayer/CalamityPlayerLifeRegen.cs
@@ -424,7 +424,7 @@ namespace CalamityMod.CalPlayer
             {
                 float missingLifeRatio = (Player.statLifeMax2 - Player.statLife) / (float)Player.statLifeMax2;
                 //Ambrosial Ampule and ooze give between 2 and 6 hp/s
-                int lifeRegenToGive = (int)Math.Round(MathHelper.Lerp((purity || aAmpoule ? 2f : 4f), (purity || aAmpoule ? 8f : 12f), missingLifeRatio)); // Rounding is needed for it to ever actually give +6 hp/s, as the integer conversion would otherwise floor it.
+                int lifeRegenToGive = (int)Math.Round(MathHelper.Lerp((purity || aAmpoule ? 2f : 4f), (purity || aAmpoule ? 10f : 12f), missingLifeRatio)); // Rounding is needed for it to ever actually give +6 hp/s, as the integer conversion would otherwise floor it.
                 Player.lifeRegen += (int)lifeRegenToGive;
             }
 
