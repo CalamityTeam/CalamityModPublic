b2234be8066985bacc0ba0fa611a75e5fe608068
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-05-28 21:34:31 -0400

    Super Slau uses Galac Sing for consistency with public elemental set

--- Items/Weapons/Ranged/SuperradiantSlaughterer.cs
+++ Items/Weapons/Ranged/SuperradiantSlaughterer.cs
@@ -124,7 +124,7 @@ namespace CalamityMod.Items.Weapons.Ranged
                 AddIngredient<SpeedBlaster>().
                 AddIngredient(ItemID.LunarBar, 5).
                 AddIngredient<LifeAlloy>(5).
-                AddIngredient(ItemID.FragmentVortex, 5).
+                AddIngredient<GalacticaSingularity>(5).
                 AddTile(TileID.MythrilAnvil).
                 Register();
         }
