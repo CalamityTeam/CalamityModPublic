60bc1582716e582fb3e965015a650fc9e0664f20
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-11-20 13:16:59 -0500

    Restored Sans Undertale with different dedication

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -205,6 +205,7 @@ namespace CalamityMod.NPCs
         };
         private static readonly string[] SkeletonMerchantNames =
         {
+            "Sans Undertale", // <@!534770496038895616> (done_22_)
             "Papyrus Undertale", // <@!262663471189983242> (nycro)
             "Gaster Undertale", // <@!924706306093379614> (enamoured)
             "Mr. Bones", // <@!359215912856977408> (jaybones.)
