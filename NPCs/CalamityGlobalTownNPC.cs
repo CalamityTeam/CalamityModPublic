314368ae17ccecb507e94bbaf23b714806be99da
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-11-20 13:10:44 -0500

    Removed remaining instances from repo

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -205,7 +205,6 @@ namespace CalamityMod.NPCs
         };
         private static readonly string[] SkeletonMerchantNames =
         {
-            "Sans Undertale", // <@!145379091648872450> (shayy)
             "Papyrus Undertale", // <@!262663471189983242> (nycro)
             "Gaster Undertale", // <@!924706306093379614> (enamoured)
             "Mr. Bones", // <@!359215912856977408> (jaybones.)
