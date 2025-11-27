a673e77fb3a8efde454e077cc35ffe4d7677955d
Emi <43892535+emiipo@users.noreply.github.com>
2025-11-27 18:23:11 +0000

    Avoiding invalid casting in case of different ModWaterStyle.

--- Systems/CalamityModWaterStyle.cs
+++ Systems/CalamityModWaterStyle.cs
@@ -38,19 +38,27 @@ namespace CalamityMod.Systems
     {
         internal static void ModifyLightSetup(int i, int j, int type, ref float r, ref float g, ref float b)
         {
-            CalamityModWaterStyle styles = (CalamityModWaterStyle)LoaderManager.Get<WaterStylesLoader>().Get(type);
-            if (styles != null)
+            ModWaterStyle waterStyle = LoaderManager.Get<WaterStylesLoader>().Get(type);
+            if(waterStyle is CalamityModWaterStyle)
             {
-                styles?.ModifyLight(i, j, ref r, ref g, ref b);
+                CalamityModWaterStyle styles = (CalamityModWaterStyle)waterStyle;
+                if (styles != null)
+                {
+                    styles?.ModifyLight(i, j, ref r, ref g, ref b);
+                }
             }
         }
 
         internal static void DrawColorSetup(int x, int y, int type, ref VertexColors liquidColor, bool isSlope = false)
         {
-            CalamityModWaterStyle styles = (CalamityModWaterStyle)LoaderManager.Get<WaterStylesLoader>().Get(type);
-            if (styles != null)
+            ModWaterStyle waterStyle = LoaderManager.Get<WaterStylesLoader>().Get(type);
+            if(waterStyle is CalamityModWaterStyle)
             {
-                styles?.DrawColor(x, y, ref liquidColor, isSlope);
+                CalamityModWaterStyle styles = (CalamityModWaterStyle)waterStyle;
+                if (styles != null)
+                {
+                    styles?.DrawColor(x, y, ref liquidColor, isSlope);
+                }
             }
         }
     }
