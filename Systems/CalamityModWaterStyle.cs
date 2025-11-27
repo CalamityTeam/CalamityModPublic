13abe40eac641a36a40040136012bda2260dee99
Flowaria <wnsghgj1234@naver.com>
2025-11-27 14:24:53 +0900

    Fixed Type Casting Exception on CalamityModWaterStyle

--- Systems/CalamityModWaterStyle.cs
+++ Systems/CalamityModWaterStyle.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using Terraria;
-using Terraria.Graphics;
+﻿using Terraria.Graphics;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Systems
@@ -38,20 +34,35 @@ namespace CalamityMod.Systems
     {
         internal static void ModifyLightSetup(int i, int j, int type, ref float r, ref float g, ref float b)
         {
-            CalamityModWaterStyle styles = (CalamityModWaterStyle)LoaderManager.Get<WaterStylesLoader>().Get(type);
-            if (styles != null)
+            if (TryGetCalamityWaterStyle(type, out var styles))
             {
-                styles?.ModifyLight(i, j, ref r, ref g, ref b);
+                styles.ModifyLight(i, j, ref r, ref g, ref b);
             }
         }
 
         internal static void DrawColorSetup(int x, int y, int type, ref VertexColors liquidColor, bool isSlope = false)
         {
-            CalamityModWaterStyle styles = (CalamityModWaterStyle)LoaderManager.Get<WaterStylesLoader>().Get(type);
-            if (styles != null)
+            if (TryGetCalamityWaterStyle(type, out var styles))
             {
-                styles?.DrawColor(x, y, ref liquidColor, isSlope);
+                styles.DrawColor(x, y, ref liquidColor, isSlope);
             }
+        }
+
+        internal static bool TryGetCalamityWaterStyle(int type, out CalamityModWaterStyle waterStyle)
+        {
+            waterStyle = GetCalamityWaterStyle(type);
+            return waterStyle != null;
+        }
+
+        internal static CalamityModWaterStyle GetCalamityWaterStyle(int type)
+        {
+            var modWaterStyle = LoaderManager.Get<WaterStylesLoader>().Get(type);
+            if (modWaterStyle is CalamityModWaterStyle calWaterStyle)
+            {
+                return calWaterStyle;
+            }
+
+            return null;
         }
     }
 }
