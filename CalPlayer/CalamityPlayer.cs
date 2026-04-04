8c0af01626ff1be81dd3de3c18504185de5ced92
shade-storm <snowstorm2103@gmail.com>
2026-04-04 19:12:26 +0100

    Fixed Infinite Stealth when messing with GenericAttackSpeed with other mods

--- CalPlayer/CalamityPlayer.cs
+++ CalPlayer/CalamityPlayer.cs
@@ -5932,7 +5932,7 @@ namespace CalamityMod.CalPlayer
             // "Clockwork" weapons can chain-fire multiple stealth strikes (really only 2 max) until you run out of stealth.
             bool animationCheck = it.useAnimation == it.useTime
                 ? Player.itemAnimation == Player.itemAnimationMax - 1 // Standard weapon (first frame of use animation)
-                : Player.itemTime == (int)(it.useTime / Player.GetAttackSpeed<RogueDamageClass>()); // Clockwork weapon (first frame of any individual use event)
+                : Player.itemTime == (int)(it.useTime / Player.GetTotalAttackSpeed<RogueDamageClass>()); // Clockwork weapon (first frame of any individual use event)
 
             if (!stealthStrikeThisFrame && animationCheck && playerUsingWeapon)
             {
